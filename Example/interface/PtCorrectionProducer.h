27d58f7f77666e19683fe4debcf0f76dca18e830
Thomas Hauth <Thomas.Hauth@cern.ch>
2013-11-07 16:02:56 +0100

    added PtCorrection global producer

---
+++ Example/interface/PtCorrectionProducer.h
@@ -0,0 +1,27 @@
+/* Copyright (c) 2013 - All Rights Reserved
+ *   Thomas Hauth  <Thomas.Hauth@cern.ch>
+ *   Joram Berger  <Joram.Berger@cern.ch>
+ *   Dominik Haitz <Dominik.Haitz@kit.edu>
+ */
+
+#pragma once
+
+#include "Artus/Core/interface/Cpp11Support.h"
+
+#include "TraxTypes.h"
+
+class PtCorrectionProducer: public TraxGlobalMetaDataProducerBase {
+public:
+
+	virtual bool PopulateGlobalMetaData(TraxEventData const& data,
+			TraxGlobalMetaData & metaData,
+			TraxGlobalSettings const& globalSettings) const
+					ARTUS_CPP11_OVERRIDE {
+
+		metaData.m_floatPtSim_corrected = data.m_floatPtSim
+				* globalSettings.GetProducerPtCorrectionFactor();
+
+		return true;
+	}
+
+};
