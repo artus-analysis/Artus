40614c4d8734a3a2580078fa88a4c9a85656355a
Thomas Müller <tmueller@ekp.uni-karlsruhe.de>
2014-02-04 17:55:26 +0100

    added python logging script

---
+++ Configuration/python/logger.py
@@ -0,0 +1,23 @@
+
+# -*- coding: utf-8 -*-
+
+import argparse
+import logging
+
+# http://docs.python.org/2/library/logging.html
+# http://docs.python.org/2/howto/logging.html#logging-advanced-tutorial
+
+loggingParser = argparse.ArgumentParser(add_help=False)
+loggingParser.add_argument("--log-level", default="debug",
+                           choices=["debug", "info", "warning", "error", "critical"],
+                           help="Log level. [Default: debug]")
+
+def initLogger(argParserArgs=None, name=""):
+
+	logLevel = "debug"
+
+	if argParserArgs: logLevel = argParserArgs.log_level
+
+	logging.basicConfig(level=getattr(logging, logLevel.upper()),
+	                    format="%(levelname)s: %(message)s")
+
