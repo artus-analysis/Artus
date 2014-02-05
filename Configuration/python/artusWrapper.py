effad50ce72838af19440fefa98c5678c53bf387
Thomas Müller <tmueller@ekp.uni-karlsruhe.de>
2014-02-05 01:25:19 +0100

    added root config input to Artus wrapper, improved error handling

--- Configuration/python/artusWrapper.py
+++ Configuration/python/artusWrapper.py
@@ -13,8 +13,11 @@ import Artus.Configuration.jsonTools as jsonTools
 def artusWrapper(defaultExecutable=None):
 	parser = argparse.ArgumentParser(description="Artus executable wrapper script", parents=[logger.loggingParser])
 
-	parser.add_argument("-c", "--config-files", help="JSON Config files.", nargs="+", required=True)
-	parser.add_argument("-i", "--input-files", help="Input root files.", nargs="+", required=True)
+	configGroup = parser.add_mutually_exclusive_group(required=True)
+	configGroup.add_argument("-c", "--config-files", help="JSON config files or string represenations of dicts", nargs="+")
+	configGroup.add_argument("-r", "--root-config", help="Take JSON configuration from existing root file.", nargs=1)
+	
+	parser.add_argument("-i", "--input-files", help="Input root files. Leave empty (\"\") if input files from root file should be taken.", nargs="+", required=True)
 	parser.add_argument("-o", "--output-file", help="Output root file. [Default: output.root]", default="output.root")
 	
 	if defaultExecutable: parser.add_argument("-x", "--executable", help="Artus executable. [Default: %s]" % str(defaultExecutable), default=defaultExecutable)
@@ -23,12 +26,18 @@ def artusWrapper(defaultExecutable=None):
 	args = parser.parse_args()
 	logger.initLogger(args)
 	
+	if args.config_files and args.input_files == "":
+		logging.getLogger(__name__).critial("No input file specified!")
+		sys.exit(1)
+	
 	# constuct main JSON config
 	mainConfig = {
-		"InputFiles" : args.input_files,
 		"OutputPath" : args.output_file,
 	}
+	if args.config_files: mainConfig["InputFiles"] = args.input_files
+	else: args.config_files = args.root_config
 	mainConfig = jsonTools.mergeJsonDicts(mainConfig, *args.config_files)
+	
 	mainConfigFileName = reduce(lambda a, b: a+"__"+b, map(lambda fileName: os.path.splitext(os.path.basename(fileName))[0], args.config_files))+".json"
 	jsonTools.saveJsonDict(mainConfig, mainConfigFileName)
 	logging.getLogger(__name__).info("Saved combined JSON config \"%s\" for temporary usage." % mainConfigFileName)
@@ -36,7 +45,12 @@ def artusWrapper(defaultExecutable=None):
 	# call C++ executable
 	command = args.executable + " " + mainConfigFileName
 	logging.getLogger(__name__).info("Execute \"%s\"." % command)
-	os.system(command)
+	exitCode = os.system(command)
+	if exitCode != 0:
+		logging.getLogger(__name__).error("Exit with code %s.\n\n" % exitCode)
+		logging.getLogger(__name__).info("Dump configuration:\n")
+		jsonTools.printJsonDict(mainConfigFileName)
+		sys.exit(exitCode)
 	
 	# remove tmp. config
 	logging.getLogger(__name__).info("Remove temporary config file.")
