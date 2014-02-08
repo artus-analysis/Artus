01ff2d598382ba3a20690e1cb72d481a9258cc74
Thomas Hauth <Thomas.Hauth@cern.ch>
2014-02-08 23:57:27 +0100

    first version of Kappa example

---
+++ Example/interface/TraxKappaEventProvider.h
@@ -0,0 +1,31 @@
+/* Copyright (c) 2013 - All Rights Reserved
+ *   Thomas Hauth  <Thomas.Hauth@cern.ch>
+ *   Joram Berger  <Joram.Berger@cern.ch>
+ *   Dominik Haitz <Dominik.Haitz@kit.edu>
+ */
+
+#pragma once
+
+#include "Artus/Core/interface/Cpp11Support.h"
+#include "Artus/Provider/interface/KappaEventProvider.h"
+
+#include "TraxTypes.h"
+
+/*
+ * Will load the corresponding ntuple from a root file
+ * The memory locations are passed to ROOT one time, in the
+ * WireEvent() method call.
+ */
+
+class TraxKappaEventProvider: public KappaEventProvider<TraxTypes> {
+public:
+	TraxEventProvider(FileInterface2 & fi, InputTypeEnum inpType) :
+		KappaEventProvider<TraxTypes>(fi, inpType) {
+	}
+
+	virtual void WireEvent( TraxTypes::global_setting_type const& )
+		ARTUS_CPP11_OVERRIDE
+	{
+		// set up the Kppa pointers to our local memory regions
+	}
+};
