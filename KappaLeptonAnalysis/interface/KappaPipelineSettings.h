d7613ff45bac3571664114f8253662178e6005df
Thomas Müller <tmueller@ekp.uni-karlsruhe.de>
2014-02-10 18:10:13 +0100

    added missing file

---
+++ KappaLeptonAnalysis/interface/KappaPipelineSettings.h
@@ -0,0 +1,23 @@
+
+#pragma once
+
+#include "Artus/Core/interface/GlobalInclude.h"
+#include "Artus/Configuration/interface/SettingsBase.h"
+#include "Artus/Configuration/interface/SettingMacros.h"
+#include "Artus/Configuration/interface/PropertyTreeSupport.h"
+
+class KappaLeptonPipelineSettings: public SettingsBase {
+public:
+
+};
+
+class KappaLeptonGlobalSettings: public GlobalSettingsBase {
+
+public:
+	IMPL_SETTING_DEFAULT(std::string, Muons, "");
+	IMPL_SETTING_DEFAULT(std::string, Jets, "");
+	IMPL_SETTING_DEFAULT(std::string, BeamSpot, "");
+	IMPL_SETTING_DEFAULT(std::string, VertexSummary, "");
+	IMPL_SETTING_DEFAULT(std::string, LumiMetadata, ""); // TODO
+
+};
