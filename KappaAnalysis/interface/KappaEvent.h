d1e87b340d8d55d23768f4cb7ff9a475b4d160b8
Thomas Müller <tmueller@ekp.uni-karlsruhe.de>
2014-02-11 17:56:51 +0100

    added (completed?) base kappa event

--- KappaAnalysis/interface/KappaEvent.h
+++ KappaAnalysis/interface/KappaEvent.h
@@ -19,15 +19,26 @@ public:
 
 	// Jets
 	KDataPFJets* m_pfJets = 0;
+	KJetArea* m_jetArea = 0;
 	
 	// MET info
+	KDataPFMET* m_pfMet = 0;
 	
 	// Vertex info
 	KDataBeamSpot* m_beamSpot = 0;
 	KVertexSummary* m_vertexSummary = 0;
+	
+	// Generator info
+	KGenParticles* m_particles = 0;
+	
 
 	// Meta data
 	KLumiMetadata* m_lumiMetadata = 0;
+	KGenLumiMetadata* m_genLumiMetadata = 0;
+	KEventMetadata* m_eventMetadata = 0;
+	KGenEventMetadata* m_genEventMetadata = 0;
+	KFilterMetadata* m_filterMetadata = 0;
+	KFilterSummary* m_filterSummary = 0;
 };
 
 
