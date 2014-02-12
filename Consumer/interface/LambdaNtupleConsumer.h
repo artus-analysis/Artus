b7089e4ce1b7d7e782f04b228fbb40269052d0e0
Thomas Müller <tmueller@ekp.uni-karlsruhe.de>
2014-02-12 23:34:27 +0100

    added new NtupleConsumer which contains less string operations

---
+++ Consumer/interface/LambdaNtupleConsumer.h
@@ -0,0 +1,77 @@
+
+#pragma once
+
+#include "Artus/Consumer/interface/NtupleConsumerBase.h"
+
+/*
+ * Fills NTuples with valueExtractors defined as lambda functions
+ * This removes the string operations from its base class
+ */
+
+template<class TTypes>
+class LambdaNtupleConsumer: public NtupleConsumerBase<TTypes> {
+
+public:
+
+	typedef typename TTypes::event_type event_type;
+	typedef typename TTypes::product_type product_type;
+	typedef typename TTypes::setting_type setting_type;
+	
+	typedef typename std::function<float(event_type const&, product_type const&)> float_extractor_lambda;
+
+	virtual std::string GetConsumerId()
+	{
+		return "lambda_ntuple";
+	}
+
+	LambdaNtupleConsumer(std::map<std::string, float_extractor_lambda> valueExtractorMap = std::map<std::string, float_extractor_lambda>()) :
+			NtupleConsumerBase<TTypes>(),
+			m_valueExtractorMap(valueExtractorMap)
+	{
+	}
+
+	virtual ~LambdaNtupleConsumer() {
+	}
+
+	virtual void Init(Pipeline<TTypes> * pset) ARTUS_CPP11_OVERRIDE {
+		NtupleConsumerBase<TTypes>::Init(pset);
+		
+		float defaultValue = -999.0;
+		float_extractor_lambda defaultExtractor = [&defaultValue](event_type const&, product_type const&) { return defaultValue; };
+		
+		// construct extractors vector
+		m_valueExtractors.clear();
+		m_valueExtractors.resize(this->quantities_vector.size());
+		transform(this->quantities_vector.begin(), this->quantities_vector.end(), m_valueExtractors.begin(),
+		          [&](std::string quantity) { return(m_valueExtractorMap.count(quantity) > 0 ? m_valueExtractorMap[quantity] : defaultExtractor); });
+
+	}
+
+	virtual void ProcessFilteredEvent(event_type const& event, product_type const& product ) ARTUS_CPP11_OVERRIDE
+	{
+		// do not call NtupleConsumerBase<TTypes>::ProcessFilteredEvent due to different filling logic
+		ConsumerBase<TTypes>::ProcessFilteredEvent(event, product);
+		
+		// preallocated vector
+		std::vector<float> array (this->quantities_vector.size());
+		
+		size_t arrayIndex = 0;
+		for(typename std::vector<float_extractor_lambda>::iterator valueExtractor = m_valueExtractors.begin();
+		    valueExtractor != m_valueExtractors.end(); ++valueExtractor)
+		{
+			array[arrayIndex] = (*valueExtractor)(event, product);
+			arrayIndex++;
+		}
+
+		// add the array to the ntuple
+		this->ntuple->Fill(&array[0]);
+	}
+
+
+private:
+	std::map<std::string, float_extractor_lambda> m_valueExtractorMap;
+	std::vector<float_extractor_lambda> m_valueExtractors;
+
+};
+
+
