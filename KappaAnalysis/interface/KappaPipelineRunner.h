d5af535102b8d97a8c1dcad9d5c4810bb8f46b10
Roger <roger.wolf@cern.ch>
2014-02-26 18:51:09 +0100

    added/doxygenized comments

--- KappaAnalysis/interface/KappaPipelineRunner.h
+++ KappaAnalysis/interface/KappaPipelineRunner.h
@@ -3,11 +3,24 @@
 
 #include "Artus/Core/interface/PipelineRunner.h"
 
+#include "Artus/KappaAnalysis/interface/Producers/GenTauDecayProducer.h"
 #include "Artus/KappaAnalysis/interface/Producers/ValidElectronsProducer.h"
 #include "Artus/KappaAnalysis/interface/Producers/ValidMuonsProducer.h"
 #include "Artus/KappaAnalysis/interface/Producers/ValidTausProducer.h"
 #include "Artus/KappaAnalysis/interface/Producers/ValidJetsProducer.h"
 
+/**
+   \brief Class to manages all registered Pipelines and to connect them to the event.
+   
+   The KappaPipelineRunner is derived from the PipelineRunner of Artus/Core. It utilizes the 
+   KappaEventProvider to load events and passes them to all registered Pipelines. The EventProvider 
+   is passed to the function PipelineRunner::RunPipelines as an argument. In addition five Global-
+   Producers are registered, which can generate Pipeline-independet products of the event. These 
+   GlobalProducers are run before any pipeline is started and the generated data is passed on to 
+   the pipelines. 
+
+   The order of running is: GlobalProducers -> LocalProducers -> Filters -> Consumers.
+*/
 
 template<class TTypes, typename TPipeline, typename TGlobalProducer>
 class KappaPipelineRunner : public PipelineRunner<TPipeline, TGlobalProducer> {
@@ -23,12 +36,15 @@ public:
 	}
 	
 	
-	// Overwrite this function to define your own order of calling the producers
+	/// Overwrite this function to define your own order of calling the producers
 	virtual void AddGlobalProducersById() {
 	
 		BOOST_FOREACH(std::string producerId, m_globalSettings.GetGlobalProducers())
 		{
-			if(producerId == ValidElectronsProducer<TTypes>().GetProducerId()) {
+		        if(producerId == GenTauDecayProducer<TTypes>().GetProducerId()) {
+		  		this->AddGlobalProducer(new GenTauDecayProducer<TTypes>());
+		  	}
+			else if(producerId == ValidElectronsProducer<TTypes>().GetProducerId()) {
 				this->AddGlobalProducer(new ValidElectronsProducer<TTypes>());
 			}
 			else if(producerId == ValidMuonsProducer<TTypes>().GetProducerId()) {
---
+++ KappaAnalysis/interface/Producers/GenTauDecayProducer.h
@@ -0,0 +1,61 @@
+
+#pragma once
+
+#include <Math/VectorUtil.h>
+
+#include "Kappa/DataFormats/interface/Kappa.h"
+
+
+/**
+   \brief GlobalProducer, for tau decays on generator level.
+   
+   This producers has the GenParticles as input and will extract the following information from 
+   this collection:
+
+   - pointer to tau particle
+   - list of pointers to tau decay products
+*/
+
+template<class TTypes>
+class GenTauDecayProducer: public GlobalProducerBase<TTypes>
+{
+
+public:
+
+	typedef typename TTypes::event_type event_type;
+	typedef typename TTypes::product_type product_type;
+	typedef typename TTypes::global_setting_type global_setting_type;
+
+	virtual std::string GetProducerId() {
+		return "gen_taudecay";
+	}
+
+	virtual bool ProduceGlobal(event_type const& event,
+	                           product_type& product,
+	                           global_setting_type const& globalSettings) const
+	{
+		for (KGenParticles::iterator part = event.m_genParticles->begin();
+			 part != event.m_genParticles->end(); ++part)
+		{
+		        if (abs(part->pdgId()) == 15)
+			{
+			        product.m_genTauDecay.push_back(&(*part));
+				for (unsigned int d=0; d<part->daughterIndices.size(); ++d)
+				{
+				        if (part->daughterIndex(d) < event.m_genParticles->size())
+				        {
+				                product.m_genTauDecay.push_back(&(event.m_genParticles->at(part->daughterIndex(d))));
+					}
+					else
+					{
+					  LOG("Daughter index larger than size of gen particle vector:" 
+					      << part->daughterIndex(d) << ">" << event.m_genParticles->size() << ".");
+					}
+				}
+			}
+		}
+
+		return true;
+	}
+};
+
