c6b709bb4c45665089682affa8df0b1b9394d7cc
Thomas Mueller <tmuller@cern.ch>
2014-02-28 18:39:45 +0100

    merged global and local producers

---
+++ Core/interface/ProducerBase.h
@@ -0,0 +1,46 @@
+
+#pragma once
+
+#include <boost/noncopyable.hpp>
+
+/**
+   \brief Base class for Producers, that extend the product.
+   
+   This is a base class for Producers, that extend the product after performing
+   some calculations. The function ProduceGlobal is called once per event before
+   the pipelines are split. The function ProduceLocal is called once per event
+   within a given pipeline. Only one of these functions are called.
+*/
+
+template<class TTypes>
+class ProducerBase: public boost::noncopyable {
+public:
+
+	typedef typename TTypes::event_type event_type;
+	typedef typename TTypes::product_type product_type;
+	typedef typename TTypes::setting_type setting_type;
+	typedef typename TTypes::global_setting_type global_setting_type;
+
+	virtual ~ProducerBase() {
+
+	}
+
+	virtual void InitGlobal(global_setting_type const& globalSettings) {
+	}
+
+	//virtual void InitLocal(setting_type const& pipelineSettings) {
+	//}
+
+	/// if false is returned, the event is dropped as it does not meet the minimum requirements for the producer
+	// called once per event before the pipelines are split
+	virtual bool ProduceGlobal(event_type const& event, product_type& product,
+	                           global_setting_type const& globalSettings) const = 0;
+
+	// called once per event within a given pipeline
+	virtual void ProduceLocal(event_type const& event, product_type & product, 
+	                          setting_type const& pipelineSettings) const = 0;
+
+	/// Must return a unique id of the producer.
+	virtual std::string GetProducerId() = 0;
+};
+
