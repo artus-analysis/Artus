299fece9f208c873ded6efa6824a354ebee430eb
Georg <sieber@cern.ch>
2014-03-13 17:28:37 +0100

    adapted artusWrapper example and small fixes

--- Configuration/python/artusWrapper.py
+++ Configuration/python/artusWrapper.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 
 import argparse
@@ -18,17 +17,18 @@ import Artus.Configuration.jsonTools as jsonTools
 # main function
 class ArtusWrapper(object):
 
-	def __init__(self, executable=None):
+	def __init__(self, executable=None, userArgParsers=None):
 
 		self._config = jsonTools.JsonDict()
 		self._executable = executable
 
-		self.parser = None
+		self._parser = None
 		#Load default argument parser
-		self._init_argumentparser()
+		self._initArgumentParser(userArgParsers)
 		#Parse command line arguments and return dict
-		self._args = vars(self.parser.parse_args())
 
+		self._args = vars(self._parser.parse_args())
+		
 	def run(self):
 
 		#Expand Config
@@ -37,7 +37,7 @@ class ArtusWrapper(object):
 
 		#Run Artus if desired
 		if not self._args['no_run']:
-			callExecutable(self._configFilename())
+			self.callExecutable()
 
 	def setInputFilenames(self, files):
 		if isinstance(files, basestring):
@@ -60,12 +60,12 @@ class ArtusWrapper(object):
 			basename = "artus_{0}.json".format(hashlib.md5(str(self._config)).hexdigest())
 			filepath = os.path.join(tempfile.gettempdir(), basename)
 		self._configFilename = filepath
+		print self._configFilename
 		self._config.save(filepath)
 	
 	def expandConfig(self):
 
 		# merge all base configs into the main config
-		print self._args.get('base_configs')
 		self._config += jsonTools.JsonDict.mergeAll(self._args.get('base_configs'))
 
 		#Set Input Filenames
@@ -90,12 +90,15 @@ class ArtusWrapper(object):
 		self._config = self._config.doComments().doIncludes().doComments()
 
 
-	def _init_argumentparser(self):
-	
-		self.parser = argparse.ArgumentParser(parents=[logger.loggingParser], fromfile_prefix_chars="@",
+	def _initArgumentParser(self, userArgParsers=None):
+
+		if userArgParsers is None:
+			userArgParsers = []
+
+		self._parser = argparse.ArgumentParser(parents=[logger.loggingParser] + userArgParsers, fromfile_prefix_chars="@",
 		                                       description="Wrapper for Artus executables. JSON configs can be file names pointing to JSON text files or Artus ROOT output files with saved configs or python statements that can be evaluated as dictionary. When JSON configs are merged, the first ones in the list have a higher priority than later ones. In the final config, all includes and comments are replaced accordingly.")
 
-		fileOptionsGroup = self.parser.add_argument_group("File options")
+		fileOptionsGroup = self._parser.add_argument_group("File options")
 		fileOptionsGroup.add_argument("-i", "--input-files", nargs="+", required=False,
 	                               help="Input root files. Leave empty (\"\") if input files from root file should be taken.")
 		fileOptionsGroup.add_argument("-o", "--output-file", default="output.root",
@@ -105,7 +108,7 @@ class ArtusWrapper(object):
 		fileOptionsGroup.add_argument("-n", "--project-name",
 	                               help="Name for this Artus project specifies the name of the work subdirectory.")
 
-		configOptionsGroup = self.parser.add_argument_group("Config options")
+		configOptionsGroup = self._parser.add_argument_group("Config options")
 		configOptionsGroup.add_argument("-c", "--base-configs", nargs="+", required=False, default={},
 	                                 help="JSON base configurations. All configs are merged.")
 		configOptionsGroup.add_argument("-p", "--pipeline-configs", nargs="+", action="append",
@@ -113,7 +116,7 @@ class ArtusWrapper(object):
 		configOptionsGroup.add_argument("-P", "--print-config", default=False, action="store_true",
 	                                 help="Print out the JSON config before running Artus.")
 
-		runningOptionsGroup = self.parser.add_argument_group("Running options")
+		runningOptionsGroup = self._parser.add_argument_group("Running options")
 		runningOptionsGroup.add_argument("--no-run", default=False, action="store_true",
 	                                  help="Exit before running Artus to only check the configs.")
 		runningOptionsGroup.add_argument("-r", "--root", default=False, action="store_true",
@@ -124,9 +127,9 @@ class ArtusWrapper(object):
 	                                  help="Resume the grid-control run and hadd after interrupting it.")
 
 		if self._executable:
-			self.parser.add_argument("-x", "--executable", help="Artus executable. [Default: %s]" % str(self._executable), default=self._executable)
+			self._parser.add_argument("-x", "--executable", help="Artus executable. [Default: %s]" % str(self._executable), default=self._executable)
 		else:
-			self.parser.add_argument("-x", "--executable", help="Artus executable.", required=True)
+			self._parser.add_argument("-x", "--executable", help="Artus executable.", required=True)
 	
 
 
@@ -135,11 +138,11 @@ class ArtusWrapper(object):
 	
 		exitCode = 0
 	
-		if args['batch']:
+		if self._args['batch']:
 			# check work directory
-			args['work'] = os.path.expandvars(args.get('work'))
-			if not os.path.exists(args.get('work')):
-				os.makedirs(args['work'])
+			self._args['work'] = os.path.expandvars(self._args.get('work'))
+			if not os.path.exists(self._args.get('work')):
+				os.makedirs(self._args['work'])
 	
 			# run Artus with grid-control
 			pass
@@ -147,21 +150,20 @@ class ArtusWrapper(object):
 		else:
 	
 			# check output directory
-			outputDir = os.path.dirname(args.output_file)
-			if outputDir > 0 and not os.path.exists(outputDir):
+			outputDir = os.path.dirname(self._args.get('output_file'))
+			if outputDir and not os.path.exists(outputDir):
 				os.makedirs(outputDir)
 	
 			# call C++ executable locally
-			command = [args.executable, jsonConfigFileName]
+			command = [self._executable, self._configFilename]
 			logging.getLogger(__name__).info("Execute \"%s\"." % command)
 			exitCode = subprocess.call(command)
 			if exitCode != 0:
 				logging.getLogger(__name__).error("Exit with code %s.\n\n" % exitCode)
 				logging.getLogger(__name__).info("Dump configuration:\n")
-				print jsonConfigFileName
 	
 		# remove tmp. config
-		logging.getLogger(__name__).info("Remove temporary config file.")
-		os.system("rm " + jsonConfigFileName)
+		# logging.getLogger(__name__).info("Remove temporary config file.")
+		# os.system("rm " + self._configFilename)
 	
 		return exitCode
--- Configuration/scripts/artus.py
+++ Configuration/scripts/artus.py
@@ -3,13 +3,17 @@
 
 import sys
 
-import Artus.Configuration.artusWrapper as artusWrapper
-
+from Artus.Configuration.artusWrapper import ArtusWrapper
 
 if __name__ == "__main__":
 	print "This is an example of how to use the artusWrapper."
 	print "For illustrative purposes it just tries to print the JSON config file with cat."
 	print
 	
-	sys.exit(artusWrapper.artusWrapper("cat"))
+	wrapper = ArtusWrapper("cat")
+	# User can manipulate config in code if desired
+	conf = wrapper.getConfig()
+	conf['higgs_mass'] = 126
+	wrapper.setConfig(conf)
 
+	wrapper.run()
