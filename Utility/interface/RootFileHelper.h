18f2d66d7052a19ea7c07bca9cf5e262f4fb137b
Thomas Mueller <tmuller@cern.ch>
2014-03-21 17:27:10 +0100

    Copy non-template functions from headers to source files; needs scram b clean or similar

--- Utility/interface/RootFileHelper.h
+++ Utility/interface/RootFileHelper.h
@@ -15,6 +15,8 @@
 #include <TDirectory.h>
 #include <TGraphErrors.h>
 
+#include "Artus/Core/interface/GlobalInclude.h"
+
 class RootFileHelper {
 public:
 
@@ -23,59 +25,27 @@ public:
 		T ob = (T) pDir->Get(objName.c_str());
 
 		if (ob == NULL) {
-			std::cout << std::endl << "Cant load " << objName
-					<< " from directory " << pDir->GetName() << std::endl;
-			exit(0);
+			LOG_FATAL("Cant load " + objName + " from directory " + pDir->GetName());
 		}
 		return ob;
 	}
 
-	static void SafeCd(TDirectory * pDir, std::string const& dirName) {
-		assert(pDir);
-
-		if (pDir->GetDirectory(dirName.c_str()) == 0) {
-			pDir->mkdir(dirName.c_str());
-		}
-		pDir->cd(dirName.c_str());
-	}
+	static void SafeCd(TDirectory * pDir, std::string const& dirName);
 	static TH1D * GetStandaloneTH1D_1(std::string sName, std::string sCaption,
-			int binCount, double dCustomBins[255]) {
-		return new TH1D(sName.c_str(), sCaption.c_str(), binCount,
-				&dCustomBins[0]);
-	}
-
+			int binCount, double dCustomBins[255]);
 	static TH1D * GetStandaloneTH1D_2(std::string sName, std::string sCaption,
-			int binCount, double min, double max) {
-		return new TH1D(sName.c_str(), sCaption.c_str(), binCount, min, max);
-	}
-
+			int binCount, double min, double max);
 	static TProfile * GetStandaloneTProfile(std::string m_sName,
 			std::string m_sCaption, int m_iBinXCount, double m_dBinXLower,
-			double m_dBinXUpper) {
-		return new TProfile(m_sName.c_str(), m_sCaption.c_str(), m_iBinXCount,
-				m_dBinXLower, m_dBinXUpper);
-	}
-
-	static TGraphErrors * GetStandaloneTGraphErrors(int i) {
-		return new TGraphErrors(i);
-	}
-
+			double m_dBinXUpper);
+	static TGraphErrors * GetStandaloneTGraphErrors(int i);
 	static TH2D * GetStandaloneTH2D_1(std::string m_sName,
 			std::string m_sCaption, int m_iBinXCount, double m_dBinXLower,
 			double m_dBinXUpper, int m_iBinYCount, double m_dBinYLower,
-			double m_dBinYUpper) {
-		return new TH2D(m_sName.c_str(), m_sCaption.c_str(), m_iBinXCount,
-				m_dBinXLower, m_dBinXUpper, m_iBinYCount, m_dBinYLower,
-				m_dBinYUpper);
-	}
-
+			double m_dBinYUpper);
 	static TProfile2D * GetStandaloneTProfile2D(std::string m_sName,
 			std::string m_sCaption, int m_iBinXCount, double m_dBinXLower,
 			double m_dBinXUpper, int m_iBinYCount, double m_dBinYLower,
-			double m_dBinYUpper) {
-		return new TProfile2D(m_sName.c_str(), m_sCaption.c_str(), m_iBinXCount,
-				m_dBinXLower, m_dBinXUpper, m_iBinYCount, m_dBinYLower,
-				m_dBinYUpper);
-	}
+			double m_dBinYUpper);
 
 };
--- Utility/interface/StringHelper.h
+++ Utility/interface/StringHelper.h
@@ -3,21 +3,8 @@
 
 #include <string>
 
+
 class StringHelper {
 public:
-	static std::string ReadStringFromFile(std::string fileName)
-	{
-		std::ifstream inStream(fileName, std::ios::in | std::ios::binary);
-		if(inStream)
-		{
-			std::string content;
-			inStream.seekg(0, std::ios::end);
-			content.resize(inStream.tellg());
-			inStream.seekg(0, std::ios::beg);
-			inStream.read(&content[0], content.size());
-			inStream.close();
-			return(content);
-		}
-		throw(errno);
-	};
+	static std::string ReadStringFromFile(std::string fileName);
 };
