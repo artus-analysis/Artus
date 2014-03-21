034f9b0ce8753004e6b38edeaebeb00d5491b8e9
Thomas Mueller <tmuller@cern.ch>
2014-03-21 13:40:55 +0100

    Copy non-template functions from headers to source files; needs scram b clean or similar

--- Core/interface/ProgressReport.h
+++ Core/interface/ProgressReport.h
@@ -7,12 +7,10 @@
 #pragma once
 
 #include "Cpp11Support.h"
-#include <iostream>
 
 class ProgressReportBase {
 public:
-	virtual ~ProgressReportBase() {
-	}
+	virtual ~ProgressReportBase() {  };
 
 	virtual void update(long long currentIndex, long long maxItems) = 0;
 
@@ -22,34 +20,13 @@ public:
 class ConsoleProgressReport: public ProgressReportBase {
 public:
 
-	ConsoleProgressReport(float reportIntervall = 0.1f) :
-			m_reportIntervall(reportIntervall), m_lastReport(0.0f) {
-	}
+	ConsoleProgressReport(float reportIntervall = 0.1f);
+	
+	virtual ~ConsoleProgressReport() {  };
 
-	virtual ~ConsoleProgressReport() {
-	}
+	virtual void update(long long currentIndex, long long maxItems) ARTUS_CPP11_OVERRIDE;
 
-	virtual void update(long long currentIndex, long long maxItems)
-			ARTUS_CPP11_OVERRIDE
-			{
-		const float ratio = (float(currentIndex) / float(maxItems - 1));
-
-		if (((ratio - m_reportIntervall) > m_lastReport) ||
-		// be always sure to report we reached the last item
-				((currentIndex + 1) == (maxItems))) {
-
-			m_lastReport = ratio;
-			std::cout << "\r" << " -- Events : " << currentIndex << " / "
-					<< maxItems << " - " << (ratio * 100.0f) << " %                                          ";
-			std::cout.flush();
-		}
-	}
-
-	virtual void finish(long long currentIndex, long long maxItems)
-			ARTUS_CPP11_OVERRIDE
-			{
-		std::cout << std::endl;
-	}
+	virtual void finish(long long currentIndex, long long maxItems) ARTUS_CPP11_OVERRIDE;
 
 	// in percent
 	float m_reportIntervall;
