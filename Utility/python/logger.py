5405e34149241a6168443b5453abaa4dc15902c7
Thomas Mueller <tmuller@cern.ch>
2014-03-25 00:03:23 +0100

    logger: added option to output log into a file

--- Utility/python/logger.py
+++ Utility/python/logger.py
@@ -11,16 +11,24 @@ loggingParser = argparse.ArgumentParser(add_help=False)
 
 logginOptionsGroup = loggingParser.add_argument_group("Logging options")
 logginOptionsGroup.add_argument("--log-level", default="debug",
-                           choices=["debug", "info", "warning", "error", "critical"],
-                           help="Log level. [Default: debug]")
+                                choices=["debug", "info", "warning", "error", "critical"],
+                                help="Log level. [Default: debug]")
+logginOptionsGroup.add_argument("--log-file", help="Log file. [Default: None (output on console)]")
 
 def initLogger(argParserArgs=None, name=""):
 
 	logLevel = "debug"
+	logFile = None
 
 	if argParserArgs:
 		logLevel = argParserArgs.log_level
+		logFile = argParserArgs.log_file
 
-	logging.basicConfig(level=getattr(logging, logLevel.upper()),
+	logging.basicConfig(level=getattr(logging, logLevel.upper()),# fileName=logFile,
 	                    format="%(levelname)s: %(message)s")
+
+	if logFile:
+		loggingFileHandler = logging.FileHandler(logFile)
+		logging.getLogger().addHandler(loggingFileHandler)
+		logging.getLogger(__name__).info("Output will be logged to \"%s\"." % logFile)
 
