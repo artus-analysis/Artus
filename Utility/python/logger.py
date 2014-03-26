52cd0809b43a38be601f7660cb0a5631ea1d54fd
Max Fischer <mfischer@ekp.uni-karlsruhe.de>
2014-03-26 17:22:13 +0100

    added docstrings to logging module; initLogger: added support for multiple output destinations, implemented logger name functionality

--- Utility/python/logger.py
+++ Utility/python/logger.py
@@ -7,10 +7,96 @@ import os
 import subprocess
 import sys
 import threading
+'''
+Global logging module.
 
+loggingParser
+	argparse Parser for inclusion to main Parser as parent.
+
+print
+	redirector of print through the logging module.
+	/!\ this is a function and requires the import of print_function from __future__
+'''
 
 log = logging.getLogger(__name__)
 
+# add-on to be included in a main argument parser
+loggingParser = argparse.ArgumentParser(add_help=False)
+
+_loggingOptionsGroup = loggingParser.add_argument_group("Logging options")
+_loggingOptionsGroup.add_argument("--log-level",
+                                default = "debug",
+                                choices = ["debug", "info", "warning", "error", "critical"],
+                                help    = "Detail level of logging. [Default: %(default)s]")
+_loggingOptionsGroup.add_argument("--log-files",
+                               nargs    = '*',
+                               help     = "List of files to log to. [Default: %(default)s]")
+_loggingOptionsGroup.add_argument("--log-std",
+                                default = "NONE",
+                                choices = ["stdout", "stderr", "NONE"],
+                                help    = "List of streams to log to. [Default: %(default)s]")
+
+def initLogger(argParserArgs=None, name="", logLevel="debug", logFiles=[], logStreams = []):
+	'''
+	Initialize a logger for usage.
+
+	Optional:
+	  argParserArgs
+	                argparse namespace instance compatible with this module's parser
+	  name
+	                The name of the logger to initalize
+	  logLevel
+	                minimum logging level required for messages to be logged
+	  logFiles
+	                list of names of files to direct log output to
+	  logStreams [stderr, stdout]
+	                list of names of streams to direct log output to
+
+	Returns:
+	  _thisLogger
+	                initialized logger instance
+	'''
+	_thisLogger = logging.getLogger(name)
+	try:
+		logLevel = argParserArgs.log_level
+	except AttributeError:
+		pass
+	try:
+		_thisLogger.setLevel(int(logLevel))
+	except ValueError:
+		_thisLogger.setLevel(getattr(logging, logLevel.upper()))
+	### TODO: read LDFormat dynamically by name from initLogger attributes
+	levelDependentFormatter = LevelDependentFormatter(initLogger.LDFormatDefault)
+	try:
+		logStreams.extend(argParserArgs.log_std)
+	except AttributeError:
+		pass
+	try:
+		logFiles.extend(argParserArgs.log_files)
+	except AttributeError:
+		pass
+	loggingHandlers = []
+	for logStream in logStreams:
+		if logStream in 'sys.stdout':
+			loggingHandlers.append(logging.StreamHandler(sys.stdout))
+		elif logStream in 'sys.stderr':
+			loggingHandlers.append(logging.StreamHandler(sys.stderr))
+		elif logStream in 'NONE':
+			continue
+		else:
+			raise ValueError('Invalid stream designator for logging: %s' % logStream)
+	for logFile in logFiles:
+		loggingHandlers.append(logging.FileHandler(logFile))
+	for loggingHandler in loggingHandlers:
+		loggingHandler.setFormatter(levelDependentFormatter)
+		_thisLogger.addHandler(loggingHandler)
+	return _thisLogger
+# By-name-reference default dictionaries
+initLogger.LDFormatDefault = {
+		logging.DEBUG   : "%(levelname)s (%(filename)s.%(lineno)d): %(message)s",
+		logging.INFO	: "%(message)s",
+		logging.WARNING : "%(levelname)s: %(message)s"
+	}
 
 # Custom formatter with level-dependent formats
 class LevelDependentFormatter(logging.Formatter):
@@ -57,43 +143,6 @@ class LogPipe(threading.Thread):
 	def close(self):
 		os.close(self.fdWrite)
 
-
-loggingParser = argparse.ArgumentParser(add_help=False)
-
-logginOptionsGroup = loggingParser.add_argument_group("Logging options")
-logginOptionsGroup.add_argument("--log-level", default="debug",
-                                choices=["debug", "info", "warning", "error", "critical"],
-                                help="Log level. [Default: debug]")
-logginOptionsGroup.add_argument("--log-file", help="Log file. [Default: None (output on console)]")
-
-def initLogger(argParserArgs=None, name=""):
-
-	logLevel = "debug"
-	logFile = None
-
-	if argParserArgs:
-		logLevel = argParserArgs.log_level
-		logFile = argParserArgs.log_file
-
-	logging.root.setLevel(getattr(logging, logLevel.upper()))
-	
-	# level dependent formats
-	levelDependentFormatter = LevelDependentFormatter({
-		logging.DEBUG   : "%(levelname)s (%(filename)s.%(lineno)d): %(message)s",
-		logging.INFO	: "%(message)s",
-		logging.WARNING : "%(levelname)s: %(message)s"
-	})
-
-	# add output to stdout by default, and file output if requested
-	loggingHandlers = [logging.StreamHandler(sys.stdout)]
-	if logFile:
-		loggingHandlers.append(logging.FileHandler(logFile))
-	
-	for loggingHandler in loggingHandlers:
-		loggingHandler.setFormatter(levelDependentFormatter)
-		logging.root.addHandler(loggingHandler)
-	
-	log.info("Output will be logged to \"%s\"." % logFile)
 
 # wrapper for subprocess.call with connects with the logger and outputs everything
 def subprocessCall(args, **kwargs):
