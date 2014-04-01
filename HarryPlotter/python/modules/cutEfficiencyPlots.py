6a0330a707dcb57c2be851a84c8305de9fba924f
Thomas Mueller <tmuller@cern.ch>
2014-04-01 14:37:20 +0200

    plotting: added module for ROC plots

--- HarryPlotter/python/modules/cutEfficiencyPlots.py
+++ HarryPlotter/python/modules/cutEfficiencyPlots.py
@@ -2,6 +2,7 @@
 import array
 import copy
 import numpy
+import pprint
 import ROOT
 
 import Artus.HarryPlotter.plot1d as plot1d
@@ -54,7 +55,7 @@ class CutEfficiency(object):
 		return [self._histogram.GetBinLowEdge(xBin) for xBin in xrange(1, self._histogram.GetNbinsX()+2)]
 
 
-def cutEfficiencyPlot(plotdict, invertCut=False, yName="Efficiency"):
+def cutEffPlot(plotdict, invertCut=False, yName="Efficiency"):
 	""" Plot cut efficiencies for 1D histograms. """
 	
 	plot1d.get_root_histos(plotdict)
@@ -73,6 +74,63 @@ def cutEfficiencyPlot(plotdict, invertCut=False, yName="Efficiency"):
 	labels.add_labels(plotdict)
 	utils.save(plotdict)
 
-def invertedCutEfficiencyPlot(plotdict):
-	cutEfficiencyPlot(plotdict, invertCut=True, yName="1 - Efficiency")
+def invCutEffPlot(plotdict):
+	""" Plot inverted cut efficiencies for 1D histograms. """
+	cutEffPlot(plotdict, invertCut=True, yName="1 - Efficiency")
+
+
+def bkgRejVsSigEffPlot(plotdict, invertCutX=False, invertCutY=True,
+                       xName="Signal Efficiency", yName="Background Rejection"):
+	""" Plot background rejection vs. signal efficiency for 1D histograms. """
+	
+	plot1d.get_root_histos(plotdict)
+	
+	# determine indices for multiple plots
+	plots = {}
+	for index, group in enumerate(plotdict["groups"]):
+		plotLabel = group.lower().replace("sig", "").replace("bkg", "")
+		if "sig" in group.lower():
+			plots.setdefault(plotLabel, {})["sig"] = index
+		elif "bkg" in group.lower():
+			plots.setdefault(plotLabel, {})["bkg"] = index
+	
+	# create efficiency graph
+	for plotLabel, plotIndices in plots.items():
+		sigCutEfficiency = CutEfficiency(plotdict["roothistos"][plotIndices["sig"]])
+		bkgCutEfficiency = CutEfficiency(plotdict["roothistos"][plotIndices["bkg"]])
+		
+		efficiencyGraph = ROOT.TGraph(plotdict["roothistos"][plotIndices["sig"]].GetNbinsX()+1,
+		                              array.array("d", sigCutEfficiency.get_cut_efficiencies(invertCut=invertCutX)),
+		                              array.array("d", bkgCutEfficiency.get_cut_efficiencies(invertCut=invertCutY)))
+		
+		plotdict["roothistos"][plotIndices["sig"]] = efficiencyGraph
+	
+	# modify plotdict
+	utils.removeplots(plotdict, [plots[plotLabel]["bkg"] for plotLabel in plots])
+	
+	plotdict["xname"] = xName
+	plotdict["yname"] = yName
+	plotdict["xlims"] = [0.0, 1.0] # TODO
+	plot1d.plot1d_mpl(plotdict)
+	utils.setaxislimits(plotdict)
+	labels.add_labels(plotdict)
+	utils.save(plotdict)
+
+def bkgRejVsSigRejPlot(plotdict, invertCutX=True, invertCutY=True,
+                       xName="Signal Rejection", yName="Background Rejection"):
+	""" Plot background rejection vs. signal rejection for 1D histograms. """
+	bkgRejVsSigEffPlot(plotdict, invertCutX=invertCutX, invertCutY=invertCutY,
+                       xName=xName, yName=yName)
+
+def bkgEffVsSigRejPlot(plotdict, invertCutX=True, invertCutY=False,
+                       xName="Signal Rejection", yName="Background Efficiency"):
+	""" Plot background efficiency vs. signal rejection for 1D histograms. """
+	bkgRejVsSigEffPlot(plotdict, invertCutX=invertCutX, invertCutY=invertCutY,
+                       xName=xName, yName=yName)
+
+def bkgEffVsSigEffPlot(plotdict, invertCutX=False, invertCutY=False,
+                       xName="Signal Efficiency", yName="Background Efficiency"):
+	""" Plot background efficiency vs. signal efficiency for 1D histograms. """
+	bkgRejVsSigEffPlot(plotdict, invertCutX=invertCutX, invertCutY=invertCutY,
+                       xName=xName, yName=yName)
 
