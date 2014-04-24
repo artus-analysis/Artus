656a72c084c2e0882a9e86fddd6a4e572afa54c1
Thomas Mueller <tmuller@cern.ch>
2014-04-24 16:47:09 +0200

    python tools: added function to fine lists element matching regex

--- Utility/python/tools.py
+++ Utility/python/tools.py
@@ -5,8 +5,23 @@ import logging
 import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
+import re
 
-# flatten 2D list
-# return [1, 2, 3, 4, ...] for input [[1, 2], [3, 4, ...], ...]
+
 def flattenList(listOfLists):
+	"""
+	flatten 2D list
+	return [1, 2, 3, 4, ...] for input [[1, 2], [3, 4, ...], ...]
+	"""
 	return [item for subList in listOfLists for item in subList]
+
+def matchingItem(stringList, regexForItem):
+	"""
+	find/return item in string list that matches the given regex
+	returns None in case no matching item is found
+	"""
+	for item in stringList:
+		if re.search(regexForItem, item) != None:
+			return item
+	return None
+
