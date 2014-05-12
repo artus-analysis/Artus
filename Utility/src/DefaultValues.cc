26b80be05255db5cb50fd1dbcc903b3c5a2daeac
Thomas Mueller <tmuller@cern.ch>
2014-05-12 15:39:55 +0200

    bugfix

--- Utility/src/DefaultValues.cc
+++ Utility/src/DefaultValues.cc
@@ -4,5 +4,5 @@
 const float DefaultValues::UndefinedFloat = -999.0;
 const double DefaultValues::UndefinedDouble = -999.0;
 
-const double DefaultValues::EtaBorderEB = 1.479;
+const float DefaultValues::EtaBorderEB = 1.479;
 
