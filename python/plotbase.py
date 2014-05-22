a00a8be35f0b7cdbdc8ac2fd6e5106435418b57e
Thomas Mueller <tmuller@cern.ch>
2014-05-22 16:32:14 +0200

    plotting: added some plot settings from old HarryPlotter and implemented their usage for ROOT plots

--- python/plotbase.py
+++ python/plotbase.py
@@ -27,6 +27,55 @@ class PlotBase(processor.Processor):
 	def modify_argument_parser(self, parser):
 		processor.Processor.modify_argument_parser(self, parser)
 		
+		# axis settings
+		self.axis_options = parser.add_argument_group("Axis options")
+		self.axis_options.add_argument("--x-lims", type=float, nargs=2,
+		                               help="Lower and Upper limit for x-axis.")
+		self.axis_options.add_argument("--x-label", type=str,
+		                               help="X-axis label name.")
+		self.axis_options.add_argument("--x-log", action="store_true", default=False,
+		                               help="Logarithmic x-axis.")
+		self.axis_options.add_argument("--x-ticks", type=float, nargs="+",
+		                               help="Custom ticks for the X-axis")
+		#self.axis_options.add_argument("--xview", type=float, nargs=2, default=xview,
+		#                               help="Lower and Upper limit for x-axis viewing in the plot")
+		
+		self.axis_options.add_argument("--y-lims", type=float, nargs=2,
+		                               help="Lower and Upper limit for y-axis.")
+		self.axis_options.add_argument("--y-ratio-lims", type=float, nargs=2,
+		                               help="Lower and Upper limit for y-axis of a possible ratio subplot.")
+		self.axis_options.add_argument("--y-label", type=str, default="Events",
+		                               help="Y-axis label name. [Default: %(default)s]")
+		self.axis_options.add_argument("--y-log", action="store_true", default=False,
+		                               help="Logarithmic y-axis.")
+		self.axis_options.add_argument("--y-ticks", type=float, nargs="+",
+		                               help="Custom ticks for the Y-axis")
+		
+		self.axis_options.add_argument("--z-lims", type=float, nargs=2,
+		                               help="Lower and Upper limit for z-axis.")
+		self.axis_options.add_argument("--z-label", type=str, default="Events",
+		                               help="Z-axis label name. [Default: %(default)s]")
+		self.axis_options.add_argument("--z-log", action="store_true", default=False,
+		                               help="Logarithmic z-axis.")
+		self.axis_options.add_argument("--z-ticks", type=float, nargs="+",
+		                               help="Custom ticks for the Z-axis")
+
+		# plot labelling
+		self.labelling_options = parser.add_argument_group("Labelling options")
+		self.labelling_options.add_argument("-t", "--title", type=str,
+		                                    help="Plot title")
+		self.labelling_options.add_argument("-l", "--lumi", type=float,
+		                                    help="Luminosity for the given data in /fb.")
+		self.labelling_options.add_argument("-e", "--energy", type=int,
+		                                    help="Centre-of-mass energy for the given samples in TeV.")
+		self.labelling_options.add_argument("-A", "--author", type=str,
+		                                    help="author name of the plot")
+		self.labelling_options.add_argument("--date", type=str,
+		                                    help="Show the date in the top left corner. \"iso\" is YYYY-MM-DD, \"today\" is DD Mon YYYY and \"now\" is DD Mon YYYY HH:MM.")
+		self.labelling_options.add_argument("-E", "--event-number-label", action="store_true", default=False,
+		                                    help="Add event number label")
+		
+		# output settings
 		self.output_options = parser.add_argument_group("Output options")
 		self.output_options.add_argument("-o", "--output-dir", default="plots",
 		                                 help="Output directory for plots. [Default: %(default)s]")
@@ -37,6 +86,13 @@ class PlotBase(processor.Processor):
 	
 	def prepare_args(self, plotData):
 		processor.Processor.prepare_args(self, plotData)
+		
+		# construct labels from x/y/z expressions if not specified by user
+		for labelKey, expressionKey in zip(["x_label", "y_label", "z_label"],
+		                                   ["x_expressions", "y_expressions", "z_expressions"]):
+			if plotData.plotdict[labelKey] == None:
+				plotData.plotdict[labelKey] = reduce(lambda a, b: "%s, %s" % (str(a), str(b)), set(plotData.plotdict[expressionKey]))
+			if plotData.plotdict[labelKey] == None: plotData.plotdict[labelKey] = ""
 		
 		# create output directory if not exisiting
 		if not os.path.exists(plotData.plotdict["output_dir"]):
@@ -63,8 +119,10 @@ class PlotBase(processor.Processor):
 		processor.Processor.run(self, plotData)
 		
 		self.create_canvas(plotData)
+		self.prepare_histograms(plotData)
 		self.make_plots(plotData)
 		self.modify_axes(plotData)
+		self.add_labels(plotData)
 		self.add_texts(plotData)
 		self.save_canvas(plotData)
 	
@@ -73,11 +131,19 @@ class PlotBase(processor.Processor):
 		pass
 	
 	@abc.abstractmethod
+	def prepare_histograms(self, plotData):
+		pass
+	
+	@abc.abstractmethod
 	def make_plots(self, plotData):
 		pass
 	
 	@abc.abstractmethod
 	def modify_axes(self, plotData):
+		pass
+	
+	@abc.abstractmethod
+	def add_labels(self, plotData):
 		pass
 	
 	@abc.abstractmethod
--- python/plotroot.py
+++ python/plotroot.py
@@ -9,6 +9,7 @@ log = logging.getLogger(__name__)
 
 import os
 import ROOT
+import sys
 
 import Artus.HarryPlotter.plotbase as plotbase
 
@@ -27,14 +28,75 @@ class PlotRoot(plotbase.PlotBase):
 
 	def create_canvas(self, plotData):
 		self.canvas = ROOT.TCanvas()
+
+	def prepare_histograms(self, plotData):
+		pass
 	
 	def make_plots(self, plotData):
 		self.canvas.cd()
+		
+		self.x_min = sys.float_info.max
+		self.x_max = -sys.float_info.max
+		self.y_min = sys.float_info.max
+		self.y_max = -sys.float_info.max
+		self.z_min = sys.float_info.max
+		self.z_max = -sys.float_info.max
+		
 		for index, root_histogram in enumerate(plotData.plotdict["root_histos"].values()):
 			option = "" if index == 0 else "same"
 			root_histogram.Draw(option)
+			if index == 0: self.first_plotted_histogram = root_histogram
+			
+			# trace min. and max. values of axes
+			if root_histogram.GetXaxis().GetXmin() < self.x_min: self.x_min = root_histogram.GetXaxis().GetXmin()
+			if root_histogram.GetXaxis().GetXmax() > self.x_max: self.x_max = root_histogram.GetXaxis().GetXmax()
+			if root_histogram.GetDimension() > 1:
+				if root_histogram.GetYaxis().GetXmin() < self.y_min: self.y_min = root_histogram.GetYaxis().GetXmin()
+				if root_histogram.GetYaxis().GetXmax() > self.y_max: self.y_max = root_histogram.GetYaxis().GetXmax()
+			else:
+				if root_histogram.GetMinimum() < self.y_min: self.y_min = root_histogram.GetMinimum()
+				if root_histogram.GetMaximum() > self.y_max: self.y_max = root_histogram.GetMaximum()
+			if root_histogram.GetDimension() > 2:
+				if root_histogram.GetZaxis().GetXmin() < self.z_min: self.z_min = root_histogram.GetZaxis().GetXmin()
+				if root_histogram.GetZaxis().GetXmax() > self.z_max: self.z_max = root_histogram.GetZaxis().GetXmax()
+			else:
+				if root_histogram.GetMinimum() < self.z_min: self.z_min = root_histogram.GetMinimum()
+				if root_histogram.GetMaximum() > self.z_max: self.z_max = root_histogram.GetMaximum()
 	
 	def modify_axes(self, plotData):
+	
+		# axis labels
+		self.first_plotted_histogram.GetXaxis().SetTitle(plotData.plotdict["x_label"])
+		self.first_plotted_histogram.GetYaxis().SetTitle(plotData.plotdict["y_label"])
+		self.first_plotted_histogram.GetZaxis().SetTitle(plotData.plotdict["z_label"])
+	
+		# logaritmic axis
+		if plotData.plotdict["x_log"]: self.canvas.SetLogx()
+		if plotData.plotdict["y_log"]: self.canvas.SetLogy()
+		if plotData.plotdict["z_log"]: self.canvas.SetLogz()
+	
+		# axis limits
+		if plotData.plotdict["x_lims"] == None:
+			self.first_plotted_histogram.GetXaxis().SetRangeUser(self.x_min,
+			                                                     self.x_max)
+		else:
+			self.first_plotted_histogram.GetXaxis().SetRangeUser(plotData.plotdict["x_lims"][0],
+			                                                     plotData.plotdict["x_lims"][1])
+		if plotData.plotdict["y_lims"] == None:
+			self.first_plotted_histogram.GetYaxis().SetRangeUser(self.y_min,
+			                                                     self.y_max)
+		else:
+			self.first_plotted_histogram.GetYaxis().SetRangeUser(plotData.plotdict["y_lims"][0],
+			                                                     plotData.plotdict["y_lims"][1])
+		if plotData.plotdict["z_lims"] == None:
+			self.first_plotted_histogram.GetZaxis().SetRangeUser(self.z_min,
+			                                                     self.z_max)
+		else:
+			self.first_plotted_histogram.GetZaxis().SetRangeUser(plotData.plotdict["z_lims"][0],
+			                                                     plotData.plotdict["z_lims"][1])
+		
+		
+	def add_labels(self, plotData):
 		pass
 	
 	def add_texts(self, plotData):
