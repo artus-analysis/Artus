16846df121ef2d8be35e8e51a7695356376ca2f7
Thomas Mueller <tmuller@cern.ch>
2014-05-23 16:19:36 +0200

    plotting: changed nagrs=* to nargs=+

--- python/inputbase.py
+++ python/inputbase.py
@@ -19,23 +19,23 @@ class InputBase(processor.Processor):
 	
 	def modify_argument_parser(self, parser):
 		self.input_options = parser.add_argument_group("Input options")
-		self.input_options.add_argument("-i", "--files", type=str, nargs="*",
+		self.input_options.add_argument("-i", "--files", type=str, nargs="+",
 		                                 help="Input root file(s).")
-		self.input_options.add_argument("--nicks", type=str, nargs="*",
+		self.input_options.add_argument("--nicks", type=str, nargs="+",
 		                                 help="Defines nick names for inputs. Inputs with the same nick name will be merged. By default, every input gets a unique nick name.")
 
-		self.input_options.add_argument("-x", "--x-expressions", type=str, nargs="*",
+		self.input_options.add_argument("-x", "--x-expressions", type=str, nargs="+",
 		                                help="x-axis variable expression(s)")
-		self.input_options.add_argument("-y", "--y-expressions", type=str, nargs="*",
+		self.input_options.add_argument("-y", "--y-expressions", type=str, nargs="+",
 		                                help="y-axis variable expression(s)")
-		self.input_options.add_argument("-z", "--z-expressions", type=str, nargs="*",
+		self.input_options.add_argument("-z", "--z-expressions", type=str, nargs="+",
 		                                help="z-axis variable expression(s)")
 		self.input_options.add_argument("-w", "--weights", type=str, nargs="+", default="1.0",
 		                                help="Weight (cut) expression(s). [Default: %(default)s]")
 
 		self.input_options.add_argument("--type", type=str, # TODO: default
 		                                help="Type of plot.")
-		self.input_options.add_argument("-n", "--norm-references", type=str, nargs="*",
+		self.input_options.add_argument("-n", "--norm-references", type=str, nargs="+",
 		                                help="Nick names of inputs or float values to normalise to. One parameter can contain a whitespace separated list of multiple nick names, that are summed up before the normalisation.")
 	
 	def prepare_args(self, plotData):
