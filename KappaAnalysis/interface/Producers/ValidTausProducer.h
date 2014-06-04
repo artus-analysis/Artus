9954ca2d1af5a3fcfbfb9595ac676dd56159b949
Thomas Mueller <tmuller@cern.ch>
2014-06-05 00:39:58 +0200

    ValidTausProducer: added kinematic cuts (configurable)

--- KappaAnalysis/interface/Producers/ValidTausProducer.h
+++ KappaAnalysis/interface/Producers/ValidTausProducer.h
@@ -1,6 +1,10 @@
 
 #pragma once
 
+#include <algorithm>
+
+#include <boost/regex.hpp>
+
 #include "Kappa/DataFormats/interface/Kappa.h"
 
 #include "Artus/Core/interface/ProducerBase.h"
@@ -35,6 +39,11 @@ public:
 	
 		// parse additional config tags
 		discriminators = Utility::ParseVectorToMap(globalSettings.GetTauDiscriminators());
+		
+		lowerPtCutsByIndex = Utility::ParseMapTypes<size_t, float>(Utility::ParseVectorToMap(globalSettings.GetTauLowerPtCuts()),
+		                                                           lowerPtCutsByHltName);
+		upperAbsEtaCutsByIndex = Utility::ParseMapTypes<size_t, float>(Utility::ParseVectorToMap(globalSettings.GetTauLowerPtCuts()),
+		                                                               upperAbsEtaCutsByHltName);
 	}
 	
 	virtual void InitLocal(setting_type const& settings)  ARTUS_CPP11_OVERRIDE
@@ -43,6 +52,11 @@ public:
 	
 		// parse additional config tags
 		discriminators = Utility::ParseVectorToMap(settings.GetTauDiscriminators());
+		
+		lowerPtCutsByIndex = Utility::ParseMapTypes<size_t, float>(Utility::ParseVectorToMap(settings.GetTauLowerPtCuts()),
+		                                                           lowerPtCutsByHltName);
+		upperAbsEtaCutsByIndex = Utility::ParseMapTypes<size_t, float>(Utility::ParseVectorToMap(settings.GetTauLowerPtCuts()),
+		                                                               upperAbsEtaCutsByHltName);
 	}
 
 	virtual void ProduceGlobal(event_type const& event,
@@ -83,6 +97,9 @@ protected:
 				validTau = validTau && tau->hasID(*discriminatorName, event.m_tauDiscriminatorMetadata);
 			}
 			
+			// kinematic cuts
+			validTau = validTau && PassKinematicCuts(&(*tau), event, product);
+			
 			// check possible analysis-specific criteria
 			validTau = validTau && AdditionalCriteria(&(*tau), event, product);
 			
@@ -103,5 +120,70 @@ protected:
 
 private:
 	std::map<std::string, std::vector<std::string> > discriminators;
+	
+	std::map<size_t, std::vector<float> > lowerPtCutsByIndex;
+	std::map<std::string, std::vector<float> > lowerPtCutsByHltName;
+	std::map<size_t, std::vector<float> > upperAbsEtaCutsByIndex;
+	std::map<std::string, std::vector<float> > upperAbsEtaCutsByHltName;
+	
+	bool PassKinematicCuts(KDataPFTau* tau, event_type const& event, product_type& product) const
+	{
+		bool validTau = true;
+		
+		for (std::map<size_t, std::vector<float> >::const_iterator lowerPtCutByIndex = lowerPtCutsByIndex.begin();
+		     lowerPtCutByIndex != lowerPtCutsByIndex.end() && validTau; ++lowerPtCutByIndex)
+		{
+			LOG(INFO) << tau->p4.Pt() << ", " << *std::max_element(lowerPtCutByIndex->second.begin(), lowerPtCutByIndex->second.end());
+			if ((tau->p4.Pt() < *std::max_element(lowerPtCutByIndex->second.begin(), lowerPtCutByIndex->second.end()))
+			    && (lowerPtCutByIndex->first == product.m_validTaus.size()))
+			{
+				validTau = false;
+			}
+		}
+		
+		for (std::map<size_t, std::vector<float> >::const_iterator upperAbsEtaCutByIndex = upperAbsEtaCutsByIndex.begin();
+		     upperAbsEtaCutByIndex != upperAbsEtaCutsByIndex.end() && validTau; ++upperAbsEtaCutByIndex)
+		{
+			if ((std::abs(tau->p4.Eta()) > *std::min_element(upperAbsEtaCutByIndex->second.begin(), upperAbsEtaCutByIndex->second.end()))
+			    && (upperAbsEtaCutByIndex->first == product.m_validTaus.size()))
+			{
+				validTau = false;
+			}
+		}
+		
+		for (std::map<std::string, std::vector<float> >::const_iterator lowerPtCutByHltName = lowerPtCutsByHltName.begin();
+		     lowerPtCutByHltName != lowerPtCutsByHltName.end() && validTau; ++lowerPtCutByHltName)
+		{
+			if ((tau->p4.Pt() < *std::max_element(lowerPtCutByHltName->second.begin(), lowerPtCutByHltName->second.end()))
+			    &&
+			    (
+			    	(lowerPtCutByHltName->first == "default")
+			    	||
+			    	boost::regex_search(product.selectedHltName, boost::regex(lowerPtCutByHltName->first, boost::regex::icase | boost::regex::extended))
+			    )
+			   )
+			{
+				validTau = false;
+			}
+		}
+		
+		for (std::map<std::string, std::vector<float> >::const_iterator upperAbsEtaCutByHltName = upperAbsEtaCutsByHltName.begin();
+		     upperAbsEtaCutByHltName != upperAbsEtaCutsByHltName.end() && validTau; ++upperAbsEtaCutByHltName)
+		{
+			if ((std::abs(tau->p4.Eta()) > *std::min_element(upperAbsEtaCutByHltName->second.begin(), upperAbsEtaCutByHltName->second.end()))
+			    &&
+			    (
+			    	(upperAbsEtaCutByHltName->first == "default")
+			    	||
+			    	boost::regex_search(product.selectedHltName, boost::regex(upperAbsEtaCutByHltName->first, boost::regex::icase | boost::regex::extended))
+			    )
+			   )
+			{
+				validTau = false;
+			}
+		}
+
+		return validTau;
+	}
 };
 
