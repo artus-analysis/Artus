6e193ed3584d3d5252c9e7228d938a9c3e790430
Thomas Mueller <tmuller@cern.ch>
2014-06-06 17:02:31 +0200

    plotting: improved binning functionalities, part 2

--- python/inputroot.py
+++ python/inputroot.py
@@ -42,14 +42,12 @@ class InputRoot(inputbase.InputBase):
 	def run(self, plotData):
 		inputbase.InputBase.run(self, plotData)
 		
-		x_bins = copy.deepcopy(plotData.plotdict["x_bins"])
-		y_bins = copy.deepcopy(plotData.plotdict["y_bins"])
-		z_bins = copy.deepcopy(plotData.plotdict["z_bins"])
+		root_tools = roottools.RootTools()
 		for root_files, folders, x_expression, y_expression, z_expression, weight, nick in zip(*
 			[plotData.plotdict[key] for key in ["files", "folders", "x_expressions", "y_expressions", "z_expressions", "weights", "nicks"]]):
 			
 			# check wether to read from tree or directly from histograms
-			root_object_type = roottools.check_type(root_files, folders)
+			root_object_type = roottools.RootTools.check_type(root_files, folders)
 			root_histogram = None
 			
 			if root_object_type == ROOT.TTree:
@@ -57,14 +55,17 @@ class InputRoot(inputbase.InputBase):
 				                                  y_expression + ":" if y_expression else "",
 				                                  x_expression)
 				
-				root_histogram, x_bins, y_bins, z_bins = roottools.histogram_from_tree(root_files, folders, variable_expression,
-				                                                                       x_bins=x_bins, y_bins=y_bins, z_bins=z_bins,
-				                                                                       weight_selection=weight, option="", name=None)
+				root_histogram = root_tools.histogram_from_tree(root_files, folders,
+				                                                x_expression, y_expression, z_expression,
+				                                                x_bins=plotData.plotdict["x_bins"],
+				                                                y_bins=plotData.plotdict["y_bins"],
+				                                                z_bins=plotData.plotdict["z_bins"],
+				                                                weight_selection=weight, option="", name=None)
 				
 			elif root_object_type == ROOT.TH1:
 				root_objects = [os.path.join(folder, x_expression) for folder in folders]
 				
-				root_histogram = roottools.histogram_from_file(root_files, root_objects, name=None)
+				root_histogram = roottools.RootTools.histogram_from_file(root_files, root_objects, name=None)
 			
 			# save histogram in plotData merging histograms with same nick names
 			if nick in plotData.plotdict.setdefault("root_histos", {}):
--- python/plotbase.py
+++ python/plotbase.py
@@ -189,10 +189,10 @@ class PlotBase(processor.Processor):
 			for numerator_nicks, denominator_nicks in zip(plotData.plotdict["ratio_num"],
 			                                              plotData.plotdict["ratio_denom"]):
 				name = hashlib.md5("_".join(numerator_nicks+denominator_nicks)).hexdigest()
-				numerator_histogram = roottools.add_root_histograms(*[plotData.plotdict["root_histos"][nick] for nick in numerator_nicks],
-				                                                    name=name+"_numerator")
-				denominator_histogram = roottools.add_root_histograms(*[plotData.plotdict["root_histos"][nick] for nick in denominator_nicks],
-				                                                      name=name+"_denominator")
+				numerator_histogram = roottools.RootTools.add_root_histograms(*[plotData.plotdict["root_histos"][nick] for nick in numerator_nicks],
+				                                                              name=name+"_numerator")
+				denominator_histogram = roottools.RootTools.add_root_histograms(*[plotData.plotdict["root_histos"][nick] for nick in denominator_nicks],
+				                                                                name=name+"_denominator")
 				ratio_histogram = numerator_histogram.Clone(name + "_ratio")
 				ratio_histogram.Divide(denominator_histogram)
 				plotData.plotdict.setdefault("root_ratio_histos", []).append(ratio_histogram)
