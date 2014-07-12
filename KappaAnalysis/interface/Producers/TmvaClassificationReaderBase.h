0108b8902cd8c4e42e9dd5f6fa942c52ff67ccc4
Thomas Mueller <tmuller@cern.ch>
2014-07-12 15:06:09 +0200

    TmvaClassificationReaderBase: added abstract wrapper class for TMVA::Reader for classifications

---
+++ KappaAnalysis/interface/Producers/TmvaClassificationReaderBase.h
@@ -0,0 +1,97 @@
+
+#pragma once
+
+#include "TMVA/Reader.h"
+
+#include "Artus/Core/interface/ProducerBase.h"
+#include "Artus/Consumer/interface/LambdaNtupleConsumer.h"
+
+
+/**
+   \brief Abstract producer base for reading/applying TMVA classifications.
+*/
+template<class TTypes>
+class TmvaClassificationReaderBase: public ProducerBase<TTypes>
+{
+
+public:
+
+	typedef typename TTypes::event_type event_type;
+	typedef typename TTypes::product_type product_type;
+	typedef typename TTypes::setting_type setting_type;
+	
+	typedef std::function<float(event_type const&, product_type const&)> float_extractor_lambda;
+	
+	TmvaClassificationReaderBase(std::vector<std::string>& (setting_type::*GetTmvaInputQuantities)(void) const,
+	                             std::vector<std::string>& (setting_type::*GetTmvaMethods)(void) const,
+	                             std::vector<std::string>& (setting_type::*GetTmvaWeights)(void) const,
+	                             std::vector<double> product_type::*mvaOutputs) :
+		ProducerBase<TTypes>(),
+		GetTmvaInputQuantities(GetTmvaInputQuantities),
+		GetTmvaMethods(GetTmvaMethods),
+		GetTmvaWeights(GetTmvaWeights),
+		m_mvaOutputsMember(mvaOutputs)
+	{
+	}
+
+	virtual void Init(setting_type const& settings) ARTUS_CPP11_OVERRIDE
+	{
+		ProducerBase<TTypes>::Init(settings);
+		
+		// construct extractors vector
+		m_inputExtractors.clear();
+		for (std::vector<std::string>::const_iterator quantity = (settings.*GetTmvaInputQuantities)().begin();
+		     quantity != (settings.*GetTmvaInputQuantities)().end(); ++quantity)
+		{
+			m_inputExtractors.push_back(SafeMap::Get(LambdaNtupleConsumer<TTypes>::Quantities, *quantity));
+		}
+		
+		// loading TMVA weight files
+		assert((settings.*GetTmvaMethods)().size() == (settings.*GetTmvaWeights)().size());
+		LOG(DEBUG) << "\tLoading TMVA weight files...";
+		for (size_t mvaMethodIndex = 0; mvaMethodIndex < (settings.*GetTmvaMethods)().size(); ++mvaMethodIndex)
+		{
+			std::string tmvaMethod = (settings.*GetTmvaMethods)()[mvaMethodIndex];
+			std::string tmvaWeights = (settings.*GetTmvaWeights)()[mvaMethodIndex];
+			LOG(DEBUG) << "\t\tmethod: " << tmvaMethod << ", weight file: " << tmvaWeights;
+			tmvaReader.BookMVA(tmvaMethod, tmvaWeights);
+		}
+		
+	}
+
+	virtual void Produce(event_type const& event, product_type& product,
+	                     setting_type const& settings) const ARTUS_CPP11_OVERRIDE
+	{
+		// construct and fill input vector
+		std::vector<double> tmvaInputs(m_inputExtractors.size());
+		size_t inputQuantityIndex = 0;
+		for(typename std::vector<float_extractor_lambda>::const_iterator inputExtractor = m_inputExtractors.begin();
+		    inputExtractor != m_inputExtractors.end(); ++inputExtractor)
+		{
+			tmvaInputs[inputQuantityIndex] = (*inputExtractor)(event, product);
+			++inputQuantityIndex;
+		}
+		
+		// retrieve MVA outputs
+		(product.*m_mvaOutputsMember) = std::vector<double>((settings.*GetTmvaMethods)().size());
+		size_t mvaMethodIndex = 0;
+		for (std::vector<std::string>::const_iterator tmvaMethod = (settings.*GetTmvaMethods)().begin();
+		     tmvaMethod != (settings.*GetTmvaMethods)().end(); ++tmvaMethod)
+		{
+			(product.*m_mvaOutputsMember)[mvaMethodIndex] = tmvaReader.EvaluateMVA(tmvaInputs, tmvaMethod->c_str());
+			++mvaMethodIndex;
+		}
+	}
+
+
+private:
+	std::vector<std::string>& (setting_type::*GetTmvaInputQuantities)(void) const;
+	std::vector<std::string>& (setting_type::*GetTmvaMethods)(void) const;
+	std::vector<std::string>& (setting_type::*GetTmvaWeights)(void) const;
+	std::vector<double> product_type::*m_mvaOutputsMember;
+	
+	std::vector<float_extractor_lambda> m_inputExtractors;
+	mutable TMVA::Reader tmvaReader;
+
+};
+
