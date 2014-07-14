f71c345284343b7541abe546008aca4f483cb1d5
rfriese <raphael.friese@cern.ch>
2014-07-14 18:01:57 +0200

    first version of plotmpl.py that actually can create a plot. Basically all features are still missing

--- python/plotmpl.py
+++ python/plotmpl.py
@@ -2,13 +2,17 @@
 
 """
 """
-
+import os
+import pprint
 import logging
 import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
+
 import Artus.HarryPlotter.plotbase as plotbase
 
+import Artus.HarryPlotter.tools.mplconvert as mplconvert
+import matplotlib.pyplot as plt
 
 class PlotMpl(plotbase.PlotBase):
 	def __init__(self):
@@ -24,4 +28,39 @@ class PlotMpl(plotbase.PlotBase):
 	
 	def run(self, plotData):
 		super(PlotMpl, self).run(plotData)
+	
+	def create_canvas(self, plotData):
+		if plotData.plotdict["ratio"]:
+			self.fig = plt.figure(figsize=[7, 7])
+			self.ax1 = self.fig.add_subplot(111, position = [0.13, 0.37, 0.83, 0.58])
+			self.ax2 = self.fig.add_subplot(111, position = [0.13, 0.12, 0.83, 0.22], sharex=self.ax1)
+			#plotdict['ratiosubplotaxes'] = ax2 # needed?
+		else:
+			self.fig = plt.figure(figsize=[7, 7])
+			self.ax = self.fig.add_subplot(111)
 
+	def prepare_histograms(self, plotData):
+		# create root histograms with TTree:Project
+
+		# normalize if wanted
+		# call functions from plotroot?
+		pass
+
+	def make_plots(self, plotData):
+		print "in MakePlots"
+		print plotData.plotdict["root_histos"]
+		pprint.pprint(plotData.plotdict)
+		plotData.plotdict["root_histos"]
+		for root_histogram in plotData.plotdict["root_histos"].values(): # todo: filename, color
+			mpl_histogram = mplconvert.root2histo(root_histogram, "someFilename", 1)
+		label = "dummylabel"
+		color = "blue"
+		marker = "-"
+		self.ax.errorbar(mpl_histogram.xc, mpl_histogram.y, mpl_histogram.yerr, color=color, fmt=marker, capsize=0, label=label, zorder=10, drawstyle='default')
+
+
+	def save_canvas(self, plotData):
+		for plot_format in plotData.plotdict["formats"]:
+			filename = os.path.join(plotData.plotdict["output_dir"],
+				                     plotData.plotdict["filename"]+"."+plot_format)
+			self.fig.savefig(filename)
