a16dcd4b25b2a7d515c59fe4a211d83c0e426273
Georg <sieber@cern.ch>
2014-09-03 18:50:24 +0200

    deleted HarryPlotter folder from master. It was moved to separated repository https://github.com/artus-analysis/HarryPlotter
    Please clone the new repository HarryPlotter.

--- HarryPlotter/scripts/ini_harry_cmssw.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This line fixes a bug in CMSSW6. The dateutils are not present there. This uses the dateutils from python 2.6
-export PYTHONPATH=$PYTHONPATH:${CMSSW_RELEASE_BASE}/../../../../slc5_amd64_gcc462/external/py2-matplotlib/1.0.1/lib/python2.6/site-packages/
-
