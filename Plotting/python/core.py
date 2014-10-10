32fe25ebd1b231114ef32be538806c03e9bfec36
Thomas Mueller <tmuller@cern.ch>
2014-10-10 18:30:35 +0200

    core.py: use parser.set_defaults instead of args.update, to be tested

--- Plotting/python/core.py
+++ Plotting/python/core.py
@@ -61,8 +61,10 @@ class HarryCore(object):
 		args = vars(args)
 		json_default_initialisation = None
 		if args["json_defaults"] != None:
-			json_default_initialisation = args["json_defaults"][0]
-			args.update({k:v for k,v in json_tools.JsonDict(args["json_defaults"]).iteritems() if (k in args.keys() and v)})
+			json_default_initialisation = args["json_defaults"]
+			json_defaults = json_tools.JsonDict(args["json_defaults"]).doIncludes().doComments()
+			parser.set_defaults(**json_defaults)
+			#args.update({k:v for k,v in json_defaults.iteritems() if (k in args.keys() and v)})
 
 		# replace 'json_defaults' from imported json file to actual name of imported json file
 		if json_default_initialisation != None:
