e2aa94f03de5d968549c53e3361b26031e366085
Benjamin Treiber <benjamin.treiber@gmail.com>
2014-10-23 10:21:40 +0200

    Changes of function and class names
    Changes of function and class names accord. to Kappa electronunification branch

--- KappaAnalysis/interface/KappaEvent.h
+++ KappaAnalysis/interface/KappaEvent.h
@@ -18,7 +18,7 @@ public:
 
 	/// pointer to electron collection
 	KDataElectrons* m_electrons = 0;
-    KElectronMvaIdMetadata* m_electronMvaIdMetadata = 0;
+    KElectronIdMetadata* m_electronIdMetadata = 0;
 
 	/// pointer to muon collection
 	KDataMuons* m_muons = 0;
