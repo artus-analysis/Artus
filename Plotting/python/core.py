727ea9562dc402bc0c71b0715fe4bab99af1be5c
Georg Sieber <sieber@cern.ch>
2014-11-09 17:08:52 +0100

    Renamed scalebincontentsbywidth.py to normalization.py and added more Normalization modules.

--- Plotting/python/core.py
+++ Plotting/python/core.py
@@ -28,7 +28,8 @@ import HarryPlotter.Plotting.modules.functionplot as functionplot
 import HarryPlotter.Plotting.modules.shapeyieldmerge as shapeyieldmerge
 import HarryPlotter.Plotting.modules.extrapolationfactor as extrapolationfactor
 import HarryPlotter.Plotting.modules.binerrorsofemptybins as binerrorsofemptybins
-import HarryPlotter.Plotting.modules.scalebincontentsbywidth as scalebincontentsbywidth
+from HarryPlotter.Plotting.modules.normalization import NormalizeByBinWidth, NormalizeToUnity, NormalizeToFirstHisto
+
 import HarryPlotter.Plotting.modules.correctnegativebincontents as correctnegativebincontents
 import HarryPlotter.Plotting.modules.printinfos as printinfos
 
@@ -52,7 +53,9 @@ class HarryCore(object):
 			shapeyieldmerge.ShapeYieldMerge.name() : shapeyieldmerge.ShapeYieldMerge(),
 			extrapolationfactor.ExtrapolationFactor.name() : extrapolationfactor.ExtrapolationFactor(),
 			binerrorsofemptybins.BinErrorsOfEmptyBins.name() : binerrorsofemptybins.BinErrorsOfEmptyBins(),
-			scalebincontentsbywidth.ScaleBinContentsByWidth.name() : scalebincontentsbywidth.ScaleBinContentsByWidth(),
+			NormalizeByBinWidth.name(): NormalizeByBinWidth(),
+			NormalizeToUnity.name(): NormalizeToUnity(),
+			NormalizeToFirstHisto.name(): NormalizeToFirstHisto(),
 			correctnegativebincontents.CorrectNegativeBinContents.name() : correctnegativebincontents.CorrectNegativeBinContents(),
 			printinfos.PrintInfos.name() : printinfos.PrintInfos(),
 			plotroot.PlotRoot.name() : plotroot.PlotRoot(),
---
+++ Plotting/python/modules/normalization.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import HarryPlotter.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import ROOT
+
+import HarryPlotter.Plotting.analysisbase as analysisbase
+
+
+class NormalizeByBinWidth(analysisbase.AnalysisBase):
+	"""Normalize all histograms by bin width."""
+	def __init__(self):
+		super(NormalizeByBinWidth, self).__init__()
+
+	def run(self, plotData=None):
+		super(NormalizeByBinWidth, self).run(plotData)
+		
+		for nick, root_histogram in plotData.plotdict["root_objects"].iteritems():
+			if isinstance(root_histogram, ROOT.TH1):
+				root_histogram.Sumw2()
+				root_histogram.Scale(1.0, "width")
+
+class NormalizeToUnity(analysisbase.AnalysisBase):
+	"""Normalize all histograms to unity."""
+	def __init__(self):
+		super(NormalizeToUnity, self).__init__()
+
+	def run(self, plotData=None):
+		super(NormalizeToUnity, self).run(plotData)
+		
+		for nick, root_histogram in plotData.plotdict["root_objects"].iteritems():
+			if isinstance(root_histogram, ROOT.TH1):
+				root_histogram.Sumw2()
+				root_histogram.Scale(1.0 / root_histogram.Integral())
+
+class NormalizeToFirstHisto(analysisbase.AnalysisBase):
+	"""Normalize histograms to first histogram."""
+	def __init__(self):
+		super(NormalizeToFirstHisto, self).__init__()
+
+	def run(self, plotData=None):
+		super(NormalizeToFirstHisto, self).run(plotData)
+		refhisto_int = plotData.plotdict["root_objects"][plotData.plotdict["nicks"][0]].Integral()
+
+		for nick, root_histogram in plotData.plotdict["root_objects"].iteritems():
+			if isinstance(root_histogram, ROOT.TH1):
+				root_histogram.Sumw2()
+				root_histogram.Scale(refhisto_int / root_histogram.Integral())
+
