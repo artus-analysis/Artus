3fcf290534425096683fa2445282e49c76a391fc
Thomas Mueller <tmuller@cern.ch>
2014-11-10 23:55:23 +0100

    LambdaNtupleConsumer: added option to add non-float-type quantities, breakes code (needs renaming AddQuantity to AddFloatQuantity)

--- Consumer/interface/LambdaNtupleConsumer.h
+++ Consumer/interface/LambdaNtupleConsumer.h
@@ -24,8 +24,12 @@
 
 
 class LambdaNtupleQuantities {
+
 public:
-static std::map<std::string, std::function<float(EventBase const&, ProductBase const& ) >> CommonQuantities ;
+	static std::map<std::string, std::function<bool(EventBase const&, ProductBase const& ) >> CommonBoolQuantities;
+	static std::map<std::string, std::function<int(EventBase const&, ProductBase const& ) >> CommonIntQuantities;
+	static std::map<std::string, std::function<float(EventBase const&, ProductBase const& ) >> CommonFloatQuantities;
+	static std::map<std::string, std::function<double(EventBase const&, ProductBase const& ) >> CommonDoubleQuantities;
 
 };
 
@@ -38,40 +42,128 @@ public:
 	typedef typename TTypes::product_type product_type;
 	typedef typename TTypes::setting_type setting_type;
 
-	typedef std::function<float(event_type const&, product_type const&)> float_extractor_lambda;
+	//typedef std::function<bool(EventBase const&, ProductBase const&)> bool_extractor_lambda_base;
+	typedef std::function<int(EventBase const&, ProductBase const&)> int_extractor_lambda_base;
 	typedef std::function<float(EventBase const&, ProductBase const&)> float_extractor_lambda_base;
+	typedef std::function<double(EventBase const&, ProductBase const&)> double_extractor_lambda_base;
 
 	
-	static void AddQuantity ( std::string const& name, float_extractor_lambda lmbToAdd ) {
-		LambdaNtupleQuantities::CommonQuantities[ name ] = [lmbToAdd] ( EventBase const& ev, ProductBase const& pd ) -> float { 
-			auto const& specEv = static_cast< event_type const& > ( ev );
-			auto const& specPd = static_cast< product_type const& > ( pd );
-			return lmbToAdd( specEv, specPd );
+	/*static void AddBoolQuantity(std::string const& name,
+	                            std::function<bool(event_type const&, product_type const&)> valueExtractor)
+	{
+		LambdaNtupleQuantities::CommonBoolQuantities[name] = [valueExtractor](EventBase const& ev, ProductBase const& pd) -> bool
+		{ 
+			auto const& specEv = static_cast<event_type const&>(ev);
+			auto const& specPd = static_cast<product_type const&>(pd);
+			return valueExtractor(specEv, specPd);
+		};
+	}*/
+	static void AddIntQuantity(std::string const& name,
+	                           std::function<int(event_type const&, product_type const&)> valueExtractor)
+	{
+		LambdaNtupleQuantities::CommonIntQuantities[name] = [valueExtractor](EventBase const& ev, ProductBase const& pd) -> int
+		{ 
+			auto const& specEv = static_cast<event_type const&>(ev);
+			auto const& specPd = static_cast<product_type const&>(pd);
+			return valueExtractor(specEv, specPd);
+		};
+	}
+	static void AddFloatQuantity(std::string const& name,
+	                             std::function<float(event_type const&, product_type const&)> valueExtractor)
+	{
+		LambdaNtupleQuantities::CommonFloatQuantities[name] = [valueExtractor](EventBase const& ev, ProductBase const& pd) -> float
+		{ 
+			auto const& specEv = static_cast<event_type const&>(ev);
+			auto const& specPd = static_cast<product_type const&>(pd);
+			return valueExtractor(specEv, specPd);
+		};
+	}
+	static void AddDoubleQuantity(std::string const& name,
+	                        std::function<double(event_type const&, product_type const&)> valueExtractor)
+	{
+		LambdaNtupleQuantities::CommonDoubleQuantities[name] = [valueExtractor](EventBase const& ev, ProductBase const& pd) -> double
+		{ 
+			auto const& specEv = static_cast<event_type const&>(ev);
+			auto const& specPd = static_cast<product_type const&>(pd);
+			return valueExtractor(specEv, specPd);
 		};
 	}
 
 	static std::map<std::string, std::function<float(EventBase const&, ProductBase const& ) >> & GetQuantities () {
-		return LambdaNtupleQuantities::CommonQuantities;
+		return LambdaNtupleQuantities::CommonFloatQuantities;
 	}
 
 	virtual void Init(setting_type const& settings) ARTUS_CPP11_OVERRIDE {
 		ConsumerBase<TTypes>::Init(settings);
 		
-		// construct tree and value extractors
-		m_tree = new TTree("ntuple", ("Tree for Pipeline \"" + settings.GetName() + "\"").c_str());
-		
-		m_values.resize(settings.GetQuantities().size());
-		m_valueExtractors.clear();
+		// construct value extractors
+		m_floatValueExtractors.clear();
 		
 		size_t quantityIndex = 0;
 		for (std::vector<std::string>::iterator quantity = settings.GetQuantities().begin();
 		     quantity != settings.GetQuantities().end(); ++quantity)
 		{
-			m_tree->Branch(quantity->c_str(), &m_values[quantityIndex], (*quantity + "/F").c_str());
-		
-			m_valueExtractors.push_back(SafeMap::Get(LambdaNtupleQuantities::CommonQuantities, *quantity));
+			if (LambdaNtupleQuantities::CommonFloatQuantities.count(*quantity) > 0)
+			{
+				m_floatValueExtractors.push_back(SafeMap::Get(LambdaNtupleQuantities::CommonFloatQuantities, *quantity));
+			}
+			else if (LambdaNtupleQuantities::CommonIntQuantities.count(*quantity) > 0)
+			{
+				m_intValueExtractors.push_back(SafeMap::Get(LambdaNtupleQuantities::CommonIntQuantities, *quantity));
+			}
+			else if (LambdaNtupleQuantities::CommonDoubleQuantities.count(*quantity) > 0)
+			{
+				m_doubleValueExtractors.push_back(SafeMap::Get(LambdaNtupleQuantities::CommonDoubleQuantities, *quantity));
+			}
+			/*else if (LambdaNtupleQuantities::CommonBoolQuantities.count(*quantity) > 0)
+			{
+				m_boolValueExtractors.push_back(SafeMap::Get(LambdaNtupleQuantities::CommonBoolQuantities, *quantity));
+			}*/
+			else
+			{
+				LOG(FATAL) << "No lambda expression available for quantity \"" << *quantity << "\"!";
+			}
 			
 			quantityIndex++;
+		}
+		
+		// create tree
+		m_tree = new TTree("ntuple", ("Tree for Pipeline \"" + settings.GetName() + "\"").c_str());
+		
+		// create branches
+		//m_boolValues.resize(m_boolValueExtractors.size());
+		m_intValues.resize(m_intValueExtractors.size());
+		m_floatValues.resize(m_floatValueExtractors.size());
+		m_doubleValues.resize(m_doubleValueExtractors.size());
+		
+		//size_t boolQuantityIndex = 0;
+		size_t intQuantityIndex = 0;
+		size_t floatQuantityIndex = 0;
+		size_t doubleQuantityIndex = 0;
+		for (std::vector<std::string>::iterator quantity = settings.GetQuantities().begin();
+		     quantity != settings.GetQuantities().end(); ++quantity)
+		{
+			if (LambdaNtupleQuantities::CommonFloatQuantities.count(*quantity) > 0)
+			{
+				m_tree->Branch(quantity->c_str(), &(m_floatValues[floatQuantityIndex]), (*quantity + "/F").c_str());
+				floatQuantityIndex++;
+			}
+			else if (LambdaNtupleQuantities::CommonIntQuantities.count(*quantity) > 0)
+			{
+				m_tree->Branch(quantity->c_str(), &(m_intValues[intQuantityIndex]), (*quantity + "/I").c_str());
+				intQuantityIndex++;
+			}
+			else if (LambdaNtupleQuantities::CommonDoubleQuantities.count(*quantity) > 0)
+			{
+				m_tree->Branch(quantity->c_str(), &(m_doubleValues[doubleQuantityIndex]), (*quantity + "/D").c_str());
+				doubleQuantityIndex++;
+			}
+			/*else if (LambdaNtupleQuantities::CommonBoolQuantities.count(*quantity) > 0)
+			{
+				// TODO: http://stackoverflow.com/questions/8399417/why-vectorboolreference-doesnt-return-reference-to-bool
+				m_tree->Branch(quantity->c_str(), &(m_boolValues[boolQuantityIndex]), (*quantity + "/O").c_str());
+				boolQuantityIndex++;
+			}*/
 		}
 	}
 
@@ -79,12 +171,37 @@ public:
 	{
 		ConsumerBase<TTypes>::ProcessFilteredEvent(event, product, settings);
 		
-		size_t valueIndex = 0;
-		for(typename std::vector<float_extractor_lambda_base>::iterator valueExtractor = m_valueExtractors.begin();
-		    valueExtractor != m_valueExtractors.end(); ++valueExtractor)
+		// calculate values
+		/*size_t boolValueIndex = 0;
+		for(typename std::vector<bool_extractor_lambda_base>::iterator valueExtractor = m_boolValueExtractors.begin();
+		    valueExtractor != m_boolValueExtractors.end(); ++valueExtractor)
 		{
-			m_values[valueIndex] = (*valueExtractor)(event, product);
-			valueIndex++;
+			m_boolValues[boolValueIndex] = (*valueExtractor)(event, product);
+			boolValueIndex++;
+		}*/
+		
+		size_t intValueIndex = 0;
+		for(typename std::vector<int_extractor_lambda_base>::iterator valueExtractor = m_intValueExtractors.begin();
+		    valueExtractor != m_intValueExtractors.end(); ++valueExtractor)
+		{
+			m_intValues[intValueIndex] = (*valueExtractor)(event, product);
+			intValueIndex++;
+		}
+		
+		size_t floatValueIndex = 0;
+		for(typename std::vector<float_extractor_lambda_base>::iterator valueExtractor = m_floatValueExtractors.begin();
+		    valueExtractor != m_floatValueExtractors.end(); ++valueExtractor)
+		{
+			m_floatValues[floatValueIndex] = (*valueExtractor)(event, product);
+			floatValueIndex++;
+		}
+		
+		size_t doubleValueIndex = 0;
+		for(typename std::vector<double_extractor_lambda_base>::iterator valueExtractor = m_doubleValueExtractors.begin();
+		    valueExtractor != m_doubleValueExtractors.end(); ++valueExtractor)
+		{
+			m_doubleValues[doubleValueIndex] = (*valueExtractor)(event, product);
+			doubleValueIndex++;
 		}
 
 		// fill tree
@@ -101,8 +218,15 @@ public:
 private:
 	TTree* m_tree = 0;
 	
-	std::vector<float_extractor_lambda_base> m_valueExtractors;
-	std::vector<float> m_values;
+	//std::vector<bool_extractor_lambda_base> m_boolValueExtractors;
+	std::vector<int_extractor_lambda_base> m_intValueExtractors;
+	std::vector<float_extractor_lambda_base> m_floatValueExtractors;
+	std::vector<double_extractor_lambda_base> m_doubleValueExtractors;
+	
+	//std::vector<bool> m_boolValues;
+	std::vector<int> m_intValues;
+	std::vector<float> m_floatValues;
+	std::vector<double> m_doubleValues;
 };
 
 
--- Consumer/src/LambdaNtupleConsumer.cc
+++ Consumer/src/LambdaNtupleConsumer.cc
@@ -1,7 +1,16 @@
 #include "Artus/Consumer/interface/LambdaNtupleConsumer.h"
 
 
-std::map<std::string, std::function<float(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonQuantities 
-	= std::map<std::string, std::function<float(EventBase const&, ProductBase const& ) >>(); 
+std::map<std::string, std::function<bool(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonBoolQuantities 
+	= std::map<std::string, std::function<bool(EventBase const&, ProductBase const& ) >>();
+	
+std::map<std::string, std::function<int(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonIntQuantities 
+	= std::map<std::string, std::function<int(EventBase const&, ProductBase const& ) >>();
+	
+std::map<std::string, std::function<float(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonFloatQuantities 
+	= std::map<std::string, std::function<float(EventBase const&, ProductBase const& ) >>();
+	
+std::map<std::string, std::function<double(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonDoubleQuantities 
+	= std::map<std::string, std::function<double(EventBase const&, ProductBase const& ) >>();
 
 
