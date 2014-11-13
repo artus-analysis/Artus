1f7d36d798a71d8045327118b2c62594db7df030
Georg Sieber <sieber@cern.ch>
2014-11-13 14:31:57 +0100

    start cleaning things up

--- Plotting/python/plotmpl.py
+++ Plotting/python/plotmpl.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#  -*- coding: utf-8 -*-
 
 """
 """
@@ -7,22 +7,26 @@ import logging
 import HarryPlotter.Utility.logger as logger
 log = logging.getLogger(__name__)
 
+import ROOT
 
 import HarryPlotter.Plotting.plotbase as plotbase
-
 import HarryPlotter.Utility.mplconvert as mplconvert
+
+import HarryPlotter.Utility.rootpy
+
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from matplotlib.colors import Normalize
 import matplotlib.gridspec as gridspec
+
 import numpy as np
-import ROOT
-import matplotlib
 
 class PlotMpl(plotbase.PlotBase):
 	def __init__(self):
 		super(PlotMpl, self).__init__()
 		self.mpl_version = int(matplotlib.__version__.replace(".", ""))
+		self.set_matplotlib_defaults()
 
 	def modify_argument_parser(self, parser, args):
 		super(PlotMpl, self).modify_argument_parser(parser, args)
@@ -30,10 +34,12 @@ class PlotMpl(plotbase.PlotBase):
 		                                     help="Colormap for matplotlib [Default: 'afmhot']")
 	def prepare_args(self, parser, plotData):
 		super(PlotMpl, self).prepare_args(parser, plotData)
-		# defaults for colors
+
+		# Set meaningful default values for colors if none provided
+		default_colorcycle = iter(matplotlib.rcParams['axes.color_cycle'])
 		for index, color in enumerate(plotData.plotdict["colors"]):
-			if color == None:
-				plotData.plotdict["colors"][index] = "black"
+			if color is None:
+				plotData.plotdict["colors"][index] = next(self._default_colorcycle)
 			else:
 				plotData.plotdict["colors"][index] = color
 		self.set_default_ratio_colors(plotData)
@@ -53,12 +59,12 @@ class PlotMpl(plotbase.PlotBase):
 	
 	def create_canvas(self, plotData):
 		if plotData.plotdict["ratio"]:
-			self.fig = plt.figure(figsize=[7, 7])
+			self.fig = plt.figure()
 			self.ax = plt.subplot2grid((4,1), (0, 0), rowspan=3)
 			self.ax2 = plt.subplot2grid((4,1), (3, 0))
 			#plotdict['ratiosubplotaxes'] = ax2 # needed?
 		else:
-			self.fig = plt.figure(figsize=[7, 7])
+			self.fig = plt.figure()
 			self.ax = self.fig.add_subplot(111)
 
 
@@ -119,21 +125,26 @@ class PlotMpl(plotbase.PlotBase):
 					                            cmap=plt.cm.get_cmap(plotData.plotdict["colormap"]),
 					                            norm=norm())
 				elif marker=="bar":
-					bottom=0
+					bottom=0.
 					self.ax.bar(mpl_histogram.x, mpl_histogram.y, widths, yerr=yerr, bottom=bottom,
 					            label=label, fill=True, facecolor=color, edgecolor=color, ecolor=color, alpha=1.0)
 				elif marker=='fill':
 					y = np.array(mpl_histogram.y)
 					self.ax.fill_between(x=self.steppify_bin(bin_edges, isx=True), y1=self.steppify_bin(y),y2=0., color=color,
-					            label=label, facecolor=color, edgecolor='black', alpha=0.8, zorder=1)
-					self.ax.errorbar(mpl_histogram.xc, y, yerr=yerr,
-					                 color=color, fmt='+', capsize=0, label=label, zorder=10, linestyle='')
+					                     facecolor=color, edgecolor=color, alpha=1.0, zorder=1)
+					if errorbar:
+						self.ax.errorbar(mpl_histogram.xc, y, yerr=yerr,
+						                 color=color, fmt='+', capsize=0, zorder=1, linestyle='')
+					# draw the legend proxy
+					proxy = plt.Rectangle((0, 0), 0, 0, label=label, facecolor=color, edgecolor='black', alpha=1.0)
+					self.ax.add_patch(proxy)
 				else:
 					y = np.array(mpl_histogram.y)
 					self.ax.plot(self.steppify_bin(bin_edges, isx=True), self.steppify_bin(y), color=color,
-					             linestyle='-')
-					self.ax.errorbar(mpl_histogram.xc, y, yerr=yerr,
-					                 color=color, fmt=marker, capsize=0, label=label, zorder=1, linestyle='')
+					             linestyle='-', label=label, zorder = 4)
+					if errorbar:
+						self.ax.errorbar(mpl_histogram.xc, y, yerr=yerr,
+						                 color=color, fmt=marker, capsize=0, zorder=4, linestyle='')
 			# draw functions from dictionary
 			elif isinstance(root_object, ROOT.TF1):
 				x_values = []
@@ -162,9 +173,9 @@ class PlotMpl(plotbase.PlotBase):
 		self.ax.grid(plotData.plotdict["grid"])
 
 		if not plotData.plotdict["ratio"]:
-			self.ax.set_xlabel(plotData.plotdict["x_label"], fontsize=14)
-		self.ax.set_ylabel(plotData.plotdict["y_label"], fontsize=14)
-		self.ax.ticklabel_format(style='sci',scilimits=(-3,4),axis='both')
+			self.ax.set_xlabel(plotData.plotdict["x_label"], position=(1., 0.), va='top', ha='right')
+		self.ax.set_ylabel(plotData.plotdict["y_label"], position=(0., 1.), va='top', ha='right')
+		# self.ax.ticklabel_format(style='sci',scilimits=(-3,4),axis='both')
 
 		# do special things for 1D Plots
 		if self.plot1D:
@@ -174,15 +185,15 @@ class PlotMpl(plotbase.PlotBase):
 				self.ax.set_yscale('log', nonposy='clip')
 
 			if plotData.plotdict["ratio"]:
-				self.ax2.set_xlabel(plotData.plotdict["x_label"])
+				self.ax2.set_xlabel(plotData.plotdict["x_label"],position=(1., 0.), va='top', ha='right')
 				self.ax2.set_ylabel(plotData.plotdict["y_ratio_label"])
 				self.ax2.grid(plotData.plotdict["ratio_grid"])
+				# Don't show ticklabels on main plot
+				self.ax.xaxis.set_ticklabels([])
+				# Ratio plot shares xlims of main plot
 				self.ax2.set_xlim(self.ax.get_xlim())
 				if plotData.plotdict["x_log"]: 
 					self.ax2.set_xscale('log', nonposx='clip')
-
-
-
 
 		# do special things for 2D Plots
 		if self.plot2D:
@@ -202,16 +213,56 @@ class PlotMpl(plotbase.PlotBase):
 			energy = "+".join(plotData.plotdict["energy"])
 			plt.text(1.0, 1.030, "$\sqrt{s}=" + energy + "\\ \mathrm{TeV}$", transform=self.ax.transAxes, fontsize=10, horizontalalignment="right")
 			#self.fig.suptitle("$\sqrt{s}=" + energy + "\\ \mathrm{TeV}$", ha="right",x=0.9) 
-		legend = self.ax.legend()
+		legend = self.ax.legend(loc='upper right')
 		if self.mpl_version >= 121:
 			plt.tight_layout()
 		# Decrease vertical distance between subplots
-		plt.subplots_adjust(hspace=0.3)
+		plt.subplots_adjust(hspace=0.2)
 
 	def save_canvas(self, plotData):
 		for output_filename in plotData.plotdict["output_filenames"]:
 			self.fig.savefig(output_filename)
 			log.info("Created plot \"%s\"." % output_filename)
+
+	@staticmethod
+	def set_matplotlib_defaults():
+		# Set matplotlib rc settings
+		# matplotlib.rcParams['font.family'] = 'sans-serif'
+		matplotlib.rcParams['mathtext.fontset'] = 'stixsans'
+		matplotlib.rcParams['mathtext.default'] = 'rm'
+		# matplotlib.rcParams['font.sans-serif'] = 'helvetica, Helvetica, Nimbus Sans L, Mukti Narrow, FreeSans'
+		# figure
+		matplotlib.rcParams['figure.figsize'] = 7., 7.
+		# axes
+		matplotlib.rcParams['axes.linewidth'] = 2
+		matplotlib.rcParams['axes.labelsize'] = 20
+		matplotlib.rcParams['xtick.labelsize'] = 16
+		matplotlib.rcParams['xtick.major.size'] = 12
+		matplotlib.rcParams['xtick.minor.size'] = 6
+		matplotlib.rcParams['ytick.labelsize'] = 16
+		matplotlib.rcParams['ytick.major.size'] = 14
+		matplotlib.rcParams['ytick.minor.size'] = 7
+		matplotlib.rcParams['lines.markersize'] = 8
+		# color cycle
+		matplotlib.rcParams['axes.color_cycle'] = [(0.0, 0.0, 0.0),
+		                                           (0.21568627450980393, 0.49411764705882355, 0.7215686274509804),
+		                                           (0.30196078431372547, 0.6862745098039216, 0.2901960784313726),
+		                                           (0.596078431372549, 0.3058823529411765, 0.6392156862745098),
+		                                           (1.0, 0.4980392156862745, 0.0),
+		                                           (1.0, 1.0, 0.2),
+		                                           (0.6509803921568628, 0.33725490196078434, 0.1568627450980392),
+		                                           (0.9686274509803922, 0.5058823529411764, 0.7490196078431373),
+		                                           (0.6, 0.6, 0.6)]
+		# legend
+		matplotlib.rcParams['legend.numpoints'] = 1
+		matplotlib.rcParams['legend.fontsize'] = 19
+		matplotlib.rcParams['legend.labelspacing'] = 0.3
+		matplotlib.rcParams['legend.frameon'] = False
+		  # Saving
+		matplotlib.rcParams['savefig.bbox'] = 'tight'
+		matplotlib.rcParams['savefig.dpi'] = 150
+		matplotlib.rcParams['savefig.format'] = 'png'
+		matplotlib.rcParams['agg.path.chunksize'] = 20000
 
 
 	@staticmethod
@@ -225,3 +276,35 @@ class PlotMpl(plotbase.PlotBase):
 			newarr = np.array(zip(arr, arr)).ravel()
 		return newarr
 
+	def plot_errorbar(hist, xerr=True, yerr=True, emptybins=True, ax=None, zorder=1, **kwargs)
+	
+		if ax is None:
+			ax = plt.gca()
+		pass
+
+		if xerr:
+			xerr = np.array(h.xerrl, h.xerrh)
+		else:
+			xerr = 0.
+		if yerr:
+			yerr = np.array(h.yerrl, h.yerrh)
+		else:
+			yerr = 0.
+
+		ax.errorbar(h.x, h.y, xerr=xerr, yerr=yerr, zorder=zorder, **kwargs)
+
+	def plot_hist1d(hist, style='fill', **kwargs):
+
+		if ax is None:
+			ax = plt.gca()
+		pass
+
+		bottom = 0.
+		x = self.steppify_bin(hist.xedges, isx=True)
+		y = self.steppify_bin(hist.y)
+
+		ax.fill_between(x, y, bottom=0., color=color,
+		                alpha=1.0, zorder=1)
+		# draw the legend proxy
+		proxy = plt.Rectangle((0, 0), 0, 0, label=label, facecolor=color, edgecolor='black', alpha=1.0)
+		self.ax.add_patch(proxy)
