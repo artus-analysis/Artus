03f74e28300f5ce5e0f268b1a9ca5b5822cc397c
Thomas Mueller <tmuller@cern.ch>
2014-11-26 22:18:30 +0100

    Move files from Artus and HarryPlotter to here

---
+++ Tools/python/jsonTools.py
@@ -0,0 +1,333 @@
+
+# -*- coding: utf-8 -*-
+
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import collections
+import copy
+import json
+import os
+import pprint
+import sys
+
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+import Artus.Utility.tools as tools
+
+
+class JsonDict(dict):
+	"""
+	Class that stores python dicts and offers some additional JSON functionality
+	"""
+	
+	# static members as global settings
+	PATH_TO_ROOT_CONFIG = "config"
+	ALWAYS_DO_COMMENTS = True
+	COMMENT_DELIMITER = "#"
+
+	def __init__(self, jsonDict={}):
+		"""
+		Constructor
+		The parameter jsonDict can be of typ dict, string or list
+		If jsonDict is of type string, it is first assumed to be a filename (can be JSON
+		text file or ROOT file (with TObjString object named PATH_TO_ROOT_CONFIG in it).
+		Otherwise, the string is tried to be evaluated as python code.
+		If jsonDict is of type list, the list is converted into a JsonDictList
+		and all items are merged into one single JsonDict
+		"""
+	
+		if isinstance(jsonDict, dict):
+			dict.__init__(self, jsonDict)
+		elif isinstance(jsonDict, basestring):
+			if os.path.exists(os.path.expandvars(jsonDict)):
+				dict.__init__(self, JsonDict.readJsonDict(jsonDict))
+			else:
+				dict.__init__(self, json.loads(jsonDict))
+		elif isinstance(jsonDict, collections.Iterable):
+			dict.__init__(self, JsonDict.mergeAll(*jsonDict))
+		else:
+			raise TypeError("Unsupported type \"%s\" in JsonDict constructor", type(jsonDict))
+		if JsonDict.ALWAYS_DO_COMMENTS:
+			JsonDict.deepuncomment(self)
+	
+	def __add__(self, jsonDict2):
+		""" merges two JSON dicts and returns a new object """
+		
+		jsonDict1 = JsonDict(self)
+		JsonDict.deepmerge(jsonDict1, jsonDict2)
+		return jsonDict1
+	
+	def merge(self, jsonDict2):
+		""" merges two JSON dicts and returns a new object """
+		return (self + jsonDict2)
+	
+	@staticmethod
+	def mergeAll(*jsonDicts):
+		""" merge all given parameters into one JSON dict """
+		if len(jsonDicts) == 1:
+			return JsonDict(jsonDicts[0])
+		else:
+			return reduce(lambda jsonDict1, jsonDict2: JsonDict(jsonDict1) + JsonDict(jsonDict2), jsonDicts)
+	
+	def __sub__(self, jsonDict2):
+		""" compares two JSON dicts and returns two diff dicts """
+		return JsonDict.deepdiff(self, jsonDict2)
+	
+	def diff(self, jsonDict2):
+		""" compares two JSON dicts and returns two diff dicts """
+		return (self - jsonDict2)
+	
+	def __mul__(self, jsonDict2):
+		"""
+		expands all possible combinations of second-layer dictionaries
+		returns new JsonDict { keyA1_keyB1 : valueA1+valueB1, ... }
+		"""
+		
+		jsonDict = JsonDict()
+		for key1, value1 in self.items():
+			for key2, value2 in jsonDict2.items():
+				#key = (str(key1) if len(self) > 1 else "") + \
+				#      ("_" if len(self) > 1 and len(jsonDict2) > 1 else "") + \
+				#      (str(key2) if len(jsonDict2) > 1 else "")
+				key = str(key1) + "_" + str(key2)
+				jsonDict[key] = JsonDict(value1) + JsonDict(value2)
+		return jsonDict
+	
+	def expand(self, jsonDict2):
+		"""
+		expands all possible combinations of second-layer dictionaries
+		returns new JsonDict { keyA1_keyB1 : valueA1+valueB1, ... }
+		"""
+		return (self * jsonDict2)
+	
+	@staticmethod
+	def expandAll(*jsonDicts):
+		""" expand all given parameters into one JSON dict """
+		if len(jsonDicts) == 1:
+			return JsonDict(jsonDicts[0])
+		else:
+			return reduce(lambda jsonDict1, jsonDict2: JsonDict(jsonDict1) * JsonDict(jsonDict2), jsonDicts)
+	
+	def doIncludes(self):
+		""" resolves the includes and returns a new object """
+		return JsonDict(JsonDict.deepinclude(self))
+	
+	def doNicks(self, nick="default"):
+		""" resolves the nicks and returns a new object """
+		return JsonDict(JsonDict.deepresolvenicks(self, nick))
+	
+	def doComments(self):
+		""" resolves the comments and returns a new object """
+		return JsonDict.deepuncomment(JsonDict(copy.deepcopy(self)))
+	
+	def doExpandvars(self):
+		""" expands environment variables in dictionary values """
+		return JsonDict(JsonDict.deepexpandvars(self))
+	
+	def __str__(self):
+		""" converts JSON dict to a string """
+		return self.toString()
+	
+	def toString(self, indent=4, sort_keys=True, **kwargs):
+		""" converts JSON dict to a string """
+		return json.dumps(self, indent=indent, sort_keys=sort_keys, **kwargs)
+
+	def save(self, fileName, indent=None, sort_keys=True, **kwargs):
+		""" write JSON dict to a file """
+		with open(fileName, "w") as jsonFile: json.dump(self, jsonFile, indent=indent, sort_keys=sort_keys, **kwargs)
+
+	@staticmethod
+	def readJsonDict(fileName):
+		"""
+		reads JSON dictionary from file
+		fileName can point to a JSON text file or to a ROOT file containing
+		a TObjString object named as specified by PATH_TO_ROOT_CONFIG
+		"""
+		
+		fileName = os.path.expandvars(fileName)
+		if not os.path.exists(fileName):
+			log.critical("File \"%s\" does not exist!" % fileName)
+			sys.exit(1)
+
+		jsonStrings = []
+		# read all cycles of JsonDict.PATH_TO_ROOT_CONFIG from ROOT file and merge them
+		if os.path.splitext(fileName)[1] == ".root":
+			rootFile = ROOT.TFile(fileName, "READ")
+			listOfKeys = rootFile.GetListOfKeys()
+			for keyIndex in range(listOfKeys.GetSize()):
+				namecycle = "%s;%d" % (listOfKeys.At(keyIndex).GetName(), listOfKeys.At(keyIndex).GetCycle())
+				if namecycle.startswith(JsonDict.PATH_TO_ROOT_CONFIG):
+					jsonStrings.append(str(rootFile.Get(namecycle).GetString()))
+			rootFile.Close()
+			if len(jsonStrings) == 0:
+				log.critical("Could not read \"%s\" from file \"%s\"!" % (JsonDict.PATH_TO_ROOT_CONFIG, rootFileName))
+				sys.exit(1)
+		else:
+			with open(fileName, "r") as jsonFile:
+				jsonStrings.append(jsonFile.read())
+	
+		jsonDict = JsonDict()
+		try:
+			for jsonString in [jsonStrings[-1]]: #jsonStrings[::-1]: # TODO need cmore careful merging of lists
+				jsonDict += JsonDict(json.loads(jsonString))
+		except ValueError, e:
+			log.critical("Invalid JSON syntax in file \"%s\"." % fileName)
+			log.critical(str(e))
+			sys.exit(1)
+		return jsonDict
+
+	@staticmethod
+	def deepmerge(dictWithHigherPriority, dictWithLowerPriority):
+		"""
+		deepmerge of two dictionaries.
+		Entries from dictWithLowerPriority are recursively merged into
+		dictWithHigherPriority in case, nothing has to be overwritten.
+		"""
+		
+		for key, value in dictWithLowerPriority.items():
+			if isinstance(value, dict):
+				newTarget = dictWithHigherPriority.setdefault(key, {})
+				if newTarget == None: dictWithHigherPriority.pop(key)
+				else: JsonDict.deepmerge(newTarget, value)
+			elif isinstance(value, collections.Iterable) and not isinstance(value, basestring):
+				dictWithHigherPriority.setdefault(key, []).extend(value)
+			elif dictWithHigherPriority != None and key not in dictWithHigherPriority:
+				dictWithHigherPriority[key] = value
+		return dictWithHigherPriority
+
+	@staticmethod
+	def deepdiff(dictA, dictB):
+		"""
+		deepdiff of two dictionaries.
+		returns one dictionary for each given dictionary, that contains differences to other dictionary
+		"""
+		
+		diffDictA = {}
+		diffDictB = {}
+		for key in set(dictA.keys() + dictB.keys()):
+			if key not in dictA:
+				diffDictB[key] = dictB[key]
+			elif key not in dictB:
+				diffDictA[key] = dictA[key]
+			elif dictA[key] != dictB[key]:
+				if isinstance(dictA[key], dict) and isinstance(dictB[key], dict):
+					diffDictA2, diffDictB2 = JsonDict.deepdiff(dictA[key], dictB[key])
+					diffDictA[key] = diffDictA2
+					diffDictB[key] = diffDictB2
+				else:
+					diffDictA[key] = dictA[key]
+					diffDictB[key] = dictB[key]
+		return diffDictA, diffDictB
+
+	@staticmethod
+	def deepinclude(jsonDict):
+		"""
+		resolves/replaces include options in JSON dictionaries
+		protected property names are "include" which is followed by a list of files to include
+		and "property" which is used as a property name in the included file to include just one property
+		"""
+		
+		result = None
+		if isinstance(jsonDict, dict):
+			result = JsonDict()
+			for key, value in jsonDict.items():
+				if key == "include":
+					if isinstance(value, basestring):
+						value = [value]
+					for includeFile in value:
+						tmpResult = JsonDict.deepinclude(JsonDict(includeFile))
+						if isinstance(tmpResult, dict):
+							result += JsonDict(tmpResult)
+						elif isinstance(tmpResult, collections.Iterable) and not isinstance(tmpResult, basestring):
+							if result == JsonDict():
+								result = []
+							result.extend(tmpResult)
+						else:
+							result = tmpResult
+				elif key == "property":
+					result = JsonDict.deepinclude(value)
+				else:
+					result[key] = JsonDict.deepinclude(value)
+		elif isinstance(jsonDict, collections.Iterable) and not isinstance(jsonDict, basestring):
+			result = []
+			for element in jsonDict:
+				tmpResult = JsonDict.deepinclude(element)
+				if isinstance(tmpResult, collections.Iterable) and not isinstance(tmpResult, basestring):
+					result.extend(tmpResult)
+				else:
+					result.append(tmpResult)
+		else:
+			result = copy.deepcopy(jsonDict)
+		return result
+
+	@staticmethod
+	def deepresolvenicks(jsonDict, nick="default"):
+		"""
+		resolves/replaces nick options in JSON dictionaries
+		"nick" is a protected property name which is followed by a dict with nick name regexs as keys
+		if no matching nick name is found, it is looked for "default".
+		"""
+		
+		result = None
+		if isinstance(jsonDict, dict):
+			result = JsonDict()
+			for key, value in jsonDict.items():
+				if isinstance(value, dict) and isinstance(value.get("nick"), dict):
+					nickDict = value["nick"]
+					tmpValue = nickDict.get(tools.matchingItem(nickDict.keys(), nick),
+					                        nickDict.get("default"))
+					if tmpValue != None:
+						result[key] = JsonDict.deepresolvenicks(tmpValue, nick=nick)
+				else:
+					result[key] = JsonDict.deepresolvenicks(value, nick=nick)
+		else:
+			result = copy.deepcopy(jsonDict)
+		return result
+	
+	@staticmethod
+	def deepuncomment(jsonDict):
+		"""
+		remove all comments from this JSON dictionary
+		comments are keys, values and list entries starting with COMMENT_DELIMITER
+		editing in place.
+		"""
+		if isinstance(jsonDict, dict):
+			for key, value in jsonDict.items():
+				if key.strip().startswith(JsonDict.COMMENT_DELIMITER):
+					pass
+					#del jsonDict[key]
+				else:
+					if isinstance(value, basestring) and value.strip().startswith(JsonDict.COMMENT_DELIMITER):
+						del jsonDict[key]
+					elif isinstance(value, collections.Iterable) and not isinstance(value, basestring):
+						for index, element in enumerate(value):
+							if isinstance(element, basestring) and element.strip().startswith(JsonDict.COMMENT_DELIMITER):
+								del value[index]
+					if isinstance(value, dict):
+						JsonDict.deepuncomment(value)
+		return jsonDict
+
+	@staticmethod
+	def deepexpandvars(jsonDict):
+		"""
+		call os.path.expandvars recursively for each string-type value in this dictionary
+		"""
+		
+		result = None
+		if isinstance(jsonDict, dict):
+			result = JsonDict()
+			for key, value in jsonDict.items():
+				result[key] = JsonDict.deepexpandvars(value)
+		elif isinstance(jsonDict, collections.Iterable) and not isinstance(jsonDict, basestring):
+			result = []
+			for item in jsonDict:
+				result.append(JsonDict.deepexpandvars(item))
+		elif isinstance(jsonDict, basestring):
+			result = os.path.expandvars(jsonDict)
+		else:
+			result = jsonDict
+		return result
+
---
+++ Tools/scripts/dcache-tools.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import argparse
+import fnmatch
+import os
+import subprocess
+import tempfile
+
+
+# expand wildcards in paths and lists files (recursively)
+def list_of_files(prefix, src, recursive):
+	def recursive_ls(prefix, src_checked, src_to_check, recursive_mode=False):
+		src_to_check_splitted = filter(lambda item: item != "", src_to_check.split("/"))
+		current_src_to_check = os.path.join(src_checked, src_to_check_splitted[0])
+		next_src_to_check = os.path.join(*src_to_check_splitted[1:]) if len(src_to_check_splitted) > 1 else ""
+		
+		# retrieve files/dirs in next level and match with src
+		ls_command = "ls -1" if prefix == "" else "lcg-ls"
+		next_srcs_checked = subprocess.Popen((ls_command + " " + prefix + src_checked).split(),
+		                                     stdout=subprocess.PIPE).communicate()[0].split("\n")
+		next_srcs_checked = [next_src_checked if next_src_checked.startswith("/") else os.path.join(src_checked, next_src_checked) for next_src_checked in next_srcs_checked]
+		next_srcs_checked = [next_src_checked[:-1] if next_src_checked.endswith("/") else next_src_checked for next_src_checked in next_srcs_checked]
+		next_srcs_checked = filter(lambda result: fnmatch.fnmatch(result, current_src_to_check) or fnmatch.fnmatch(os.path.join(src_checked, result), current_src_to_check), next_srcs_checked)
+		
+		# exit recursion
+		if len(src_to_check_splitted) == 1:
+			if recursive_mode:
+				next_src_to_check = "*"
+				if len(next_srcs_checked) == 0 or (len(next_srcs_checked) == 1 and fnmatch.fnmatch(next_srcs_checked[0], src_checked)):
+					return [src_checked]
+			else:
+				return next_srcs_checked
+		
+		# recursion for next level
+		src_files = []
+		for next_src_checked in next_srcs_checked:
+			src_files.extend(recursive_ls(prefix, next_src_checked, next_src_to_check, recursive_mode))
+		return src_files
+	
+	files_before_recursion = recursive_ls(prefix, "/", src, recursive_mode=False)
+	
+	files_after_recursion = []
+	if recursive:
+		for file_before_recursion in files_before_recursion:
+			files_after_recursion.append(recursive_ls(prefix, file_before_recursion, "*", recursive_mode=True))
+	else:
+		files_after_recursion = [] * len(files_before_recursion)
+	
+	return files_before_recursion, files_after_recursion
+
+
+# construct mapping between source and destination files
+def get_mapping_src_dst(src_files, src_files_recursive, dst_files):
+	if not (len(dst_files) == 1 or len(dst_files) == len(src_files)):
+		log.fatal("Wrong number of destinations!")
+	
+	tmp_dst_files = (dst_files * len(src_files))[:len(src_files)]
+	
+	mapping = {}
+	for src_file, src_file_recursive, dst_file in zip(src_files, src_files_recursive, tmp_dst_files):
+		if len(src_file_recursive) == 0:
+			mapping[src_file] = dst_file
+			# TODO could check more intelligently for existing directories
+		else:
+			for tmp_src_file_recursive in src_file_recursive:
+				rel_src_file_recursive = os.path.relpath(tmp_src_file_recursive, os.path.dirname(src_file))
+				mapping[tmp_src_file_recursive] = os.path.join(dst_file, rel_src_file_recursive)
+	
+	return mapping
+
+
+# create missing loacl directories for all destination paths
+def create_local_dst_directories(dst_files):
+	directories = list(set([os.path.dirname(dst_file) for dst_file in dst_files]))
+	for directory in directories:
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+
+
+# execute the command and print it before the execution
+def execute_command(command, print_command=True):
+	if print_command:
+		log.info(command)
+	logger.subprocessCall(command.split())
+
+
+def main():
+	
+	parser = argparse.ArgumentParser(description="Tools simplifying dCache usage.", parents=[logger.loggingParser],
+	                                 epilog="Useful help: https://twiki.opensciencegrid.org/bin/view/ReleaseDocumentation/LcgUtilities#Using_LCG_Utils_commands")
+	
+	parser.add_argument("-c", "--command", required=True,
+	                    help="Command, e.g. lcg-cp, lcg-del or ddcp.")
+	parser.add_argument("-r", "--recursive", default=False, action="store_true",
+	                    help="Follow recursively into all specified directories. Specify this also when you do not want to specify the full destination file name.")
+	parser.add_argument("-a", "--args", default="-v", help="Arguments. Default: -v.")
+	parser.add_argument("-s", "--src", help="Source.", required=True)
+	parser.add_argument("--src-prefix", default="",
+	                    help="Source prefix. \"gridka\" and \"desy\" are replaced by their dCache locations. \"\" means local path.")
+	parser.add_argument("-d", "--dst", help="Destination. Can be left empty.")
+	parser.add_argument("--dst-prefix", default="",
+	                     help="Destination prefix. \"gridka\" and \"desy\" are replaced by their dCache locations. \"\" means local path.")
+	
+	args = parser.parse_args()
+	logger.initLogger(args)
+	
+	# replace prefixes for convenience
+	prefix_replacements = {
+		"gridka" : "srm://dgridsrm-fzk.gridka.de:8443/srm/managerv2?SFN=",
+		"desy" : "srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=",
+	}
+	for replacement_from, replacement_to in prefix_replacements.items():
+		if args.src_prefix == replacement_from:
+			args.src_prefix = replacement_to
+		if args.dst_prefix == replacement_from:
+			args.dst_prefix = replacement_to
+	
+	# paths need to be abolute
+	if args.src_prefix == "":
+		args.src = os.path.abspath(args.src)
+	if args.dst_prefix == "" and args.dst:
+		args.dst = os.path.abspath(args.dst)
+	
+	# find src files
+	log.debug("Locate all source files...")
+	src_files, src_files_recursive = list_of_files(args.src_prefix, args.src, args.recursive)
+	
+	if args.dst:
+		# find dst files and mapping to src files
+		log.debug("Locate all destination files...")
+		dst_files, dst_files_recursive = list_of_files(args.dst_prefix, args.dst, False)
+		if len(dst_files) == 0:
+			dst_files = [args.dst]
+		
+		log.debug("Map destination to source files...")
+		mapping_src_dst = get_mapping_src_dst(src_files, src_files_recursive, dst_files)
+		
+		# create missing local directories (lcg does it on its own)
+		if args.dst_prefix == "":
+			create_local_dst_directories(mapping_src_dst.values())
+		
+		log.debug("Excute commands...")
+		# loop over all files and execute the command
+		for src_file, dst_file in mapping_src_dst.items():
+			if args.src_prefix == "" and args.dst_prefix == "":
+				# need intermediate temporary local copy of the file
+				local_copy = tempfile.mktemp(prefix="dcache-tools_")
+				execute_command((args.command + " " + (args.args if args.args else "") + " " + args.src_prefix + src_file + " " + local_copy))
+				execute_command((args.command + " " + (args.args if args.args else "") + " " + local_copy + " " + args.dst_prefix + dst_file))
+				os.remove(local_copy)
+			else:
+				execute_command((args.command + " " + (args.args if args.args else "") + " " + args.src_prefix + src_file + " " + args.dst_prefix + dst_file))
+		
+	else:
+		log.debug("Excute commands...")
+		# loop over all src files and execute the command
+		for src_file in (src_files_recursive if args.recursive else src_files):
+			execute_command((args.command + " " + (args.args if args.args else "") + " " + args.src_prefix + src_file))
+	
+	log.debug("Finished!")
+
+if __name__ == "__main__":
+	main()
+
