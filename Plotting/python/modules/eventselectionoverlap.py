dc954db1dc276d29e82e9c1e52df8d828f953d54
Thomas Mueller <tmuller@cern.ch>
2014-11-27 19:31:14 +0100

    eventselectionoverlap.py: added options to print non-intersecting events

--- Plotting/python/modules/eventselectionoverlap.py
+++ Plotting/python/modules/eventselectionoverlap.py
@@ -18,6 +18,15 @@ class EventSelectionOverlap(analysisbase.AnalysisBase):
 	def __init__(self):
 		super(EventSelectionOverlap, self).__init__()
 	
+	def modify_argument_parser(self, parser, args):
+		super(EventSelectionOverlap, self).modify_argument_parser(parser, args)
+		
+		self.cutflow_options = parser.add_argument_group("Event selection overlap options")
+		self.cutflow_options.add_argument("--events-only-in-1", action="store_true", default=False,
+				help="Print events that are only in the first input.")
+		self.cutflow_options.add_argument("--events-only-in-2", action="store_true", default=False,
+				help="Print events that are only in the second input.")
+	
 	def run(self, plotData=None):
 		super(EventSelectionOverlap, self).run(plotData)
 		
@@ -31,15 +40,20 @@ class EventSelectionOverlap(analysisbase.AnalysisBase):
 					# Events in KIT ntuple
 					events2 = EventSelectionOverlap.get_events_set_from_tree(tree2, plotData.plotdict["x_expressions"][index2])
 
-					n_events_only1 = len(events1.difference(events2))
-					n_events_intersection = len(events1.intersection(events2))
-					n_events_only2 = len(events2.difference(events1))
+					events_only1 = list(events2.difference(events1))
+					events_intersection = list(events1.intersection(events2))
+					events_only2 = list(events1.difference(events2))
+					
+					if plotData.plotdict["events_only_in_1"]:
+						log.info("Events only in the first input:\n\t%s" % str(events_only1))
+					if plotData.plotdict["events_only_in_2"]:
+						log.info("Events only in the second input:\n\t%s" % str(events_only2))
 
-					histogram = ROOT.TH1F("histogram_{0}".format(hashlib.md5("_".join([str(n_events_only1), str(n_events_intersection), str(n_events_only2)]))), "Event Selection Overlap", 3, -1.0, 1.0)
+					histogram = ROOT.TH1F("histogram_{0}".format(hashlib.md5("_".join([str(events_only1), str(events_intersection), str(events_only2)]))), "Event Selection Overlap", 3, -1.0, 1.0)
 
-					histogram.SetBinContent(1, n_events_only2)
-					histogram.SetBinContent(2, n_events_intersection)
-					histogram.SetBinContent(3, n_events_only1)
+					histogram.SetBinContent(1, len(events_only1))
+					histogram.SetBinContent(2, len(events_intersection))
+					histogram.SetBinContent(3, len(events_only2))
 
 					# KIT vs sync ntuple
 					plotData.plotdict["root_objects"][nick2 + "_vs_" + nick1] = histogram
