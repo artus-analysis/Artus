0baee88736d9df1c8e71ce84ae53c9e0521bcd43
Thomas Mueller <tmuller@cern.ch>
2014-11-27 15:45:46 +0100

    LambdaNtupleConsumer: added support for string quantities

--- Consumer/src/LambdaNtupleConsumer.cc
+++ Consumer/src/LambdaNtupleConsumer.cc
@@ -1,19 +1,21 @@
 #include "Artus/Consumer/interface/LambdaNtupleConsumer.h"
 
 
-std::map<std::string, std::function<bool(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonBoolQuantities 
+std::map<std::string, std::function<bool(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonBoolQuantities
 	= std::map<std::string, std::function<bool(EventBase const&, ProductBase const& ) >>();
-	
-std::map<std::string, std::function<int(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonIntQuantities 
+
+std::map<std::string, std::function<int(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonIntQuantities
 	= std::map<std::string, std::function<int(EventBase const&, ProductBase const& ) >>();
-	
-std::map<std::string, std::function<uint64_t(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonUInt64Quantities 
+
+std::map<std::string, std::function<uint64_t(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonUInt64Quantities
 	= std::map<std::string, std::function<uint64_t(EventBase const&, ProductBase const& ) >>();
-	
-std::map<std::string, std::function<float(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonFloatQuantities 
+
+std::map<std::string, std::function<float(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonFloatQuantities
 	= std::map<std::string, std::function<float(EventBase const&, ProductBase const& ) >>();
-	
-std::map<std::string, std::function<double(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonDoubleQuantities 
+
+std::map<std::string, std::function<double(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonDoubleQuantities
 	= std::map<std::string, std::function<double(EventBase const&, ProductBase const& ) >>();
 
+std::map<std::string, std::function<std::string(EventBase const&, ProductBase const& ) >> LambdaNtupleQuantities::CommonStringQuantities
+	= std::map<std::string, std::function<std::string(EventBase const&, ProductBase const& ) >>();
 
