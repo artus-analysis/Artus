8dbead0e0cc41595e64bbaa1558c6f6d484b2f40
Thomas Hauth <Thomas.Hauth@cern.ch>
2014-12-05 13:29:53 +0100

    changed sparse checkout to HarryPlotter

--- Utility/scripts/onlyHarryPlotter.sh
+++ Utility/scripts/onlyHarryPlotter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git config core.sparsecheckout true
 rm .git/info/sparse-checkout
-echo /Plotting/ >> .git/info/sparse-checkout
+echo /HarryPlotter/ >> .git/info/sparse-checkout
 echo /Utility/python/ >> .git/info/sparse-checkout
 echo /Utility/scripts/ >> .git/info/sparse-checkout
 git checkout -f master
