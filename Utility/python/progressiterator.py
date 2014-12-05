fdd8c71d1131a639a8b9dbf43c7ef92da6fce81d
Thomas Mueller <tmuller@cern.ch>
2014-12-05 16:49:13 +0100

    progressiterator.py: improved updating of the percentage

--- Utility/python/progressiterator.py
+++ Utility/python/progressiterator.py
@@ -15,26 +15,31 @@ class ProgressIterator(object):
 		self.len = length if length != None else len(iterable)
 		self.description = (description if description == "" else (description+"... "))
 		self.current_index = 0
-		self.current_progress = -1
+		self.ratio = -1.0
 		self.tty_width = tools.get_tty_size()[1]
 
 	def __iter__(self):
 		return self
 	
 	def next(self):
-		current_progress = min(int(math.ceil(float(self.tty_width) * self.current_index / self.len)),
+		ratio = float(self.current_index) / self.len
+		
+		if int(ratio*100) > int(self.ratio*100):
+			self.ratio = ratio
+			current_progress = min(int(math.ceil(float(self.tty_width) * self.current_index / self.len)),
 		                       self.tty_width)
-		if current_progress > self.current_progress:
-			self.current_progress = current_progress
-			ratio = float(self.current_index) / self.len
+			
 			line = "%s%.1f %s" % (self.description, ratio*100, "%")
 			#line += (" " * (self.tty_width - len(line)))
 			line = line.center(self.tty_width)
-			line = "\r\033[0;42m%s\033[0;41m%s\033[0m" % (line[:self.current_progress], line[self.current_progress:])
+			line = "\r\033[0;42m%s\033[0;41m%s\033[0m" % (line[:current_progress], line[current_progress:])
+			
 			sys.stdout.write(line)
 			sys.stdout.flush()
+		
 		if self.current_index == self.len:
 			sys.stdout.write("\n")
+		
 		self.current_index += 1
 		return self.iterator.next()
 
