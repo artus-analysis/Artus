503e2bf5634a144ab026335cadf99c1332f9ad1d
Thomas Mueller <tmuller@cern.ch>
2014-12-05 15:32:15 +0100

    compareRootFiles.py: moved to Utilities and implemented usage of the logger

--- Utility/scripts/compareRootFiles.py
+++ Utility/scripts/compareRootFiles.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
 import ROOT
 import sys
 
@@ -15,40 +19,38 @@ Usage: %s file1.root file2.root [-a]
 def main():
 	global opt_all
 	if len(sys.argv) < 3:
-		print __doc__
+		log.info(__doc__)
 		exit(2)
 	inputFile1 = ROOT.TFile(sys.argv[1])
 	inputFile2 = ROOT.TFile(sys.argv[2])
 	opt_all = (len(sys.argv) > 3 and sys.argv[3] == "-a")
 	if compareTree(inputFile1, inputFile2):
-		print "The files", inputFile1.GetName(), "and", inputFile2.GetName(), "are"
-		print "identical."
+		log.info("The files \"" + inputFile1.GetName() + "\" and \"" + inputFile2.GetName(), "\" are identical.")
 		exit(0)
 	else:
-		print "The files", inputFile1.GetName(), "and", inputFile2.GetName(), "are"
-		print "NOT identical."
+		log.warning("The files \"" + inputFile1.GetName() + "\" and \"" + inputFile2.GetName(), "\" are NOT identical.")
 		exit(1)
 
 
 def compareBin(histo1, histo2, idx):
 	result = True
 	if histo1.GetBinCenter(idx) != histo2.GetBinCenter(idx):
-		print "center of bin %d not identical: %f vs %f" % (idx, histo1.GetBinCenter(idx), histo2.GetBinCenter(idx))
+		log.info("center of bin %d not identical: %f vs %f" % (idx, histo1.GetBinCenter(idx), histo2.GetBinCenter(idx)))
 		if not opt_all:
 			return False
 		result = False
 	if histo1.GetBinWidth(idx) != histo2.GetBinWidth(idx):
-		print "widths of bin %d not identical: %f vs %f" % (idx, histo1.GetBinWidth(idx), histo2.GetBinWidth(idx))
+		log.info("widths of bin %d not identical: %f vs %f" % (idx, histo1.GetBinWidth(idx), histo2.GetBinWidth(idx)))
 		if not opt_all:
 			return False
 		result = False
 	if histo1.GetBinError(idx) != histo2.GetBinError(idx):
-		print "errors in bin %d not identical: %f vs %f" % (idx, histo1.GetBinError(idx), histo2.GetBinError(idx))
+		log.info("errors in bin %d not identical: %f vs %f" % (idx, histo1.GetBinError(idx), histo2.GetBinError(idx)))
 		if not opt_all:
 			return False
 		result = False
 	if histo1.GetBinContent(idx) != histo2.GetBinContent(idx):
-		print "content of bin %d not identical: %f vs %f" % (idx, histo1.GetBinContent(idx), histo2.GetBinContent(idx))
+		log.info("content of bin %d not identical: %f vs %f" % (idx, histo1.GetBinContent(idx), histo2.GetBinContent(idx)))
 		if not opt_all:
 			return False
 		result = False
@@ -60,7 +62,7 @@ def compareTH1(directory1, directory2, histoID):
 	histo1 = directory1.Get(histoID)
 	histo2 = directory2.Get(histoID)
 	if histo1.GetNbinsX() != histo2.GetNbinsX():
-		print "binning not identical"
+		log.info("binning not identical")
 		if not opt_all:
 			return False
 		result = False
@@ -77,7 +79,7 @@ def compareTH2(directory1, directory2, histoID):
 	histo1 = directory1.Get(histoID)
 	histo2 = directory2.Get(histoID)
 	if histo1.GetNbinsX() != histo2.GetNbinsX() or histo1.GetNbinsY() != histo2.GetNbinsY():
-		print "binning not identical"
+		log.info("binning not identical")
 		return False
 	try:
 		for binx in range(0, histo1.GetNbinsX() + 2):
@@ -85,12 +87,12 @@ def compareTH2(directory1, directory2, histoID):
 				idx1 = histo1.GetBin(binx, biny)
 				idx2 = histo2.GetBin(binx, biny)
 				if idx1 != idx2:
-					print "binning not identical"
+					log.info("binning not identical")
 					return False
 				if not compareBin(histo1, histo2, idx1):
 					return False
 	except:
-		print "general error"
+		log.info("general error")
 		return False
 	return True
 
@@ -99,7 +101,7 @@ def compareTH3(directory1, directory2, histoID):
 	histo1 = directory1.Get(histoID)
 	histo2 = directory2.Get(histoID)
 	if histo1.GetNbinsX() != histo2.GetNbinsX() or histo1.GetNbinsY() != histo2.GetNbinsY() or histo1.GetNbinsZ() != histo2.GetNbinsZ():
-		print "binning not identical"
+		log.info("binning not identical")
 		return False
 	try:
 		for binx in range(0, histo1.GetNbinsX() + 2):
@@ -108,12 +110,12 @@ def compareTH3(directory1, directory2, histoID):
 					idx1 = histo1.GetBin(binx, biny, binz)
 					idx2 = histo2.GetBin(binx, biny, binz)
 					if idx1 != idx2:
-						print "binning not identical"
+						log.info("binning not identical")
 						return False
 					if not compareBin(histo1, histo2, idx1):
 						return False
 	except:
-		print "general error"
+		log.info("general error")
 		return False
 	return True
 
@@ -123,7 +125,7 @@ def compareGraph(directory1, directory2, histoID):
 	histo1 = directory1.Get(histoID)
 	histo2 = directory2.Get(histoID)
 	if histo1.GetN() != histo2.GetN():
-		print "number of points not identical"
+		log.info("number of points not identical")
 		if not opt_all:
 			return False
 		result = False
@@ -145,12 +147,12 @@ def comparePoint(graph1, graph2, point):
 	graph1.GetPoint(point, x1, y1)
 	graph2.GetPoint(point, x2, y2)
 	if x1 != x2:
-		print "x value of point %d in %s not identical: %f vs %f" % (point, graph1.GetName(), x1, x2)
+		log.info("x value of point %d in %s not identical: %f vs %f" % (point, graph1.GetName(), x1, x2))
 		if not opt_all:
 			return False
 		result = False
 	if y1 != y2:
-		print "y value of point %d in %s not identical: %f vs %f" % (point, graph1.GetName(), y1, y2)
+		log.info("y value of point %d in %s not identical: %f vs %f" % (point, graph1.GetName(), y1, y2))
 		if not opt_all:
 			return False
 		result = False
@@ -159,12 +161,12 @@ def comparePoint(graph1, graph2, point):
 	x2 = graph2.GetErrorX(point)
 	y2 = graph2.GetErrorY(point)
 	if x1 != x2:
-		print "x error of point %d in %s not identical: %f vs %f" % (point, graph1.GetName(), x1, x2)
+		log.info("x error of point %d in %s not identical: %f vs %f" % (point, graph1.GetName(), x1, x2))
 		if not opt_all:
 			return False
 		result = False
 	if y1 != y2:
-		print "y error of point %d in %s not identical: %f vs %f" % (point, graph1.GetName(), y1, y2)
+		log.info("y error of point %d in %s not identical: %f vs %f" % (point, graph1.GetName(), y1, y2))
 		if not opt_all:
 			return False
 		result = False
@@ -177,7 +179,7 @@ def compareTree(directory1, directory2):
 		obj = directory1.Get(i.GetName())
 		obj2 = directory2.Get(i.GetName())
 		if not obj:
-			print "Object %s of type %s in %s is not available in first file" % (i.GetName(), obj2.IsA().GetName(), directory2.GetPath().split(":")[1])
+			log.info("Object %s of type %s in %s is not available in first file" % (i.GetName(), obj2.IsA().GetName(), directory2.GetPath().split(":")[1]))
 			if not opt_all:
 				return False
 			result = False
@@ -185,7 +187,7 @@ def compareTree(directory1, directory2):
 		obj = directory1.Get(i.GetName())
 		obj2 = directory2.Get(i.GetName())
 		if not obj2:
-			print "Object %s of type %s in %s is not available in second file" % (i.GetName(), obj.IsA().GetName(), directory1.GetPath().split(":")[1])
+			log.info("Object %s of type %s in %s is not available in second file" % (i.GetName(), obj.IsA().GetName(), directory1.GetPath().split(":")[1]))
 			if not opt_all:
 				return False
 			result = False
@@ -200,14 +202,14 @@ def compareTree(directory1, directory2):
 		if obj.IsA().GetName().startswith("TH1") or obj.IsA().GetName().startswith("TProfile"):
 			identified = True
 			if not compareTH1(directory1, directory2, i.GetName()):
-				print "problem with %s in %s" % (i.GetName(), directory1.GetPath().split(":")[1])
+				log.info("problem with %s in %s" % (i.GetName(), directory1.GetPath().split(":")[1]))
 				if not opt_all:
 					return False
 				result = False
 		if obj.IsA().GetName().startswith("TH2"):
 			identified = True
 			if not compareTH2(directory1, directory2, i.GetName()):
-				print "problem with %s in %s" % (i.GetName(), directory1.GetPath().split(":")[1])
+				log.info("problem with %s in %s" % (i.GetName(), directory1.GetPath().split(":")[1]))
 				if not opt_all:
 					return False
 				result = False
@@ -224,7 +226,7 @@ def compareTree(directory1, directory2):
 					return False
 				result = False
 		if not identified:
-			print "WARNING: %s of type %s in %s has not been checked" % (i.GetName(), obj.IsA().GetName(), directory1.GetPath().split(":")[1])
+			log.warning("%s of type %s in %s has not been checked" % (i.GetName(), obj.IsA().GetName(), directory1.GetPath().split(":")[1]))
 	return result
 
 
@@ -241,31 +243,30 @@ def compareNtuple(directory1, directory2, ntupleID):
 	nevts1 = ntuple1.GetEntries()
 	nevts2 = ntuple1.GetEntries()
 	if nleaves1 != nleaves2:
-		print "different number of leaves"
+		log.info("different number of leaves")
 		if not opt_all:
 			return False
 		result = False
 	for i in range(nleaves1):
 		if leaves1.UncheckedAt(i).GetName() != leaves2.UncheckedAt(i).GetName():
-			print "different leaf name:", leaves1.UncheckedAt(i).GetName(), leaves2.UncheckedAt(i).GetName()
+			log.info("different leaf name: " + leaves1.UncheckedAt(i).GetName() + ", " + leaves2.UncheckedAt(i).GetName())
 			if not opt_all:
 				return False
 			result = False
 	if nevts1 != nevts2:
-		print "different number of events"
+		log.info("different number of events")
 		if not opt_all:
 			return False
 		result = False
 	nevts = min(nevts1, nevts2)
 	for n in range(nevts):
 		if n % 1000 == 0:
-			print "\rEvent %d/%d (%1.1f%%)" % (n, nevts, n * 100. / nevts),
+			log.info("\rEvent %d/%d (%1.1f%%)" % (n, nevts, n * 100. / nevts))
 		ntuple1.GetEntry(n)
 		ntuple2.GetEntry(n)
 		for i in range(nleaves1):
 			if leaves1.UncheckedAt(i).GetValue() != leaves2.UncheckedAt(i).GetValue():
-				print "different leaf value:", leaves1.UncheckedAt(i).GetValue(), leaves2.UncheckedAt(i).GetValue(),
-				print "for name", leaves1.UncheckedAt(i).GetName()
+				log.info("different leaf value: " + leaves1.UncheckedAt(i).GetValue() + ", " + leaves2.UncheckedAt(i).GetValue() + " for name " + leaves1.UncheckedAt(i).GetName())
 				if not opt_all:
 					return False
 				result = False
