892539d1faa8ec3f83dc6a188f2e286ea62543e0
Georg Sieber <sieber@cern.ch>
2014-12-28 13:14:29 +0100

    Add new filter for good primary vertices

---
+++ KappaAnalysis/src/Filters/GoodPrimaryVertexFilter.cc
@@ -0,0 +1,33 @@
+
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/trim.hpp>
+
+#include "Artus/KappaAnalysis/interface/Filters/GoodPrimaryVertexFilter.h"
+
+
+std::string GoodPrimaryVertexFilter::GetFilterId() const {
+	return "GoodPrimaryVertexFilter";
+}
+
+void GoodPrimaryVertexFilter::Init(setting_type const& settings)
+{
+	FilterBase<KappaTypes>::Init(settings);
+	m_maxPrimaryVertexZ = settings.GetMaxPrimaryVertexZ();
+	m_maxPrimaryVertexRho = settings.GetMaxPrimaryVertexRho();
+	m_minPrimaryVertexFitnDOF = settings.GetMinPrimaryVertexFitnDOF();
+}
+
+bool GoodPrimaryVertexFilter::DoesEventPass(KappaEvent const& event, KappaProduct const& product,
+                                               KappaSettings const& settings) const 
+{
+	assert(event.m_vertexSummary);
+	bool pass = false;
+	if ((std::abs(event.m_vertexSummary->pv.position.Z()) < m_maxPrimaryVertexZ) &&
+	   (std::abs(event.m_vertexSummary->pv.position.Rho()) < m_maxPrimaryVertexRho) &&
+	   (event.m_vertexSummary->pv.nDOF >= m_minPrimaryVertexFitnDOF))
+	{
+		pass = true;
+	}
+	return pass;
+}
+
