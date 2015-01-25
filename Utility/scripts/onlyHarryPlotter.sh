58f4ec7e7bfe753aa07af4a841c469ce5a2cf0d1
Raphael Friese <raphael.friese@cern.ch>
2015-01-25 15:54:30 +0100

    remove script. Working checkout including the possibility to switch branches is not possible with a script. See https://twiki.cern.ch/twiki/bin/viewauth/CMS/KITHiggsAnalysisWorkflow for a recent recipe

--- Utility/scripts/onlyHarryPlotter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git config core.sparsecheckout true
-rm .git/info/sparse-checkout
-echo /HarryPlotter/ >> .git/info/sparse-checkout
-echo /Utility/python/ >> .git/info/sparse-checkout
-echo /Utility/scripts/ >> .git/info/sparse-checkout
-git checkout -f master
