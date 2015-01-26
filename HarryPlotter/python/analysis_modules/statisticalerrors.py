83d574511cf30fb85f56e6f2bb6e3d1f97f975a6
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-01-26 14:52:26 +0100

    HarryPlotter: statisticalerrors.py: improvement

--- HarryPlotter/python/analysis_modules/statisticalerrors.py
+++ HarryPlotter/python/analysis_modules/statisticalerrors.py
@@ -4,13 +4,10 @@ import logging
 import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
-import ROOT
+import Artus.HarryPlotter.analysis_modules.histogrammanipulationbase as histogrammanipulationbase
 
-import Artus.HarryPlotter.analysisbase as analysisbase
-
-
-class StatisticalErrors(analysisbase.AnalysisBase):
-	"""Plot statistical errors instead of actual bin contents."""
+class StatisticalErrors(histogrammanipulationbase.HistogramManipulationBase):
+	"""Replace bin contents of selected histograms with bin errors."""
 	def __init__(self):
 		super(StatisticalErrors, self).__init__()
 
@@ -18,32 +15,23 @@ class StatisticalErrors(analysisbase.AnalysisBase):
 		super(StatisticalErrors, self).modify_argument_parser(parser, args)
 
 		self.StatisticalErrors_options = parser.add_argument_group("StatisticalErrors options")
+		self.StatisticalErrors_options.add_argument("--stat-error", type=str, nargs='*', default=[],
+				help="List the nicks you want to make stat error (-> module whitelist ).")
 		self.StatisticalErrors_options.add_argument("--relative-error", action="store_true", default=False,
 				help="Do not plot absolute errors, but relative to bin content (in %).")
 
 	def prepare_args(self, parser, plotData):
 		super(StatisticalErrors, self).prepare_args(parser, plotData)
+		self.whitelist = plotData.plotdict['stat_error']
+		self.relative_error = plotData.plotdict['relative_error']
 
-		# modify y label
-		if plotData.plotdict["y_label"] == parser.get_default("y_label"):
-			plotData.plotdict["y_label"] = "statistical error"
-			if plotData.plotdict["relative_error"]:
-				plotData.plotdict["y_label"] += " [%]"
-
-	def run(self, plotData=None):
-		super(StatisticalErrors, self).run(plotData)
-
-		for nick, root_histogram in plotData.plotdict["root_objects"].iteritems():
-			# iterate over bins, replace contents with errors and divide by 
-			# previous content, if 'relative_error' is True:
-			for i in range(root_histogram.GetNbinsX()):
-				new_bin_content = root_histogram.GetBinError(i)
-				if plotData.plotdict["relative_error"]:
-					content = root_histogram.GetBinContent(i)
-					if content > 0:
-						new_bin_content *= (100./content)
-					else:
-						new_bin_content = 0
-				root_histogram.SetBinContent(i, new_bin_content)
-				root_histogram.SetBinError(i, 0)
-
+	def _manipulate_bin(self, histogram, global_bin):
+		new_bin_content = histogram.GetBinError(global_bin)
+		if self.relative_error:
+			content = histogram.GetBinContent(global_bin)
+			if content > 0:
+				new_bin_content *= (100./content)
+			else:
+				new_bin_content = 0
+		histogram.SetBinContent(global_bin, new_bin_content)
+		histogram.SetBinError(global_bin, 0)
