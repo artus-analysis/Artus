92aa570e9e50337ee82d110a0d92f2d05915aa23
Thomas Mueller <tmuller@cern.ch>
2015-02-04 16:26:47 +0100

    core.py: implemented returning of plot output files

--- HarryPlotter/python/core.py
+++ HarryPlotter/python/core.py
@@ -176,19 +176,24 @@ class HarryCore(object):
 			plotData.plotdict["export_json"] = "default" if plotData.plotdict["json_defaults"] is None else plotData.plotdict["json_defaults"][0]
 		
 		# prepare aguments for all processors before running them
+		output_filenames = []
 		for processor in self.processors:
 			tmpPlotData = copy.deepcopy(plotData) if isinstance(processor, PlotBase) else plotData
 			processor.prepare_args(self.parser, tmpPlotData)
 			processor.run(tmpPlotData)
-			if not isinstance(processor, PlotBase):
+			if isinstance(processor, PlotBase):
+				output_filenames.extend(tmpPlotData.plotdict.get("output_filenames", []))
+			else:
 				plotData = tmpPlotData
 		plotData = tmpPlotData
+		output_filenames = list(set(output_filenames))
 		
 		# export arguments into JSON file (2)
 		if plotData.plotdict["export_json"] != "default":
 			export_args.save(plotData.plotdict["export_json"], indent=4)
 		
 		del(plotData)
+		return output_filenames
 	
 	def register_processor(self, processor):
 		"""Add processor to list of available processors."""
--- HarryPlotter/python/harry.py
+++ HarryPlotter/python/harry.py
@@ -18,26 +18,27 @@ import Artus.HarryPlotter.core as harrycore
 
 def pool_plot(args):
 	try:
-		args[0].plot(*args[1:])
-		return None
+		return (args[0].plot(*args[1:]), None)
 	except SystemExit as e:
-		return args[1:]
+		return (None, args[1:])
 	except Exception as e:
 		log.info(str(e))
-		return args[1:]
+		return (None, args[1:])
 
 class HarryPlotter(object):
 	def __init__(self, list_of_config_dicts=None, list_of_args_strings=None, n_processes=1):
 		# load Kappa library # TODO: check if available
 		ROOT.gSystem.Load("libKappa")
 		
-		self.multi_plots(list_of_config_dicts=list_of_config_dicts,
-		                 list_of_args_strings=list_of_args_strings,
-		                 n_processes=n_processes)
+		self.output_filenames = self.multi_plots(
+				list_of_config_dicts=list_of_config_dicts,
+				list_of_args_strings=list_of_args_strings,
+				n_processes=n_processes
+		)
 	
 	def plot(self, harry_args):
 		harry_core = harrycore.HarryCore(args_from_script=harry_args)
-		harry_core.run()
+		return harry_core.run()
 	
 	def multi_plots(self, list_of_config_dicts, list_of_args_strings, n_processes=1):
 		config_dicts = list_of_config_dicts if isinstance(list_of_config_dicts, collections.Iterable) and not isinstance(list_of_config_dicts, basestring) else [list_of_config_dicts]
@@ -66,17 +67,18 @@ class HarryPlotter(object):
 					harry_args[-1] += (" "+args_string)
 		
 		# multi processing of multiple plots
+		output_filenames = []
 		failed_plots = []
 		if len(harry_args) > 1 and n_processes > 1:
 			pool = Pool(processes=n_processes)
 			results = pool.map_async(pool_plot, zip([self]*len(harry_args), harry_args))
-			failed_plots = [result for result in results.get() if not result is None and result != (None,)]
+			output_filenames, failed_plots = zip*([result for result in results.get() if not result is None and result != (None,)])
 		
 		# single processing of multiple plots
 		elif len(harry_args) > 1:
 			for harry_args in harry_args:
 				try:
-					self.plot(harry_args)
+					output_filenames.append(self.plot(harry_args))
 				except SystemExit as e:
 					failed_plots.append(harry_args)
 				except Exception as e:
@@ -85,10 +87,12 @@ class HarryPlotter(object):
 		
 		# single plot
 		elif len(harry_args) > 0:
-			self.plot(harry_args[0])
+			output_filenames.append(self.plot(harry_args[0]))
 		
 		if len(failed_plots) > 0:
 			log.error("%d failed plots:" % len(failed_plots))
 			for failed_plot in failed_plots:
 				log.info("\t%s" % failed_plot)
+		
+		return output_filenames
 
