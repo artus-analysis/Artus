a3be2c1e541ce5d45fcac824785f77bcc9193b27
Thomas Mueller <tmuller@cern.ch>
2015-02-13 15:53:42 +0100

    artusPipelines.py: allow for multiple arguments

--- Configuration/scripts/artusPipelines.py
+++ Configuration/scripts/artusPipelines.py
@@ -15,12 +15,13 @@ def main():
 	
 	parser = argparse.ArgumentParser(description="Print names of all pipelines from Artus JSON config", parents=[logger.loggingParser])
 	
-	parser.add_argument("config", help="Artus JSON config. Can be either Artus output root files or JSON text files")
+	parser.add_argument("configs", nargs="+",
+	                    help="Artus JSON configs. Can be either Artus output root files or JSON text files. Only first is considered.")
 
 	args = parser.parse_args()
 	logger.initLogger(args)
 	
-	config = jsonTools.JsonDict(args.config)
+	config = jsonTools.JsonDict(args.configs[0])
 	log.info("\n".join(sorted(config.get("Pipelines", {}).keys())))
 
 if __name__ == "__main__":
