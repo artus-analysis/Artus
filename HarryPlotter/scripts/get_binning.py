67ceac0313e405b7b7c4cd6e9b63c4bbbaab11a0
Thomas Mueller <tmuller@cern.ch>
2015-02-17 18:11:28 +0100

    get_binning.py: added script to retrieve binnins of histograms in ROOT files

---
+++ HarryPlotter/scripts/get_binning.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import argparse
+import os
+
+import ROOT
+ROOT.gROOT.SetBatch(True)
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gErrorIgnoreLevel = ROOT.kError
+
+import Artus.HarryPlotter.utility.roottools as roottools
+
+
+if __name__ == "__main__":
+	
+	parser = argparse.ArgumentParser(description="Print binning of histograms", parents=[logger.loggingParser])
+
+	parser.add_argument("root_file", help="Input ROOT file")
+
+	args = parser.parse_args()
+	logger.initLogger(args)
+	
+	root_file = ROOT.TFile(args.root_file, "READ")
+	elements = roottools.RootTools.walk_root_directory(root_file)
+	
+	for key, path in elements:
+		if key.GetClassName().startswith("TH"):
+			histogram = root_file.Get(path)
+			log.info("%s: %s" % (path, str(list(roottools.RootTools.get_binning(histogram)))))
