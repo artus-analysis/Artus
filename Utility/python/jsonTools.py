3924e800e459050854f09d84e8caed2d3642fb6a
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-02-24 12:58:13 +0100

    jsonTools.py: add a function to print comments from all json files in a directory

--- Utility/python/jsonTools.py
+++ Utility/python/jsonTools.py
@@ -331,3 +331,16 @@ class JsonDict(dict):
 			result = jsonDict
 		return result
 
+def print_comments_from_json_files(json_dir, comment_key):
+	"""print comments from all json files in a directory"""
+	#TODO generalize this. works with any string, not only comments
+
+	files = [os.path.join(json_dir, f) for f in os.listdir(json_dir) if f.endswith(".json")]
+
+	log.info(tools.get_colored_string("json files:", 'cyan'))
+	for f in files:
+		d = JsonDict(f).doIncludes().doComments()
+		string = "\t" + tools.get_colored_string(os.path.basename(f), "yellow")
+		if comment_key in d:
+			string += ("\n\t\t" + d[comment_key])
+		log.info(string)
