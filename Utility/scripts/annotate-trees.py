2b3be491b83562e30e301d9994dece71642f0c33
Thomas Mueller <tmuller@cern.ch>
2015-02-24 18:10:17 +0100

    annotate-trees.py: added script to add friend trees to existing trees

---
+++ Utility/scripts/annotate-trees.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import argparse
+import numpy
+import os
+
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gErrorIgnoreLevel = ROOT.kError
+
+import Artus.Utility.progressiterator as progressiterator
+
+
+def main():
+	
+	parser = argparse.ArgumentParser(description="Add friend to tree in file with constant values.",
+	                                 parents=[logger.loggingParser])
+	
+	parser.add_argument("files", nargs="+",
+	                    help="Files containing the tree. The files will be updated.")
+	parser.add_argument("-t", "--tree", required=True,
+	                    help="Path to the tree object in the file.")
+	parser.add_argument("--values", nargs="+", required=True,
+	                    help="Values to add to the friend tree. Each value gets a separate branch.")
+	parser.add_argument("-b", "--branches", nargs="+", default=[None],
+	                    help="Branch names.")
+	
+	args = parser.parse_args()
+	logger.initLogger(args)
+	
+	if len(args.branches) < len(args.values):
+		args.branches = (args.branches+(len(args.values)*[None]))[:len(args.values)]
+	args.branches = ["annotation%d" % i if b is None else b for i, b in enumerate(args.branches)]
+	
+	types = {
+		bool : "O",
+		int : "I",
+		float : "D",
+		str : "C",
+	}
+	value_types = []
+	for index, value in enumerate(args.values):
+		try:
+			value = int(value)
+			value_types.append(int)
+		except:
+			try:
+				value = float(value)
+				value_types.append(float)
+			except:
+				try:
+					# TODO: check if type is bool
+					#value = bool(value)
+					#value_types.append(bool)
+					raise Exception()
+				except:
+					# TODO: find way to branch strings
+					#value_types.append(str)
+					raise Exception()
+		args.values[index] = value
+	
+	log.info("New branches:")
+	for index, (branch, value_type, value) in enumerate(zip(args.branches, value_types, args.values)):
+		log.info("\t%s/%s = %s" % (branch, types[value_type], str(value)))
+	
+	for file_name in progressiterator.ProgressIterator(args.files, description="Processing files"):
+		root_file = ROOT.TFile(file_name, "UPDATE")
+		tree = root_file.Get(args.tree)
+		
+		dir_name = os.path.dirname(args.tree)
+		if not dir_name == "":
+			root_file.Get(dir_name)
+		
+		friend_tree = ROOT.TTree(tree.GetName()+"_friend", tree.GetTitle()+" (friend)")
+		values = []
+		for branch, value_type, value in zip(args.branches, value_types, args.values):
+			values.append(numpy.zeros(1, dtype=value_type))
+			values[-1][0] = value
+			friend_tree.Branch(branch, values[-1], "%s/%s" % (branch, types[value_type]))
+		
+		for entry in xrange(tree.GetEntries()):
+			friend_tree.Fill()
+		
+		friend_tree.AddFriend(tree, tree.GetName())
+		root_file.Write()
+		root_file.Close()
+		
+	
+if __name__ == "__main__":
+	main()
+
