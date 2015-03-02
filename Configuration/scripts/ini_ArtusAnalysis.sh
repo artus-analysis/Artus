ed041ded168b15c91d8d07667705f6222871f7c6
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-03-02 13:51:43 +0100

    some adjustments for compile with 'make'

--- Configuration/scripts/ini_ArtusAnalysis.sh
+++ Configuration/scripts/ini_ArtusAnalysis.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+
+# get path of Artus installation
+if [[ ${BASH_SOURCE[0]} == *"${CMSSW_BASE}/bin/"* ]]; then
+	export ARTUSPATH=$CMSSW_BASE/src/Artus
+else
+	# get Artus dir relative to location of this script
+	export ARTUSPATH=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]}))/../..)
+fi
+
+export KAPPAPATH=$(readlink -f $ARTUSPATH/../Kappa)
+export KAPPATOOLSPATH=$(readlink -f $ARTUSPATH/../KappaTools)
+
 # configurations needed for compilation of C++ code
 #export BOOSTPATH=$(ls /afs/cern.ch/cms/${SCRAM_ARCH}/external/boost/* -d | tail -n 1)/
-export LD_LIBRARY_PATH="$CMSSW_BASE/src/KappaTools/lib/:$CMSSW_BASE/src/Kappa/lib/:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$KAPPATOOLSPATH/lib/:$KAPPAPATH/lib/:$LD_LIBRARY_PATH"
 
-# useful to rediect messages
+# useful to redirect messages
 export USERPC=`who am i | sed 's/.*(\([^]]*\)).*/\1/g'`
-
-# set the environment
-export KAPPAPATH=$CMSSW_BASE/src/Kappa
-export KAPPATOOLSPATH=$CMSSW_BASE/KappaTools
-export ARTUSPATH=$CMSSW_BASE/src/Artus
 
 if [[ `hostname` == *naf* ]]; then
 	export ARTUS_WORK_BASE="/nfs/dust/cms/user/${USER}/artus/"
