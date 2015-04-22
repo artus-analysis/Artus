07686ee2bfb0fbeab33b3951c5bb46155a59772a
Thomas Mueller <tmuller@cern.ch>
2015-04-22 23:01:59 +0200

    labels.py: minor fix, comment out examples

--- HarryPlotter/python/utility/labels.py
+++ HarryPlotter/python/utility/labels.py
@@ -13,14 +13,17 @@ class LabelsDict(object):
 	def __init__(self, latex_version="latex", additional_labels=None):
 		self.labels_dict = {}
 		
+		# examples
+		"""
 		if latex_version == "root":
 			self.labels_dict["zpt"] = "Z p_{T} / GeV"
 			self.labels_dict["zmass"] = "m_{Z} / GeV"
 		else:
 			self.labels_dict["zpt"] = "Z $p_\mathrm{T}$ / GeV"
 			self.labels_dict["zmass"] = "$m_\mathrm{Z}$ / GeV"
+		"""
 		
-		if additional_labels != None:
+		if not additional_labels is None:
 			self.labels_dict.update(additional_labels)
 
 	def get_nice_label(self, label):
