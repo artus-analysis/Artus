685b72303c42b5874f71dcb06efff78c656405f9
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-11 16:43:24 +0200

    ini_harry_cmssw.sh: set sshpc to userpc if logged in from ekp

--- HarryPlotter/scripts/ini_harry_cmssw.sh
+++ HarryPlotter/scripts/ini_harry_cmssw.sh
@@ -8,8 +8,14 @@ export PYTHONPATH=$PYTHONPATH:${CMSSW_RELEASE_BASE}/../../../../slc5_amd64_gcc46
 if [ -z $HARRY_REMOTE_USER ]; then
 	export HARRY_REMOTE_USER=$USER
 fi
-export HARRY_SSHPC=ekplx26.physik.uni-karlsruhe.de
 export HARRY_USERPC=`who am i | sed 's/.*(\([^]]*\)).*/\1/g'`
+
+#if not logged in from ekplx, fall back to lx26
+if [[ $HARRY_USERPC == *"ekplx"* ]]; then
+	export HARRY_SSHPC=$HARRY_USERPC
+else
+	export HARRY_SSHPC=ekplx26.physik.uni-karlsruhe.de
+fi
 export HARRY_REMOTE_DIR=plots_archive
 export HARRY_REMOTE_PATH=/disks/ekpwww/web/${HARRY_REMOTE_USER}/public_html
 export HARRY_URL=http://www-ekp.physik.uni-karlsruhe.de/~${HARRY_REMOTE_USER}
