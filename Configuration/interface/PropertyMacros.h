40b450d5e36f005b8fc4ed68f75df821b6acdea6
Georg Sieber <sieber@cern.ch>
2015-07-06 17:19:53 +0200

    Additional magic by Th.H.

--- Configuration/interface/PropertyMacros.h
+++ Configuration/interface/PropertyMacros.h
@@ -16,6 +16,16 @@ TYPE const& GetRef##SNAME() const { return m_##SNAME; }     \
 TYPE const* GetPt##SNAME() const { return &m_##SNAME; }     \
 void Set##SNAME(TYPE val) { m_##SNAME = val; }
 
+#define IMPL_PROPERTY_INITIALIZE(TYPE, SNAME, VAL)          \
+private:                                                    \
+TYPE m_##SNAME = VAL;                                       \
+public:                                                     \
+TYPE Get##SNAME() const { return m_##SNAME; }               \
+bool Has##SNAME() const { return &m_##SNAME; }              \
+TYPE const& GetRef##SNAME() const { return m_##SNAME; }     \
+TYPE const* GetPt##SNAME() const { return &m_##SNAME; }     \
+void Set##SNAME(TYPE val) { m_##SNAME = val; }
+
 #define IMPL_PROPERTY_READONLY(TYPE, SNAME)                 \
 private:                                                    \
 TYPE m_##SNAME;                                             \
