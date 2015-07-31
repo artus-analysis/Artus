db444198bdd0942ed4f47bf05aa69303f24f7cf9
Max Fischer <max.fischer@kit.edu>
2015-07-31 10:30:49 +0200

    HARRY env setting now respects user overwrites and checks availability of WWW candidates

--- HarryPlotter/scripts/ini_harry_cmssw.sh
+++ HarryPlotter/scripts/ini_harry_cmssw.sh
@@ -8,14 +8,29 @@ export PYTHONPATH=$PYTHONPATH:${CMSSW_RELEASE_BASE}/../../../../slc5_amd64_gcc46
 if [ -z $HARRY_REMOTE_USER ]; then
 	export HARRY_REMOTE_USER=$USER
 fi
-export HARRY_USERPC=`who am i | sed 's/.*(\([^]]*\)).*/\1/g'`
 
-#if not logged in from ekplx, fall back to lx26
-if [[ $HARRY_USERPC == *"ekplx"* ]]; then
-	export HARRY_SSHPC=$HARRY_USERPC
-else
-	export HARRY_SSHPC=ekplx69.physik.uni-karlsruhe.de
+if [[ -z ${HARRY_USERPC} ]]; then
+	export HARRY_USERPC=`who am i | sed 's/.*(\([^]]*\)).*/\1/g'`
 fi
+
+if [[ -z ${HARRY_SSHPC} ]]; then
+	#if logged in from an ekplx host, assume it is reachable
+	if [[ $HARRY_USERPC == *"ekplx"* ]]; then
+		export HARRY_SSHPC=$HARRY_USERPC
+	else
+		# find a reachable host - try ekplxs in different offices
+		for lxid in 26 30 69 77 14 17 21 25; do
+			if ping -c 1 ekplx${lxid}.physik.uni-karlsruhe.de 2>/dev/null 1>&2; then
+				export HARRY_SSHPC=ekplx${lxid}.physik.uni-karlsruhe.de
+				break
+			fi
+		done
+	fi
+	if [[ -z ${HARRY_SSHPC} ]]; then
+		echo "Failed to find reachable \$HARRY_SSHP host" 1>&2
+	fi
+fi
+
 export HARRY_REMOTE_DIR=plots_archive
 export HARRY_REMOTE_PATH=/disks/ekpwww/web/${HARRY_REMOTE_USER}/public_html
 export HARRY_URL=http://www.ekp.kit.edu/~${HARRY_REMOTE_USER}
