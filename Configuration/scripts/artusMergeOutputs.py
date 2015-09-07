6a21e911a1808d268005e70db49fc1fb7f577194
Thomas Mueller <tmuller@cern.ch>
2015-09-07 19:50:27 +0200

    artusMergeOutputs.py: added multiprocessing

--- Configuration/scripts/artusMergeOutputs.py
+++ Configuration/scripts/artusMergeOutputs.py
@@ -14,7 +14,13 @@ ROOT.gROOT.SetBatch(True)
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 ROOT.gErrorIgnoreLevel = ROOT.kError
 
+import Artus.Utility.tools as tools
 import Artus.Utility.progressiterator as pi
+
+
+def _call_command(command):
+	log.debug(command)
+	logger.subprocessCall(command.split())
 
 
 def main():
@@ -22,6 +28,8 @@ def main():
 	parser = argparse.ArgumentParser(description="Merge Artus outputs per nick name.", parents=[logger.loggingParser])
 
 	parser.add_argument("project_dir", help="Artus Project directory containing the files \"output/*/*.root\" to merge")
+	parser.add_argument("-n", "--n-processes", type=int, default=1,
+	                    help="Number of (parallel) processes. [Default: %(default)s]")
 
 	args = parser.parse_args()
 	logger.initLogger(args)
@@ -31,6 +39,7 @@ def main():
 	outputs_per_nick = {nick : glob.glob(os.path.join(args.project_dir, "output", nick, "*.root")) for nick in nick_names}
 	outputs_per_nick = {nick : files for nick, files in outputs_per_nick.iteritems() if len(files) > 0}
 	
+	commands = []
 	for nick_name, output_files in pi.ProgressIterator(outputs_per_nick.iteritems(),
 	                                                   length=len(outputs_per_nick),
 	                                                   description="Merging Artus outputs"):
@@ -38,11 +47,9 @@ def main():
 		if not os.path.exists(merged_dir):
 			os.makedirs(merged_dir)
 	
-		command = "hadd -f %s %s" % (os.path.join(merged_dir, nick_name+".root"), " ".join(output_files))
-		log.info(command)
-		logger.subprocessCall(command.split())
-		log.info("\n")
-
+		commands.append("hadd -f %s %s" % (os.path.join(merged_dir, nick_name+".root"), " ".join(output_files)))
+	
+	tools.parallelize(_call_command, commands, n_processes=args.n_processes)
 
 if __name__ == "__main__":
 	main()
