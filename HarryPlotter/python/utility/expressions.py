87bdafcf3222e81fefc034e0cd634a801be4042a
Max Fischer <max.fischer@kit.edu>
2015-09-17 22:15:30 +0200

    added regex substitution for expressions

--- HarryPlotter/python/utility/expressions.py
+++ HarryPlotter/python/utility/expressions.py
@@ -1,6 +1,7 @@
 
 # -*- coding: utf-8 -*-
 
+import re
 import logging
 import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
@@ -12,9 +13,11 @@ log = logging.getLogger(__name__)
 class ExpressionsDict(object):
 	def __init__(self, additional_expressions=None):
 		self.expressions_dict = {}
-		
-		# example:
+		# mapping name => formula; example:
 		# self.expressions_dict['deltaphijet1jet2'] = '(abs(abs(abs(jet1phi-jet2phi)-TMath::Pi())-TMath::Pi()))'
+		self.expressions_regex = []
+		# list[tuple] signature => formula; example:
+		# self.expressions_regex.append(r"delta_phi\((\w+),(\w+)\)", r'(abs(abs(abs(\1phi-\2phi)-TMath::Pi())-TMath::Pi()))')
 		
 		if not additional_expressions is None:
 			self.expressions_dict.update(additional_expressions)
@@ -23,10 +26,24 @@ class ExpressionsDict(object):
 		return self.expressions_dict.get(expressions.lower(), expression)
 
 	def replace_expressions(self, expression):
-		"""Replace any occurence of a dictionary key in the given string."""
-		for key, value in self.expressions_dict.iteritems():
-			try:
-				expression = expression.replace(key, value)
-			except AttributeError:
-				return expression
-		return expression
+		"""Replace any known expressions"""
+		# old-style behaviour
+		if not self.expressions_dict:
+			for key, value in self.expressions_dict.iteritems():
+				try:
+					expression = new_expr.replace(key, value)
+				except AttributeError:
+					return expression
+			return expression
+		# iterative substitution
+		if expression is None:
+			return expression
+		old_expr, new_expr = "", expression
+		while new_expr != old_expr:
+			old_expr = new_expr
+			for key, value in self.expressions_dict.iteritems():
+				new_expr = new_expr.replace(key, value)
+			for pattern, repl in self.expressions_regex:
+				new_expr = re.sub(pattern, repl, new_expr)
+		log.debug("expression expansion: %r => %r", expression, new_expr)
+		return new_expr
