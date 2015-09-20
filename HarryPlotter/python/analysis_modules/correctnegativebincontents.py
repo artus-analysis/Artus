ee195bec5decea63dbf07656a6c735227f74ba9b
Thomas Mueller <tmuller@cern.ch>
2015-09-20 22:34:10 +0200

    correctnegativebincontents.py: derive from HistogramManipulationBase, change default to work on all nicks, disable passing whitespace-sparated lists of nicks

--- HarryPlotter/python/analysis_modules/correctnegativebincontents.py
+++ HarryPlotter/python/analysis_modules/correctnegativebincontents.py
@@ -9,10 +9,11 @@ log = logging.getLogger(__name__)
 
 import ROOT
 
-import Artus.HarryPlotter.analysisbase as analysisbase
+import Artus.HarryPlotter.analysis_modules.histogrammanipulationbase as histogrammanipulationbase
 
 
-class CorrectNegativeBinContents(analysisbase.AnalysisBase):
+class CorrectNegativeBinContents(histogrammanipulationbase.HistogramManipulationBase):
+	"""Set bin errors of negative bins to 0 while preserving the integral of histograms."""
 
 	def __init__(self):
 		super(CorrectNegativeBinContents, self).__init__()
@@ -21,41 +22,32 @@ class CorrectNegativeBinContents(analysisbase.AnalysisBase):
 		super(CorrectNegativeBinContents, self).modify_argument_parser(parser, args)
 
 		self.correctnegbincontent_options = parser.add_argument_group("CorrectNegativeBinContents options")
-		self.correctnegbincontent_options.add_argument("--nicks-correct-negative-bins", nargs="+",
-							       default=[],
-							       help="Nicks of histograms to be corrected. [Default: %(default)s]")
+		self.correctnegbincontent_options.add_argument(
+				"--nicks-correct-negative-bins", nargs="+", default=[],
+				help="Nicks of histograms to be corrected. [Default: all]"
+		)
 	
 	def prepare_args(self, parser, plotData):
 		super(CorrectNegativeBinContents, self).prepare_args(parser, plotData)
-
-		plotData.plotdict["nicks_correct_negative_bins"] = [nicks.split() for nicks in plotData.plotdict["nicks_correct_negative_bins"]]
-		if len(plotData.plotdict["nicks_correct_negative_bins"]) == 0:
-			log.warning("CorrectNegativeBinContents module called, but no nicks of the histograms to be corrected are provided")
-	
-	def run(self, plotData=None):
-		super(CorrectNegativeBinContents, self).run(plotData)
 		
-		# loop over all histograms
-		for nicks_correct_negative_bins in plotData.plotdict["nicks_correct_negative_bins"]:
-			for nick in nicks_correct_negative_bins:
-				root_histogram = plotData.plotdict["root_objects"][nick]
-				if isinstance(root_histogram, ROOT.TH1):
-					original_integral = root_histogram.Integral()
-					negative_bin_contents_found = False
-				
-					# loop over all bins
-					for x_bin in xrange(1, root_histogram.GetNbinsX()+1):
-						for y_bin in xrange(1, root_histogram.GetNbinsY()+1):
-							for z_bin in xrange(1, root_histogram.GetNbinsY()+1):
-								global_bin = root_histogram.GetBin(x_bin, y_bin, z_bin)
-							
-								# set negative bin contents to zero
-								if root_histogram.GetBinContent(global_bin) < 0.0:
-									root_histogram.SetBinContent(global_bin, 0.0)
-									negative_bin_contents_found = True
-				
-					# preserve original integral
-					if negative_bin_contents_found:
-						root_histogram.Scale(abs(original_integral / root_histogram.Integral()))
-
+		if len(plotData.plotdict["nicks_correct_negative_bins"]) > 0:
+			self.whitelist = plotData.plotdict["nicks_correct_negative_bins"]
+		else:
+			self.whitelist = plotData.plotdict["nicks"]
+	
+	def _selector(self, nick, root_histogram, plotData):
+		if isinstance(root_histogram, ROOT.TH1):
+			self.original_integral = root_histogram.Integral()
+		else:
+			return False
+		return super(CorrectNegativeBinContents, self)._selector(nick, root_histogram, plotData)
+	
+	def _manipulate_bin(self, histogram, global_bin):
+		if histogram.GetBinContent(global_bin) < 0.0:
+			histogram.SetBinContent(global_bin, 0.0)
+			
+			# preserve original integral
+			new_integral = histogram.Integral()
+			if new_integral > 0.0:
+				histogram.Scale(abs(self.original_integral / new_integral))
 
