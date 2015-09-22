e780a5e7b14ae68503145cc79ff5b55dc633b346
Joram Berger <joram.berger@cern.ch>
2015-09-22 19:15:20 +0200

    Remove extra path for HarryPlotter
    This part of the python path was only needed in older CMSSW versions and
    is now obsolete.

--- HarryPlotter/scripts/ini_harry_cmssw.sh
+++ HarryPlotter/scripts/ini_harry_cmssw.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# This line fixes a bug in CMSSW6. The dateutils are not present there. This uses the dateutils from python 2.6
-export PYTHONPATH=$PYTHONPATH:${CMSSW_RELEASE_BASE}/../../../../slc5_amd64_gcc462/external/py2-matplotlib/1.0.1/lib/python2.6/site-packages/
-
 # set some variables needed for --userpc and web plotting
 # set HARRY_REMOTE_USER manually if you have a different user name on the web plot server
 if [ -z $HARRY_REMOTE_USER ]; then
