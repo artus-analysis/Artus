8eb5fa48aecf9d51022d312c5c972e9ce7f1e324
Raphael Friese <raphael.friese@cern.ch>
2015-09-28 20:49:44 +0200

    allow merging of files from someone else

--- Configuration/scripts/artusMergeOutputs.py
+++ Configuration/scripts/artusMergeOutputs.py
@@ -30,10 +30,9 @@ def main():
 	parser.add_argument("project_dir", help="Artus Project directory containing the files \"output/*/*.root\" to merge")
 	parser.add_argument("-n", "--n-processes", type=int, default=1,
 	                    help="Number of (parallel) processes. [Default: %(default)s]")
-
+	parser.add_argument("--output-dir", help="Directory to store merged files. Default: Same as project_dir.")
 	args = parser.parse_args()
 	logger.initLogger(args)
-	
 	output_dirs = glob.glob(os.path.join(args.project_dir, "output/*"))
 	nick_names = [nick for nick in [output_dir[output_dir.rfind("/")+1:] for output_dir in output_dirs] if not ".tar.gz" in nick]
 	outputs_per_nick = {nick : glob.glob(os.path.join(args.project_dir, "output", nick, "*.root")) for nick in nick_names}
@@ -43,7 +42,7 @@ def main():
 	for nick_name, output_files in pi.ProgressIterator(outputs_per_nick.iteritems(),
 	                                                   length=len(outputs_per_nick),
 	                                                   description="Merging Artus outputs"):
-		merged_dir = os.path.join(args.project_dir, "merged", nick_name)
+		merged_dir = os.path.join(args.project_dir if(args.output_dir == None) else args.output_dir, "merged", nick_name)
 		if not os.path.exists(merged_dir):
 			os.makedirs(merged_dir)
 	
