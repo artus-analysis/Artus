f2025c9ab7ea7371de6fddb783953e4aa4b998d3
Max Fischer <max.fischer@kit.edu>
2015-10-19 18:11:24 +0200

    HarryPlotter: runranges and labels can no longer be drawn outside of plot

--- HarryPlotter/python/plot_modules/plotmplrunranges.py
+++ HarryPlotter/python/plot_modules/plotmplrunranges.py
@@ -24,12 +24,16 @@ class PlotMplRunRanges(plotbase.PlotBase):
 			self.runs = [[' 2015A', 248036.], [' 2015B', 251162.], [' 2015C', 253888.], [' 2015D', 256629.]]
 
 	def run(self, plotData):
+		x_min, x_max = plotData.plot.axes[0].dataLim.min[0], plotData.plot.axes[0].dataLim.max[0]
 		for [runlabel, runnumber] in self.runs:
 			# TODO make drawing options configurable?
-			plotData.plot.axes[0].text(
-				(runnumber - plotData.plot.axes[0].dataLim.min[0]) / (plotData.plot.axes[0].dataLim.max[0] - plotData.plot.axes[0].dataLim.min[0]),
-				0.92, runlabel, transform=plotData.plot.axes[0].transAxes, va='top',
-				ha='left', color='gray', alpha=0.7, size='medium'
-			)
-			for ax in plotData.plot.axes:
-				ax.axvline(runnumber, color='gray', linestyle='--', alpha=0.2)
+			if x_min <= runnumber <= x_max:
+				x_pos = (runnumber - x_min) / (x_max - x_min)
+				if 0 < x_pos < 0.95:
+					plotData.plot.axes[0].text(
+						x_pos,
+						0.92, runlabel, transform=plotData.plot.axes[0].transAxes, va='top',
+						ha='left', color='gray', alpha=0.7, size='medium'
+					)
+				for ax in plotData.plot.axes:
+					ax.axvline(runnumber, color='gray', linestyle='--', alpha=0.2)
