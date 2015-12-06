8c277e51ac4ab73b16c8ef657fcff4d7251fcf93
Georg Sieber <g.sieber@gmail.com>
2015-12-06 22:47:48 +0100

    update safemap tests.

--- Test/test/SafeMap_t.h
+++ Test/test/SafeMap_t.h
@@ -15,29 +15,33 @@
 
 class SomeClass {};
 
-BOOST_AUTO_TEST_CASE( test_safemap )
+BOOST_AUTO_TEST_CASE(test_safemap)
 {
     {
         // test with std::map
-        std::map < int, std::string > myMap;
+        std::map<int, std::string> myMap;
 
-        myMap [23] = "23";
-        myMap [42] = "42";
+        myMap[23] = "23";
+        myMap[42] = "42";
 
-        std::string const & rr1 = SafeMap::Get ( myMap, 42 );
-        BOOST_CHECK( rr1 == "42" );
-        std::string const & rr2 = SafeMap::GetWithDefault ( myMap, 43, std::string("42") );
-        BOOST_CHECK( rr2 == std::string("42") );
+        std::string rr1 = SafeMap::Get(myMap, 42);
+        BOOST_CHECK(rr1 == "42");
+
+        std::string rr2DefValue("42");
+        std::string rr2 = SafeMap::GetWithDefault(myMap, 43, rr2DefValue);
+        BOOST_CHECK(rr2 == std::string("42"));
+        BOOST_CHECK(&rr2 != &rr2DefValue);
+
     }
     {
         // test with boost::ptr_map
-        boost::ptr_map < int, std::string > myMapPtr;
+        boost::ptr_map<int, std::string> myMapPtr;
 
-        myMapPtr [ 23 ] = "23";
-        myMapPtr [ 42 ] = "42";
+        myMapPtr[23] = "23";
+        myMapPtr[42] = "42";
 
-        std::string const& rrPtr = SafeMap::GetPtrMap ( myMapPtr, 23 );
-        BOOST_CHECK( rrPtr == "23" );
+        std::string rrPtr = SafeMap::GetPtrMap(myMapPtr, 23);
+        BOOST_CHECK(rrPtr == "23");
     }
 }
 
