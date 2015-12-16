f26cf1ff4a1fd9cf7b4972ca2116ccf3855a9f16
Georg Sieber <g.sieber@gmail.com>
2015-12-16 09:48:03 +0100

    this should readd the stracktrace. Did not try it.

--- Utility/interface/ArtusLogging.h
+++ Utility/interface/ArtusLogging.h
@@ -2,9 +2,9 @@
 
 // include this file, if you want to have logging output in your compilation unit
 
-#define _ELPP_NO_DEFAULT_LOG_FILE
-#define _ELPP_DISABLE_VERBOSE_LOGS
-#define _ELPP_DISABLE_TRACE_LOGS
-#define _ELPP_STACKTRACE_ON_CRASH
+#define ELPP_NO_DEFAULT_LOG_FILE
+#define ELPP_DISABLE_VERBOSE_LOGS
+#define ELPP_DISABLE_TRACE_LOGS
+#define ELPP_STACKTRACE_ON_CRASH
 #include "Artus/Utility/interface/easylogging++.h"
 
