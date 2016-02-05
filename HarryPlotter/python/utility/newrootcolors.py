2e6f8889365402deb5bfa685fccb881f2a3fa756
Fabio Colombo <fabio.colombo@cern.ch>
2016-02-05 11:09:31 +0100

    Bugfix in newrootcolors Solves some weird memory leak which appears when doing many plots.

--- HarryPlotter/python/utility/newrootcolors.py
+++ HarryPlotter/python/utility/newrootcolors.py
@@ -4,7 +4,7 @@ newIdx = {}
 
 # transparent grey for uncertainty bands (as in HIG-14-034)
 colorToAdapt = ROOT.gROOT.GetColor(12)
-transGrey_idx = ROOT.gROOT.GetListOfColors().GetSize() + 1
+transGrey_idx = ROOT.gROOT.GetListOfColors().GetLast() + 1
 transGrey = ROOT.TColor(transGrey_idx, colorToAdapt.GetRed(), colorToAdapt.GetGreen(), colorToAdapt.GetBlue(), "", 0.5)
 
 newIdx["transgrey"] = transGrey_idx
