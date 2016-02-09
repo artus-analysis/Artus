13664787b1407a5071b72b5209cca4380fc90db3
dhaitz <dhaitz@cern.ch>
2016-02-09 11:41:15 +0100

    unfolding.py: add matrix inversion method, improved documentation

--- HarryPlotter/python/analysis_modules/unfolding.py
+++ HarryPlotter/python/analysis_modules/unfolding.py
@@ -11,8 +11,9 @@ import Artus.HarryPlotter.analysisbase as analysisbase
 
 
 class Unfolding(analysisbase.AnalysisBase):
-	"""This module implements a basic unfolding procedure."""
-	"""Needed are
+	"""This module implements an unfolding procedure based on RooUnfold.
+	
+	Needed are
         - The histogram to be unfolded (nick)
         - MC gen-level histogram
         - MC reco-level histogram
@@ -23,8 +24,21 @@ class Unfolding(analysisbase.AnalysisBase):
             by e.g. one sigma up or down (variation = +1 or -1)
 
         see http://hepunx.rl.ac.uk/~adye/software/unfold/RooUnfold.html
-	"""
 
+        Example config = {
+            'analysis_modules': ['Unfolding'],
+            # configure input and output nicks:
+            'unfolding': ['nick_data'],
+            'unfolding_mc_gen': 'nick_mc_gen',
+            'unfolding_mc_reco': 'mc_reco',
+            'unfolding_new_nicks': ['data_unfolded'],
+            'unfolding_responsematrix': 'nick_responsematrix',  # 2D histo, others are 1D
+            # method and lib settings
+            'unfolding_method': 'dagostini',
+            'unfolding_iterations': 4,
+            'libRooUnfold': '~/home/RooUnfold/libRooUnfold.so',
+        }
+	"""
 
 	def modify_argument_parser(self, parser, args):
 		super(Unfolding, self).modify_argument_parser(parser, args)
@@ -47,7 +61,7 @@ class Unfolding(analysisbase.AnalysisBase):
 		self.Unfolding_options.add_argument("--unfolding-iterations", type=int, default=4,
 				help="Number of iterations for unfolding")
 		self.Unfolding_options.add_argument("--unfolding-method", type=str, default="dagostini",
-				choices=['dagostini', 'binbybin', 'svd'],
+				choices=['dagostini', 'svd', 'binbybin', 'inversion'],
 				help="Method for unfolding. [Default: %(default)s]")
 
 	def prepare_args(self, parser, plotData):
@@ -72,7 +86,7 @@ class Unfolding(analysisbase.AnalysisBase):
 				plotData.plotdict["unfolding_variation"],
 				plotData.plotdict["unfolding_new_nicks"],
 		):
-			unfolded_histo = doUnfolding(
+			unfolded_histo = self.do_unfolding(
 				plotData.plotdict["root_objects"][matrix],
 				plotData.plotdict["root_objects"][reco],
 				plotData.plotdict["root_objects"][gen],
@@ -86,27 +100,29 @@ class Unfolding(analysisbase.AnalysisBase):
 			plotData.plotdict["nicks"].append(new_nick)
 
 
-def doUnfolding(hResponse_input, hMeas, hTrue, hData, method, libRooUnfold_path, variation=0, iterations=4):
-	"""return unfolded distribution"""
-	ROOT.gSystem.Load(libRooUnfold_path)
+	def do_unfolding(self, hResponse_input, hMeas, hTrue, hData, method, libRooUnfold_path, variation=0, iterations=4):
+		"""return unfolded distribution"""
+		ROOT.gSystem.Load(libRooUnfold_path)
 
-	hResponse = copy.deepcopy(hResponse_input)
+		hResponse = copy.deepcopy(hResponse_input)
 
-	# variation
-	if variation is not 0:
-		for x in range(hResponse.GetNbinsX()):
-			for y in range(hResponse.GetNbinsY()):
-				hResponse.SetBinContent(x, y, hResponse.GetBinContent(x, y) + variation * hResponse.GetBinError(x, y))
+		# variation (+/- one sigma of response matrix entries)
+		if variation is not 0:
+			for x in range(hResponse.GetNbinsX()):
+				for y in range(hResponse.GetNbinsY()):
+					hResponse.SetBinContent(x, y, hResponse.GetBinContent(x, y) + variation * hResponse.GetBinError(x, y))
 
-	response = ROOT.RooUnfoldResponse(hMeas, hTrue, hResponse, hMeas.GetName(), hMeas.GetTitle())
+		response = ROOT.RooUnfoldResponse(hMeas, hTrue, hResponse, hMeas.GetName(), hMeas.GetTitle())
 
-	#TODO make this configurable
-	if method == 'dagostini':
-		unfold = ROOT.RooUnfoldBayes(response, hData, iterations)
-	elif method == 'svd':
-		unfold = ROOT.RooUnfoldSvd(response, hData, iterations)  # singular value decomposition
-	elif method == 'binbybin':
-		unfold = ROOT.RooUnfoldBinByBin(response, hData)  # bin-by-bin (simple correction factors)
+		unfolding_methods = {
+			'dagostini': ROOT.RooUnfoldBayes,  # iterative dagostini
+			'svd': ROOT.RooUnfoldSvd,  # singular value decomposition
+			'binbybin': ROOT.RooUnfoldBinByBin,  # bin-by-bin (simple correction factors)
+			'inversion': ROOT.RooUnfoldInvert,  # inversion
+		}
 
-	hReco = unfold.Hreco()
-	return hReco
+		if method in ['dagostini', 'svd']:
+			unfold = unfolding_methods[method](response, hData, iterations)
+		else:
+			unfold = unfolding_methods[method](response, hData)
+		return unfold.Hreco()
