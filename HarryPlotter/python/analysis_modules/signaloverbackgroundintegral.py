0e840d2b2a4068e7392d0f05a354deda080bd779
MarcusSchmitt <Marcus.Schmitt@cern.ch>
2016-04-12 15:17:24 +0200

    Behaviour changed, if you already used it and it's broken please contact me

--- HarryPlotter/python/analysis_modules/signaloverbackgroundintegral.py
+++ HarryPlotter/python/analysis_modules/signaloverbackgroundintegral.py
@@ -6,7 +6,7 @@
 import logging
 import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
-
+import os
 import hashlib
 import ROOT
 import math
@@ -28,27 +28,29 @@ class SignalOverBackgroundIntegral(analysisbase.AnalysisBase):
 				help="Nick names of the signal histograms to sum up, whitespace separated")
 		self.sig_over_bkg_integral.add_argument("--sob-integral-result-nicks", nargs="+", default=["sob_integral_ratio"],
 				help="Nick names for the resulting histograms.")
-		self.sig_over_bkg_integral.add_argument("--sob-integral-method", nargs="+", default=["soversqrtb"], choices=["ams", "soversqrtb", "soverb"],
+		self.sig_over_bkg_integral.add_argument("--sob-integral-method", nargs="+", default=["righttoleft"], choices=["righttoleft", "rcombination"],
 				help="Desired method to calculate sob_integral condition.")
+		self.sig_over_bkg_integral.add_argument("--sob_integral_outputs", nargs="+", default=[None],
+				help="Desired outputfile to save calculated min/max, None is no output written .")
 
 	def prepare_args(self, parser, plotData):
 		super(SignalOverBackgroundIntegral, self).prepare_args(parser, plotData)
 
 
-		self.prepare_list_args(plotData, ["sob_integral_background_nicks", "sob_integral_signal_nicks", "sob_integral_result_nicks", "sob_integral_method"])
+		self.prepare_list_args(plotData, ["sob_integral_background_nicks", "sob_integral_signal_nicks", "sob_integral_result_nicks", "sob_integral_method", "sob_integral_outputs"])
 
 	def run(self, plotData=None):
 		super(SignalOverBackgroundIntegral, self).run(plotData)
-		max_y_bin = 0
-		y_max = -1.0
-		found_trans = False
-		trans_y_bin = 0
-		trans_y_value = 0
-		for signal_nick, background_nick, result_nick, method in  zip(
+		rtl_marks = []
+		rtl_yvalues = []
+		ltr_marks = []
+		ltr_yvalues = []
+		for signal_nick, background_nick, result_nick, method, outpath in  zip(
 																	plotData.plotdict["sob_integral_signal_nicks"],
 																	plotData.plotdict["sob_integral_background_nicks"],
 																	plotData.plotdict["sob_integral_result_nicks"],
-																	plotData.plotdict["sob_integral_method"]):
+																	plotData.plotdict["sob_integral_method"],
+																	plotData.plotdict["sob_integral_outputs"]):
 
 			signal = SumOfHistograms.return_sum_of_histograms([plotData.plotdict["root_objects"][nick] for nick in signal_nick.split(" ")])
 			#new_histogram = signal.Clone("storage_histogram")
@@ -56,57 +58,79 @@ class SignalOverBackgroundIntegral(analysisbase.AnalysisBase):
 
 			if(signal.GetNbinsX() != background.GetNbinsX()):
 				log.fatal("Signal histogram " + signal + " has a different binning than " + background )
-
 			#for x_bin in xrange(1, signal.GetNbinsX()+1):
 				#new_histogram.SetBinContent(x_bin, expression( signal.GetBinContent(x_bin), background.GetBinContent(x_bin), regularization_parameter))
+			if len(rtl_marks)+len(ltr_marks) == 0:
+				rtl_marks.append(signal.GetNbinsX()+1)
+				bin_max = signal.GetNbinsX()
+				ltr_marks.append(1)
+				bin_min = 1
+			elif len(rtl_marks) == 0:
+				rtl_marks.append(signal.GetNbinsX()+1)
+				bin_max = signal.GetNbinsX()
+			elif len(ltr_marks) == 0:
+				ltr_marks.append(1)
+				bin_min = 1
+			else:
+				bin_max = signal.GetNbinsX()
+				bin_min = 1
 
 			output_histogram = signal.Clone(result_nick)
 			sig_events = 0.0
 			bkg_events = 0.0
-			bin_max = signal.GetNbinsX()
+
 			if method == "righttoleft":
-				for x_bin in xrange(0, bin_max):
-					sig_events += signal.GetBinContent(bin_max-x_bin)
-					bkg_events += background.GetBinContent(bin_max-x_bin)
+				y_values = [0,]
+				x_values = [1,]
+				#iterate over full range histogram
+				for x_bin in xrange(bin_max, bin_min-1,-1):
+					#reset calculation at every maximum mark and restart calculating
+					for mark in rtl_marks:
+						if x_bin == mark-1:
+							sig_events = 0.0
+							bkg_events = 0.0
+					sig_events += signal.GetBinContent(x_bin)
+					bkg_events += background.GetBinContent(x_bin)
+					value = 0
 					if bkg_events > 0:
 						value = sig_events/bkg_events**0.5
-						output_histogram.SetBinContent(bin_max-x_bin, value)
-						if value > y_max:
-							max_y_bin = bin_max-x_bin
-							y_max = value
-					else:
-						output_histogram.SetBinContent(bin_max-x_bin, 0)
-
-			elif method == "lefttoright":
-				found_trans = False
-				trans_y_bin = 0
-				trans_y_value = 0
-				for x_bin in xrange(1, bin_max+1):
-					sig_events += signal.GetBinContent(x_bin)
-					bkg_events += background.GetBinContent(x_bin)
-					if bkg_events > 0:
-						output_histogram.SetBinContent(x_bin, sig_events/bkg_events**0.5)
-						if not found_trans and sig_events/bkg_events**0.5 > 0.15:
-							found_trans = True
-							trans_y_bin = x_bin
-							trans_y_value = sig_events/bkg_events**0.5
-					else:
-						output_histogram.SetBinContent(x_bin, 0)
-
-			elif method == "combination":
-				for x_bin in range(trans_y_bin, max_y_bin):
-					sig_events += signal.GetBinContent(x_bin)
-					bkg_events += background.GetBinContent(x_bin)
-				value = sig_events/bkg_events**0.5
-				for x_bin in range(1, bin_max+1):
-					if x_bin < trans_y_bin:
-						output_histogram.SetBinContent(x_bin, trans_y_value)
-					elif trans_y_bin <= x_bin and x_bin < max_y_bin:
 						output_histogram.SetBinContent(x_bin, value)
-					elif max_y_bin <= x_bin:
-						output_histogram.SetBinContent(x_bin, y_max)
 					else:
 						output_histogram.SetBinContent(x_bin, 0)
-
+					if x_bin < rtl_marks[-1]:
+						y_values.append(value)
+						x_values.append(x_bin)
+				y_max = max(y_values)
+				x_bin = x_values[y_values.index(y_max)]
+				rtl_marks.append(x_bin)
+				rtl_yvalues.append(y_max)
+			elif method == "rcombination":
+				rtl_marks.pop(-1)
+				rtl_yvalues.pop(-1)
+				sig_evt = 0
+				bkg_evt = 0
+				for x_bin in range(1, rtl_marks[-1]):
+					sig_events += signal.GetBinContent(x_bin)
+					bkg_events += background.GetBinContent(x_bin)
+				if bkg_events > 0:
+					rtl_yvalues.append(sig_events/bkg_events**0.5)
+				else:
+					rtl_yvalues.append(0)
+				rtl_marks.append(1)
+				rtl_yvalues.reverse()
+				rtl_marks.reverse()
+				for x_bin in range(1, signal.GetNbinsX()+1,1):
+					for left, right, value in zip(rtl_marks[:-1], rtl_marks[1:], rtl_yvalues):
+						if left <= x_bin and x_bin < right:
+							output_histogram.SetBinContent(x_bin, value)
+				dirpath, filename = os.path.split(outpath)
+				if not os.path.isdir(dirpath):
+					os.makedirs(dirpath)
+				if not os.path.exists(outpath):
+					with open(outpath, "w") as outfile:
+						outfile.write("%s : %s\n"%(plotData.plotdict["x_expressions"][0], " ".join(["%s"%(signal.GetBinLowEdge(x)) for x in rtl_marks])))
+				else:
+					with open(outpath, "a") as outfile:
+						outfile.write("%s : %s\n"%(plotData.plotdict["x_expressions"][0], " ".join(["%s"%(signal.GetBinLowEdge(x)) for x in rtl_marks])))
 			plotData.plotdict["root_objects"][result_nick] = output_histogram
 			plotData.plotdict["nicks"].append(result_nick)
