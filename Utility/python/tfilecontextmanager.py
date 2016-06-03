cd680683334aefe78a490cb08695bedb36aad0db
Max Fischer <max.fischer@kit.edu>
2016-06-03 11:39:08 +0200

    TFileContextManager opens file in initializer
    Replicates python's ``open`` behavior. This allows explicitly using the TFile in recovery scenarios.

--- Utility/python/tfilecontextmanager.py
+++ Utility/python/tfilecontextmanager.py
@@ -5,10 +5,10 @@ import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
 """
-	copied from https://github.com/ksamdev/exo_plots/blob/master/root/tfile.py
+Based on https://github.com/ksamdev/exo_plots/blob/master/root/tfile.py
 
-	Created by Samvel Khalatyan, Feb 16, 2012
-	Copyright 2011, All rights reserved
+Created by Samvel Khalatyan, Feb 16, 2012
+Copyright 2011, All rights reserved
 """	
 
 import ROOT
@@ -17,35 +17,37 @@ import sys
 
 class TFileContextManager(object):
 	"""
-	Context manager for ROOT TFile
+	Minimal file-object interface for ROOT TFile
 	
-	Opening/closing file is automatically done, e.g.:
-		with topen("input.root") as input_:
-			plot = input_.Get("hello")
-	the above code is equivalent to:
-		input_ = ROOT.TFile("input.root")
-		if not input_.IsZombie():
-			plot = input_.Get("hello")
-			input_.Close()
+	This class replicates part of python's file management interface
+	(``open``) for ROOT files. Most importantly, it can be used as a
+	context manager:
+	
+	>>> from tfilecontextmanager import TFileContextManager as topen
+	>>> with topen("data.root") as tfile:
+	...     plot = tfile.Get("Hello")
+	
+	This will safely open and close the file, regardless of errors.
 	"""
 	def __init__(self, filename, mode = "readonly"):
 		self._filename = filename
-		self._file = None
 		self._mode = mode
+		self._file = ROOT.TFile.Open(self._filename, self._mode)
+		# ROOT may have silently failed opening the file
+		# NOTE: didn't find what pyroot *actually* may return, this can probably be cut down - MF@030516
+		if self._file is None or (not self._file) or self._file.IsZombie():
+			self._file = None
+			# TFile::TFile "In case the file does not exist or is not a valid ROOT file, it is made a Zombie."
+			raise IOError("No valid ROOT file: %r" % self._filename)
 
 	def __enter__(self):
-		"""Return actual ROOT file"""
-		if self._file is None:
-			self._file = ROOT.TFile.Open(self._filename, self._mode)
-			# ROOT may have silently failed opening the file
-			if (not self._file) or self._file.IsZombie():
-				self._file = None
-				# TFile::TFile "In case the file does not exist or is not a valid ROOT file, it is made a Zombie."
-				raise IOError("No such ROOT file: %r" % self._filename)
+		"""Provide ROOT file safely"""
+		# Return ROOT file, not self
+		# self's management functions aren't needed in a context
 		return self._file
 
 	def __exit__(self, error_type, error_value, error_traceback):
-		"""Close file on exit"""
+		"""Close file on leaving context"""
 		if self._file is not None:
 			self._file.Close()
 			self._file = None
