689e537fdc57b97717e255c41b57493236f53f56
Thomas Mueller <t.muller@cern.ch>
2016-06-24 01:04:57 +0200

    addhistograms.py: change paramter names such that they differ from the ones of SumOfHistograms

--- HarryPlotter/python/analysis_modules/addhistograms.py
+++ HarryPlotter/python/analysis_modules/addhistograms.py
@@ -19,60 +19,60 @@ class AddHistograms(analysisbase.AnalysisBase):
 
 		self.add_histograms_options = parser.add_argument_group("{} options".format(self.name()))
 		self.add_histograms_options.add_argument(
-				"--histogram-nicks", nargs="+",
+				"--add-nicks", nargs="+",
 				help="Nick names (whitespace separated) for the histograms to be added"
 		)
 		self.add_histograms_options.add_argument(
-				"--sum-scale-factors", nargs="+",
+				"--add-scale-factors", nargs="+",
 				help="Scale factor (whitespace separated) for the histograms to be added [Default: 1]."
 		)
 		self.add_histograms_options.add_argument(
-				"--sum-result-nicks", nargs="+",
+				"--add-result-nicks", nargs="+",
 				help="Nick names for the resulting sum histograms."
 		)
 
 	def prepare_args(self, parser, plotData):
 		super(AddHistograms, self).prepare_args(parser, plotData)
-		self.prepare_list_args(plotData, ["histogram_nicks", "sum_result_nicks","sum_scale_factors"])
+		self.prepare_list_args(plotData, ["add_nicks", "add_result_nicks", "add_scale_factors"])
 		
-		for index, (histogram_nicks, sum_result_nick,sum_scale_factors) in enumerate(zip(
-				*[plotData.plotdict[k] for k in ["histogram_nicks", "sum_result_nicks","sum_scale_factors"]]
+		for index, (add_nicks, add_result_nick, add_scale_factors) in enumerate(zip(
+				*[plotData.plotdict[k] for k in ["add_nicks", "add_result_nicks", "add_scale_factors"]]
 		)):
-			plotData.plotdict["histogram_nicks"][index] = histogram_nicks.split()
-			if sum_scale_factors is None:
-				plotData.plotdict["sum_scale_factors"][index] = [1] * len(histogram_nicks.split())
+			plotData.plotdict["add_nicks"][index] = add_nicks.split()
+			if add_scale_factors is None:
+				plotData.plotdict["add_scale_factors"][index] = [1] * len(add_nicks.split())
 			else:
-				plotData.plotdict["sum_scale_factors"][index] = [float(sum_scale_factor) for sum_scale_factor in sum_scale_factors.split()]
-			if sum_result_nick is None:
-				plotData.plotdict["sum_result_nicks"][index] = "sum_{}".format(
-						"_".join(plotData.plotdict["histogram_nicks"][index]),
+				plotData.plotdict["add_scale_factors"][index] = [float(add_scale_factor) for add_scale_factor in add_scale_factors.split()]
+			if add_result_nick is None:
+				plotData.plotdict["add_result_nicks"][index] = "add_{}".format(
+						"_".join(plotData.plotdict["add_nicks"][index]),
 				)
-			if not plotData.plotdict["sum_result_nicks"][index] in plotData.plotdict["nicks"]:
+			if not plotData.plotdict["add_result_nicks"][index] in plotData.plotdict["nicks"]:
 				plotData.plotdict["nicks"].insert(
-						plotData.plotdict["nicks"].index(plotData.plotdict["histogram_nicks"][index][0]),
-						plotData.plotdict["sum_result_nicks"][index]
+						plotData.plotdict["nicks"].index(plotData.plotdict["add_nicks"][index][0]),
+						plotData.plotdict["add_result_nicks"][index]
 				)
 
 	def run(self, plotData=None):
 		super(AddHistograms, self).run(plotData)
 		
-		for histogram_nicks, sum_result_nick,sum_scale_factors in zip(
-				*[plotData.plotdict[k] for k in ["histogram_nicks", "sum_result_nicks","sum_scale_factors"]]
+		for add_nicks, add_result_nick, add_scale_factors in zip(
+				*[plotData.plotdict[k] for k in ["add_nicks", "add_result_nicks","add_scale_factors"]]
 		):
-			sum_histogram = None
-			log.debug("AddHistograms: "+sum_result_nick+" = "+(" + ".join([str(scale)+"*"+nick for nick, scale in zip(histogram_nicks, sum_scale_factors)])))
-			for nick, sum_scale_factor in zip(histogram_nicks, sum_scale_factors):
+			add_histogram = None
+			log.debug("AddHistograms: "+add_result_nick+" = "+(" + ".join([str(scale)+"*"+nick for nick, scale in zip(add_nicks, add_scale_factors)])))
+			for nick, add_scale_factor in zip(add_nicks, add_scale_factors):
 				root_object = plotData.plotdict["root_objects"][nick]
 				assert(isinstance(root_object, ROOT.TH1))
-				if sum_histogram is None:
-					sum_histogram = root_object.Clone(
-							"sum_" + hashlib.md5("_".join([str(histogram_nicks), sum_result_nick])).hexdigest()
+				if add_histogram is None:
+					add_histogram = root_object.Clone(
+							"add_" + hashlib.md5("_".join([str(add_nicks), add_result_nick])).hexdigest()
 					)
-					sum_histogram.Scale(sum_scale_factor)
-					sum_histogram.SetDirectory(0)
+					add_histogram.Scale(add_scale_factor)
+					add_histogram.SetDirectory(0)
 				else:
-					sum_histogram.Add(root_object, sum_scale_factor)
-					sum_histogram.SetDirectory(0)
+					add_histogram.Add(root_object, add_scale_factor)
+					add_histogram.SetDirectory(0)
 
-			plotData.plotdict["root_objects"][sum_result_nick] = sum_histogram
+			plotData.plotdict["root_objects"][add_result_nick] = add_histogram
 
