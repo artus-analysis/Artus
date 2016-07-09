31af6591ad3e0edb0356fbb21d2dde6cfc0bc45c
Thomas Mueller <t.muller@cern.ch>
2016-07-09 13:02:36 +0200

    inputinteractive.py: implement binning options also for functions

--- HarryPlotter/python/input_modules/inputinteractive.py
+++ HarryPlotter/python/input_modules/inputinteractive.py
@@ -8,6 +8,7 @@ import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
 import array
+import copy
 import hashlib
 import ROOT
 import sys
@@ -46,6 +47,7 @@ class InputInteractive(inputbase.InputBase):
 			except:
 				return input_string
 		
+		# prepare binning
 		for key in ["x_bins", "y_bins", "z_bins"]:
 			for index, binning in enumerate(plotData.plotdict[key]):	
 				if (not binning is None) and (len(binning) > 1):
@@ -111,72 +113,104 @@ class InputInteractive(inputbase.InputBase):
 			# prepare unique name
 			name_hash = hashlib.md5("_".join([str(item) for item in [nick, x_bins, x_values, x_errors, y_bins, y_values, y_errors, z_bins, z_values, z_errors, x_errors_up, y_errors_up]])).hexdigest()
 			
+			# determine mode
+			create_function = False
+			create_graph = False
+			create_histogram = False
 			if x_bins is None:
-				
-				# Functions
 				if len(y_values) == 0:
-					expression = " ".join([str(x_value) for x_value in x_values])
-					formula = ROOT.TFormula("formula_"+name_hash, expression)
-					function_class = None
-					function_class_name = ""
-					lim_args = [-1e3, 1e3]
-					if formula.GetNdim() == 1:
-						function_class = ROOT.TF1
-						function_class_name = "ROOT.TF1"
-					elif formula.GetNdim() == 2:
-						function_class = ROOT.TF2
-						function_class_name = "ROOT.TF2"
-						lim_args = lim_args*2
-					else:
-						function_class = ROOT.TF3
-						function_class_name = "ROOT.TF3"
-						lim_args = lim_args*3
-					log.debug(function_class_name+"(function_"+name_hash+", "+expression+", "+(", ".join([str(lim) for lim in lim_args]))+")")
-					plotData.plotdict.setdefault("root_objects", {})[nick] = function_class("function_"+name_hash, expression, *lim_args)
-				
-				# Graphs
+					create_function = True
 				else:
-					if len(z_values) == 0:
-						if len(x_errors_up) == 0 and len(y_errors_up) == 0:
-							log.debug("ROOT.TGraphErrors("+
-									str(len(x_values))+", "+
-									str(array.array("d", x_values))+", "+str(array.array("d", y_values))+", "+
-									str(array.array("d", x_errors))+", "+str(array.array("d", y_errors))+")"
-							)
-							plotData.plotdict.setdefault("root_objects", {})[nick] = ROOT.TGraphErrors(
-									len(x_values),
-									array.array("d", x_values), array.array("d", y_values),
-									array.array("d", x_errors), array.array("d", y_errors)
-							)
-						else:
-							log.debug("ROOT.TGraphAsymmErrors("+
-									str(len(x_values))+", "+
-									str(array.array("d", x_values))+", "+str(array.array("d", y_values))+", "+
-									str(array.array("d", x_errors))+", "+str(array.array("d", x_errors_up))+", "+
-									str(array.array("d", y_errors))+", "+str(array.array("d", y_errors_up))+")"
-							)
-							plotData.plotdict.setdefault("root_objects", {})[nick] = ROOT.TGraphAsymmErrors(
-									len(x_values),
-									array.array("d", x_values), array.array("d", y_values),
-									array.array("d", x_errors), array.array("d", x_errors_up),
-									array.array("d", y_errors), array.array("d", y_errors_up)
-							)
-					else:
-						log.debug("ROOT.TGraph2DErrors("+
-								str(len(x_values))+", "+
-								str(array.array("d", x_values))+", "+str(array.array("d", y_values))+", "+str(array.array("d", z_values))+", "+
-								str(array.array("d", x_errors))+", "+str(array.array("d", y_errors))+", "+str(array.array("d", z_errors))+")"
-						)
-						plotData.plotdict.setdefault("root_objects", {})[nick] = ROOT.TGraph2DErrors(
-								len(x_values),
-								array.array("d", x_values), array.array("d", y_values), array.array("d", z_values),
-								array.array("d", x_errors), array.array("d", y_errors), array.array("d", z_errors)
-						)
-					plotData.plotdict["root_objects"][nick].SetName("graph_"+name_hash)
-					plotData.plotdict["root_objects"][nick].SetTitle("")
-			
-			# Histograms
+					create_graph = True
 			else:
+				if len(x_values) == 1:
+					create_function = True
+				else:
+					create_histogram = True
+			
+			if create_function:
+				# prepare binning
+				default_binning = ["100,-1e3,1e3"]
+				if x_bins is None:
+					x_bins = copy.deepcopy(default_binning)
+				if y_bins is None:
+					y_bins = copy.deepcopy(default_binning)
+				if z_bins is None:
+					z_bins = copy.deepcopy(default_binning)
+				x_bins = [float(x) for x in x_bins[0].split(",")]
+				y_bins = [float(y) for y in y_bins[0].split(",")]
+				z_bins = [float(z) for z in z_bins[0].split(",")]
+			
+				expression = " ".join([str(x_value) for x_value in x_values])
+				formula = ROOT.TFormula("formula_"+name_hash, expression)
+				function_class = None
+				function_class_name = ""
+				lim_args = []
+				if formula.GetNdim() == 1:
+					function_class = ROOT.TF1
+					function_class_name = "ROOT.TF1"
+					lim_args.extend(x_bins[1:])
+				elif formula.GetNdim() == 2:
+					function_class = ROOT.TF2
+					function_class_name = "ROOT.TF2"
+					lim_args.extend(y_bins[1:])
+				else:
+					function_class = ROOT.TF3
+					function_class_name = "ROOT.TF3"
+					lim_args.extend(x_bins[1:])
+				log.debug(function_class_name+"(function_"+name_hash+", "+expression+", "+(", ".join([str(lim) for lim in lim_args]))+")")
+				root_function = function_class("function_"+name_hash, expression, *lim_args)
+				
+				if formula.GetNdim() == 1:
+					root_function.SetNpx(int(x_bins[0]))
+				if formula.GetNdim() == 2:
+					root_function.SetNpy(int(y_bins[0]))
+				if formula.GetNdim() == 3:
+					root_function.SetNpz(int(z_bins[0]))
+				
+				plotData.plotdict.setdefault("root_objects", {})[nick] = root_function
+			
+			elif create_graph:
+				if len(z_values) == 0:
+					if len(x_errors_up) == 0 and len(y_errors_up) == 0:
+						log.debug("ROOT.TGraphErrors("+
+								str(len(x_values))+", "+
+								str(array.array("d", x_values))+", "+str(array.array("d", y_values))+", "+
+								str(array.array("d", x_errors))+", "+str(array.array("d", y_errors))+")"
+						)
+						plotData.plotdict.setdefault("root_objects", {})[nick] = ROOT.TGraphErrors(
+								len(x_values),
+								array.array("d", x_values), array.array("d", y_values),
+								array.array("d", x_errors), array.array("d", y_errors)
+						)
+					else:
+						log.debug("ROOT.TGraphAsymmErrors("+
+								str(len(x_values))+", "+
+								str(array.array("d", x_values))+", "+str(array.array("d", y_values))+", "+
+								str(array.array("d", x_errors))+", "+str(array.array("d", x_errors_up))+", "+
+								str(array.array("d", y_errors))+", "+str(array.array("d", y_errors_up))+")"
+						)
+						plotData.plotdict.setdefault("root_objects", {})[nick] = ROOT.TGraphAsymmErrors(
+								len(x_values),
+								array.array("d", x_values), array.array("d", y_values),
+								array.array("d", x_errors), array.array("d", x_errors_up),
+								array.array("d", y_errors), array.array("d", y_errors_up)
+						)
+				else:
+					log.debug("ROOT.TGraph2DErrors("+
+							str(len(x_values))+", "+
+							str(array.array("d", x_values))+", "+str(array.array("d", y_values))+", "+str(array.array("d", z_values))+", "+
+							str(array.array("d", x_errors))+", "+str(array.array("d", y_errors))+", "+str(array.array("d", z_errors))+")"
+					)
+					plotData.plotdict.setdefault("root_objects", {})[nick] = ROOT.TGraph2DErrors(
+							len(x_values),
+							array.array("d", x_values), array.array("d", y_values), array.array("d", z_values),
+							array.array("d", x_errors), array.array("d", y_errors), array.array("d", z_errors)
+					)
+				plotData.plotdict["root_objects"][nick].SetName("graph_"+name_hash)
+				plotData.plotdict["root_objects"][nick].SetTitle("")
+			
+			elif create_histogram:
 				root_histogram = roottools.RootTools.create_root_histogram(
 						x_bins=x_bins,
 						y_bins=y_bins,
