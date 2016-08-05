a2c044e69c089e74472c1e12eb7c612f3b2a3ade
Raphael Friese <raphael.friese@cern.ch>
2016-08-05 09:50:42 +0200

    let merging script allow several input directories. Necessary when jobs have been using dbs files as inputs

--- Configuration/scripts/artusMergeOutputs.py
+++ Configuration/scripts/artusMergeOutputs.py
@@ -28,16 +28,22 @@ def main():
 	
 	parser = argparse.ArgumentParser(description="Merge Artus outputs per nick name.", parents=[logger.loggingParser])
 
-	parser.add_argument("project_dir", help="Artus Project directory containing the files \"output/*/*.root\" to merge in case there is an output dir, */*.root else")
+	parser.add_argument("project_dir", help="Artus Project directory containing the files \"output/*/*.root\" to merge in case there is an output dir, */*.root else", nargs="*")
 	parser.add_argument("-n", "--n-processes", type=int, default=1,
 	                    help="Number of (parallel) processes. [Default: %(default)s]")
-	parser.add_argument("--output-dir", help="Directory to store merged files. Default: Same as project_dir.")
+	parser.add_argument("--output-dir", help="Directory to store merged files. Default: Same as first project_dir.")
 	args = parser.parse_args()
 	logger.initLogger(args)
-	extra_path = "output/" if (os.path.isdir(os.path.join(args.project_dir, "output"))) else ""
-	output_dirs = glob.glob(os.path.join(args.project_dir, extra_path+"*"))
-	nick_names = [nick for nick in [output_dir[output_dir.rfind("/")+1:] for output_dir in output_dirs] if not ".tar.gz" in nick]
-	outputs_per_nick = {nick : glob.glob(os.path.join(args.project_dir, extra_path, nick, "*.root")) for nick in nick_names}
+	outputs_per_nick = {}
+	for project_dir in args.project_dir:
+		extra_path = "output/" if (os.path.isdir(os.path.join(project_dir, "output"))) else ""
+		output_dirs = glob.glob(os.path.join(project_dir, extra_path+"*"))
+		nick_names = [nick for nick in [output_dir[output_dir.rfind("/")+1:] for output_dir in output_dirs] if not ".tar.gz" in nick]
+		for nick in nick_names:
+			if not (nick in outputs_per_nick):
+				outputs_per_nick[nick] = []
+			outputs_per_nick[nick] = outputs_per_nick[nick] + glob.glob(os.path.join(project_dir, extra_path, nick, "*.root"))
+	print outputs_per_nick
 	# drop potentially existing SvfitCaches from the filelist
 	for nick, files in outputs_per_nick.iteritems():
 		outputs_per_nick[nick] = [file for file in files if ("SvfitCache" not in file)]
@@ -47,7 +53,7 @@ def main():
 	for nick_name, output_files in pi.ProgressIterator(outputs_per_nick.iteritems(),
 	                                                   length=len(outputs_per_nick),
 	                                                   description="Merging Artus outputs"):
-		merged_dir = os.path.join(args.project_dir if(args.output_dir == None) else args.output_dir, "merged", nick_name)
+		merged_dir = os.path.join(args.project_dir[0] if(args.output_dir == None) else args.output_dir, "merged", nick_name)
 		if not os.path.exists(merged_dir):
 			os.makedirs(merged_dir)
 	
