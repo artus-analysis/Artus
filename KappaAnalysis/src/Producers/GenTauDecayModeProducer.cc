81679a8616a5801f00e9bba34ba38f10bef16af8
Thomas Mueller <t.muller@cern.ch>
2016-09-07 16:54:35 +0200

    GenTauDecayModeProducer.cc: change genTau*DecayMode, genTau*NProngs and genTau*NPi0s to use new member of KGenTau class corresponding to the official CMSSW decay mode

--- KappaAnalysis/src/Producers/GenTauDecayModeProducer.cc
+++ KappaAnalysis/src/Producers/GenTauDecayModeProducer.cc
@@ -2,6 +2,8 @@
 #include "Artus/KappaAnalysis/interface/Producers/GenTauDecayModeProducer.h"
 #include "Artus/Utility/interface/Utility.h"
 #include "Artus/Utility/interface/DefaultValues.h"
+#include "Artus/Utility/interface/SafeMap.h"
+
 
 std::string GenTauDecayModeProducer::GetProducerId() const {
 	return "GenTauDecayModeProducer";
@@ -15,19 +17,27 @@ void GenTauDecayModeProducer::Init(KappaSettings const& settings)
 
 	LambdaNtupleConsumer<KappaTypes>::AddIntQuantity("genTau1DecayMode", [](KappaEvent const & event, KappaProduct const & product)
 	{
-		return Utility::ToUnderlyingValue(product.m_tau1DecayMode);
+		return (product.m_validGenTaus.size() > 0 ? SafeMap::Get(product.m_validGenTausMap, product.m_validGenTaus.at(0))->genDecayMode() : DefaultValues::UndefinedInt);
 	});
 	LambdaNtupleConsumer<KappaTypes>::AddIntQuantity("genTau2DecayMode", [](KappaEvent const & event, KappaProduct const & product)
 	{
-		return Utility::ToUnderlyingValue(product.m_tau2DecayMode);
+		return (product.m_validGenTaus.size() > 1 ? SafeMap::Get(product.m_validGenTausMap, product.m_validGenTaus.at(1))->genDecayMode() : DefaultValues::UndefinedInt);
 	});
-	LambdaNtupleConsumer<KappaTypes>::AddIntQuantity("genTau1ProngSize", [](KappaEvent const & event, KappaProduct const & product)
+	LambdaNtupleConsumer<KappaTypes>::AddIntQuantity("genTau1NProngs", [](KappaEvent const & event, KappaProduct const & product)
 	{
-		return product.m_tau1ProngSize;
+		return (product.m_validGenTaus.size() > 0 ? SafeMap::Get(product.m_validGenTausMap, product.m_validGenTaus.at(0))->nProngs : DefaultValues::UndefinedInt);
 	});
 	LambdaNtupleConsumer<KappaTypes>::AddIntQuantity("genTau2ProngSize", [](KappaEvent const & event, KappaProduct const & product)
 	{
-		return product.m_tau2ProngSize;
+		return (product.m_validGenTaus.size() > 1 ? SafeMap::Get(product.m_validGenTausMap, product.m_validGenTaus.at(1))->nPi0s : DefaultValues::UndefinedInt);
+	});
+	LambdaNtupleConsumer<KappaTypes>::AddIntQuantity("genTau1NPi0s", [](KappaEvent const & event, KappaProduct const & product)
+	{
+		return (product.m_validGenTaus.size() > 0 ? SafeMap::Get(product.m_validGenTausMap, product.m_validGenTaus.at(0))->nPi0s : DefaultValues::UndefinedInt);
+	});
+	LambdaNtupleConsumer<KappaTypes>::AddIntQuantity("genTau2Pi0s", [](KappaEvent const & event, KappaProduct const & product)
+	{
+		return (product.m_validGenTaus.size() > 1 ? SafeMap::Get(product.m_validGenTausMap, product.m_validGenTaus.at(1))->nProngs : DefaultValues::UndefinedInt);
 	});
 	LambdaNtupleConsumer<KappaTypes>::AddIntQuantity("genTauTauDecayMode", [](KappaEvent const & event, KappaProduct const & product)
 	{
