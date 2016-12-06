fed27e75e5fdb76a952c84fc93ee2d777bd0529d
hale sert <sert@physik.rwth-aachen.de>
2016-12-06 22:32:39 +0100

    Revert "Revert "GenJets -- ntuple variables are added""
    This reverts commit 123b4dc70df19c43eedeecf3eb65f9a3b6e7e99f.

--- KappaAnalysis/interface/KappaEvent.h
+++ KappaAnalysis/interface/KappaEvent.h
@@ -35,7 +35,7 @@ public:
 
 	/// pointer to jet collection
 	KBasicJets* m_basicJets = nullptr;
-	KLVs* m_genJets = nullptr;
+	KGenJets* m_genJets = nullptr;
 
 	/// pointer to tagged jet collection
 	KJets* m_tjets = nullptr;
