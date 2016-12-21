14f56d1857e653de3a32aebb0a241083b85a2252
Thomas Mueller <t.muller@cern.ch>
2016-12-21 01:18:10 +0100

    PropertyMacros.h: better readable macros

--- Configuration/interface/PropertyMacros.h
+++ Configuration/interface/PropertyMacros.h
@@ -6,30 +6,52 @@
 
 #pragma once
 /// Implements a property with Get, Set and Has Methods
-#define IMPL_PROPERTY(TYPE, SNAME)                          \
-private:                                                    \
-TYPE m_##SNAME;                                             \
-public:                                                     \
-TYPE Get##SNAME() const { return m_##SNAME; }               \
-bool Has##SNAME() const { return &m_##SNAME; }              \
-TYPE const& GetRef##SNAME() const { return m_##SNAME; }     \
-TYPE const* GetPt##SNAME() const { return &m_##SNAME; }     \
-void Set##SNAME(TYPE val) { m_##SNAME = val; }
+#define IMPL_PROPERTY(TYPE, SNAME) \
+private: \
+	TYPE m_##SNAME; \
+public: \
+	TYPE Get##SNAME() const { \
+		return m_##SNAME; \
+	} \
+	bool Has##SNAME() const { \
+		return &m_##SNAME; \
+	} \
+	TYPE const& GetRef##SNAME() const { \
+		return m_##SNAME; \
+	} \
+	TYPE const* GetPt##SNAME() const { \
+		return &m_##SNAME; \
+	} \
+	void Set##SNAME(TYPE val) { \
+		m_##SNAME = val; \
+	}
 
-#define IMPL_PROPERTY_INITIALIZE(TYPE, SNAME, VAL)          \
-private:                                                    \
-TYPE m_##SNAME = VAL;                                       \
-public:                                                     \
-TYPE Get##SNAME() const { return m_##SNAME; }               \
-bool Has##SNAME() const { return &m_##SNAME; }              \
-TYPE const& GetRef##SNAME() const { return m_##SNAME; }     \
-TYPE const* GetPt##SNAME() const { return &m_##SNAME; }     \
-void Set##SNAME(TYPE val) { m_##SNAME = val; }
+#define IMPL_PROPERTY_INITIALIZE(TYPE, SNAME, VAL) \
+private: \
+	TYPE m_##SNAME = VAL; \
+public: \
+	TYPE Get##SNAME() const { return m_##SNAME; \
+	} \
+	bool Has##SNAME() const { \
+		return &m_##SNAME; \
+	} \
+	TYPE const& GetRef##SNAME() const { \
+		return m_##SNAME; \
+	} \
+	TYPE const* GetPt##SNAME() const { \
+		return &m_##SNAME; \
+	} \
+	void Set##SNAME(TYPE val) { \
+		m_##SNAME = val; \
+	}
 
-#define IMPL_PROPERTY_READONLY(TYPE, SNAME)                 \
-private:                                                    \
-TYPE m_##SNAME;                                             \
-public:                                                     \
-TYPE Get##SNAME() const { return m_##SNAME; }               \
-private:                                                    \
-void Set##SNAME(TYPE val) { m_##SNAME = val; }
+#define IMPL_PROPERTY_READONLY(TYPE, SNAME) \
+private: \
+	TYPE m_##SNAME; \
+	void Set##SNAME(TYPE val) { \
+		m_##SNAME = val; \
+	} \
+public: \
+	TYPE Get##SNAME() const { \
+		return m_##SNAME; \
+	}
