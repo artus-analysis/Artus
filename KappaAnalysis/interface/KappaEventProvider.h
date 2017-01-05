a23e5a2ce76d6c77b1da52f1a1e5343c631deb03
stefan <stefan.wayand@gmail.com>
2017-01-05 15:43:16 +0100

    make the GenJets compatible to the definition in  Z+Jets

--- KappaAnalysis/interface/KappaEventProvider.h
+++ KappaAnalysis/interface/KappaEventProvider.h
@@ -46,8 +46,11 @@ public:
 		// Jets
 		if (! settings.GetBasicJets().empty())
 			this->m_event.m_basicJets = this->template SecureFileInterfaceGet<KBasicJets>(settings.GetBasicJets());
-		if (! settings.GetGenJets().empty())
-			this->m_event.m_genJets = this->template SecureFileInterfaceGet<KGenJets>(settings.GetGenJets());
+		if (! settings.GetGenJets().empty()){
+			if (settings.GetuseKLVGenJets()) this->m_event.m_genJets = (KGenJets*) this->template SecureFileInterfaceGet<KLVs>(settings.GetGenJets());
+			else this->m_event.m_genJets = this->template SecureFileInterfaceGet<KGenJets>(settings.GetGenJets());
+			
+		}
 		if (! settings.GetTaggedJets().empty())
 			this->m_event.m_tjets = this->template SecureFileInterfaceGet<KJets>(settings.GetTaggedJets());
 		if (! settings.GetPileupDensity().empty())
--- KappaAnalysis/interface/KappaSettings.h
+++ KappaAnalysis/interface/KappaSettings.h
@@ -35,6 +35,7 @@ public:
 	/// name of jet collection in kappa tuple
 	IMPL_SETTING_DEFAULT(std::string, BasicJets, "");
 	IMPL_SETTING_DEFAULT(std::string, GenJets, "");
+	IMPL_SETTING_DEFAULT(bool, useKLVGenJets, false);
 
 	/// name of tagged jet collection in kappa tuple
 	IMPL_SETTING_DEFAULT(std::string, TaggedJets, "");
