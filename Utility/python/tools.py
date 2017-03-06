c600b1545fc2de10636c1be89bdc91f02b666085
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2017-03-06 17:26:26 +0100

    minor bug fix and adding polarisation bdt weights

--- Utility/python/tools.py
+++ Utility/python/tools.py
@@ -159,7 +159,6 @@ def get_indented_text(prefix, message, width=None):
 			)
 	return '\n'.join(['\n'.join(tmp_wrapped_texts)])
 
-import Artus.Utility.progressiterator as pi
 def parallelize(function, arguments_list, n_processes=1):
 	if n_processes <= 1:
 		results = []
@@ -171,6 +170,7 @@ def parallelize(function, arguments_list, n_processes=1):
 		results = pool.map_async(function, arguments_list)
 		n_tasks = len(arguments_list)
 		left = n_tasks
+		import Artus.Utility.progressiterator as pi
 		progress_iterator = pi.ProgressIterator(range(n_tasks), description = "calling " + str(function))
 		progress_iterator.next()
 		while (True):
@@ -318,4 +318,5 @@ def _merge_sequences(seq1,seq2):
 	if len(set(res)) != len(res):
 		raise ValueError("Sequences have non-unique elements or differently ordered subsequences")
 	return res
+
 
