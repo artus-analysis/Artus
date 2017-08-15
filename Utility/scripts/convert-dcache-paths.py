4d0feaef80a22faf798589d0e93d4f726292464f
Thomas Mueller <tmuller@cern.ch>
2017-08-15 09:59:36 +0200

    dcachetools.py, convert-dcache-paths.py: added also conversions to local paths

--- Utility/scripts/convert-dcache-paths.py
+++ Utility/scripts/convert-dcache-paths.py
@@ -16,8 +16,11 @@ def main():
 	
 	parser.add_argument("input_files", nargs="+",
 	                    help="Input text files.")
-	parser.add_argument("-m", "--mode", default="xrd2dcap",
-	                    choices=["xrd2srm", "srm2xrd", "srm2dcap", "dcap2srm", "dcap2xrd", "xrd2dcap"],
+	parser.add_argument("-m", "--mode", default="xrd2local",
+	                    choices=["dcap2local", "dcap2srm", "dcap2xrd",
+	                             "local2xrd", "local2dcap", "local2srm",
+	                             "srm2dcap", "srm2local", "srm2xrd",
+	                             "xrd2dcap", "xrd2local", "xrd2srm"],
 	                    help="Conversion mode. [Default: %(default)s]")
 	parser.add_argument("-i", "--in-place", default=False, action="store_true",
 	                    help="Convert paths in place modifying the input files. [Default: Only print converted files]")
