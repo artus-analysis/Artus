44388fde4c035c7c6294f52971124e38c50b7738
Olena Hlushchenko <greyxray@gmail.com>
2017-08-17 13:38:21 +0200

    ProjectByFit: add the TH3:FitSlicesZ functionslity

--- HarryPlotter/python/analysis_modules/projectbyfit.py
+++ HarryPlotter/python/analysis_modules/projectbyfit.py
@@ -44,10 +44,16 @@ class ProjectByFit(analysisbase.AnalysisBase):
 								help="The argument option can be used to change the fit options. If debug mode is enabled the 'Q' option will be always removed. [Default: %(default)s]")
 		self.projectbyfit_options.add_argument("--projection-cuts-bins-filled", type=str, nargs="+", default="0",
 								help="Fit for bins in X for which the corresponding projection along Y has more than **cut** bins filled. [Default: %(default)s]")
+		self.projectbyfit_options.add_argument("--projection-fit-slices-z", type="bool", nargs="?", default=False, const=True,
+								help="Call `void TH3::FitSlicesZ`. The fitted distribution should be set along Z-Axis. Root reference: https://root.cern.ch/doc/v608/classTH3.html#a927fb729e31d90ecbcc2b5e4f9e23900 . [Default: %(default)s]")
+		self.projectbyfit_options.add_argument("--projection-bins-miny", type=str, nargs="+", default="0",
+								help="Fit starting from this bin along Y. [Default: %(default)s]")
+		self.projectbyfit_options.add_argument("--projection-bins-maxy", type=str, nargs="+", default="-1",
+								help="Fit starting from this bin along Y. [Default: %(default)s]")
 
 	def prepare_args(self, parser, plotData):
 		super(ProjectByFit, self).prepare_args(parser, plotData)
-		self.prepare_list_args(plotData, ["projection_functions", "projection_parameters", "projection_to_plot","projection_fit_range","projection_to_nick", "projection_fit_backend", "projection_bins_minx", "projection_bins_maxx", "projection_options", "projection_cuts_bins_filled"])
+		self.prepare_list_args(plotData, ["projection_functions", "projection_parameters", "projection_to_plot","projection_fit_range","projection_to_nick", "projection_fit_backend", "projection_bins_minx", "projection_bins_maxx", "projection_options", "projection_cuts_bins_filled", "projection_fit_slices_z", "projection_bins_miny", "projection_bins_maxy"])
 		plotData.plotdict["projection_fit_range"] = [ x.replace("\\", "") if x != None else x for x in plotData.plotdict["projection_fit_range"] ]
 
 	def run(self, plotData=None):
@@ -55,7 +61,9 @@ class ProjectByFit(analysisbase.AnalysisBase):
 
 		histograms_to_replace = []
 		result_histograms = {}
-		for function, start_parameter, parameter_to_plot, fit_range, nick, fit_backend, bin_minx, bins_maxx, option, cut_bins_filled in zip(plotData.plotdict["projection_functions"],
+		fit_slices_z = plotData.plotdict["projection_fit_slices_z"][0]
+
+		for function, start_parameter, parameter_to_plot, fit_range, nick, fit_backend, bin_minx, bins_maxx, option, cut_bins_filled, bin_miny, bin_maxy in zip(plotData.plotdict["projection_functions"],
 		                                                                plotData.plotdict["projection_parameters"],
 		                                                                plotData.plotdict["projection_to_plot"],
 		                                                                plotData.plotdict["projection_fit_range"],
@@ -64,39 +72,50 @@ class ProjectByFit(analysisbase.AnalysisBase):
 		                                                                plotData.plotdict["projection_bins_minx"],
 		                                                                plotData.plotdict["projection_bins_maxx"],
 		                                                                plotData.plotdict["projection_options"],
-		                                                                plotData.plotdict["projection_cuts_bins_filled"]):
+		                                                                plotData.plotdict["projection_cuts_bins_filled"],
+		                                                                plotData.plotdict["projection_bins_miny"],
+		                                                                plotData.plotdict["projection_bins_maxy"]):
 
-			histogram_2D = plotData.plotdict["root_objects"][nick]
+			histogram_ND = plotData.plotdict["root_objects"][nick]
+
 			if log.isEnabledFor(logging.DEBUG): option.translate(None, "Q")
-			if not histogram_2D.ClassName().startswith("TH2"):
-				log.fatal("The ProjectByFit Module only works on projecting TH2 histograms to TH1 histograms, but " + histogram_2D.ClassName() + " was provided as input.")
+			if not histogram_ND.ClassName().startswith("TH3" if fit_slices_z else "TH2"):
+				log.fatal("The ProjectByFit Module only works on projecting " + ("TH3" if fit_slices_z else "TH2") + " histograms to " + ("TH2" if fit_slices_z else "TH1") + " histograms, but " + histogram_ND.ClassName() + " was provided as input.")
 				sys.exit(1)
 
 			# Estimating fit range
 			if fit_range != None:
 				fit_min = float(fit_range.split(",")[0])
 				fit_max = float(fit_range.split(",")[1])
-			else:
-				fit_min = histogram_2D.GetYaxis().GetXmin()
-				fit_max = histogram_2D.GetYaxis().GetXmax()
+			elif not fit_slices_z:
+				fit_min = histogram_ND.GetYaxis().GetXmin()
+				fit_max = histogram_ND.GetYaxis().GetXmax()
+			elif fit_slices_z:
+				fit_min = histogram_ND.GetZaxis().GetXmin()
+				fit_max = histogram_ND.GetZaxis().GetXmax()
 			start_parameters = [float(x) for x in start_parameter.split(",")]
 
 			# Getting fitted histogram
 			if fit_backend == "ROOT":
 				root_function = functionplot.FunctionPlot.create_tf1(function, fit_min, fit_max, start_parameters, nick=nick)
-				aSlices = ROOT.TObjArray()
-				histogram_2D.FitSlicesY(root_function,  int(bin_minx), int(bins_maxx), int(cut_bins_filled), option, aSlices)
-				if log.isEnabledFor(logging.DEBUG): aSlices[parameter_to_plot].Print("all")
+				if not fit_slices_z:
+					print 1
+					aSlices = ROOT.TObjArray()
+					histogram_ND.FitSlicesY(root_function,  int(bin_minx), int(bins_maxx), int(cut_bins_filled), option, aSlices)
+					if log.isEnabledFor(logging.DEBUG): aSlices[parameter_to_plot].Print("all")
+				else:
+					print 2
+					histogram_ND.FitSlicesZ(root_function,  int(bin_minx), int(bins_maxx), int(bin_miny), int(bin_maxy), option)
 
 				if parameter_to_plot.isdigit():
-					fitted_histogram = copy.deepcopy(ROOT.gDirectory.Get(histogram_2D.GetName() + "_" + str(parameter_to_plot)))
+					fitted_histogram = copy.deepcopy(ROOT.gDirectory.Get(histogram_ND.GetName() + "_" + str(parameter_to_plot)))
 				else:
 					fitted_histograms = []
 					for i in range(len(start_parameters)):
-						fitted_histograms.append(copy.deepcopy(ROOT.gDirectory.Get(histogram_2D.GetName() + "_" + str(i))))
+						fitted_histograms.append(copy.deepcopy(ROOT.gDirectory.Get(histogram_ND.GetName() + "_" + str(i))))
 					fitted_histogram = self.postprocess_fits(fitted_histograms, parameter_to_plot)
 			elif fit_backend == "RooFit":
-				fitted_histogram = self.fitSlicesRooFit(histogram_2D, function, start_parameters, parameter_to_plot)
+				fitted_histogram = self.fitSlicesRooFit(histogram_ND, function, start_parameters, parameter_to_plot)
 			else:
 				log.fatal("No such backend")
 				sys.exit(1)
