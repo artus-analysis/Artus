c1f06e1dd4208a9f36b36a24dad249415b014573
Thomas Mueller <t.muller@cern.ch>
2017-08-19 22:38:29 +0200

    KappaAnalysisExample: include metadata in processor function signatures

--- KappaAnalysisExample/interface/DiMuonSystemConsumer.h
+++ KappaAnalysisExample/interface/DiMuonSystemConsumer.h
@@ -11,18 +11,14 @@
 class DiMuonSystemConsumer: public ConsumerBase<KappaExampleTypes> {
 public:
 
-	typedef typename KappaExampleTypes::event_type event_type;
-	typedef typename KappaExampleTypes::product_type product_type;
-	typedef typename KappaExampleTypes::setting_type setting_type;
-
 	virtual std::string GetConsumerId() const override;
 
-	virtual void Init(setting_type const& settings) override;
+	virtual void Init(setting_type const& settings, metadata_type& metadata) override;
 
 	virtual void ProcessFilteredEvent(event_type const& event, product_type const& product,
-	                                  setting_type const& settings) override;
+	                                  setting_type const& settings, metadata_type const& metadata) override;
 
-	virtual void Finish(setting_type const& settings) override;
+	virtual void Finish(setting_type const& settings, metadata_type const& metadata) override;
 
 
 private:
