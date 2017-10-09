d66a106d772088f8fdcb6861cf602def279e70f1
Dennis Roy <dennis.roy@cern.ch>
2017-10-09 15:21:49 +0200

    Remove KIT from webplotting (easy fix to make it work for outside Aachen)

--- HarryPlotter/scripts/ini_harry_cmssw.sh
+++ HarryPlotter/scripts/ini_harry_cmssw.sh
@@ -11,37 +11,17 @@ if [ -z $HARRY_REMOTE_USER ]; then
 fi
 
 if [[ -z ${HARRY_SSHPC} ]]; then
-	#if logged in from an ekplx host, assume it is reachable
-	if [[ $HARRY_USERPC == *"ekplx"* ]]; then
-		export HARRY_SSHPC=$HARRY_USERPC
-	elif [[ $HARRY_USERPC == *"rwth"* ]]; then
-		export HARRY_SSHPC=$HARRY_USERPC
-	fi
-	if [[ -z ${HARRY_SSHPC} ]]; then
-		echo "Failed to find reachable \$HARRY_SSHP host" 1>&2
-	fi
+	export HARRY_SSHPC=$HARRY_USERPC
 fi
 
-if [[ $HARRY_SSHPC == *"rwth"* ]]; then
-	export HARRY_URL=http://${HARRY_REMOTE_USER}.web.cern.ch/${HARRY_REMOTE_USER}/plots_archive
-else
-	export HARRY_URL=http://www.ekp.kit.edu/~${HARRY_REMOTE_USER}/plots_archive
-fi
+export HARRY_URL=http://${HARRY_REMOTE_USER}.web.cern.ch/${HARRY_REMOTE_USER}/plots_archive
 
 if [[ -z ${WEB_PLOTTING_MKDIR_COMMAND} ]]; then
-	if [[ $HARRY_USERPC == *"rwth"* ]]; then
-		export WEB_PLOTTING_MKDIR_COMMAND="xrdfs eosuser.cern.ch mkdir -p /eos/user/${HARRY_REMOTE_USER:0:1}/${HARRY_REMOTE_USER}/www/plots_archive/{subdir}"
-	else
-		export WEB_PLOTTING_MKDIR_COMMAND="ssh ${HARRY_REMOTE_USER}@${HARRY_SSHPC} mkdir -p /ekpwww/web/${HARRY_REMOTE_USER}/public_html/plots_archive/{subdir}"
-	fi
+	export WEB_PLOTTING_MKDIR_COMMAND="xrdfs eosuser.cern.ch mkdir -p /eos/user/${HARRY_REMOTE_USER:0:1}/${HARRY_REMOTE_USER}/www/plots_archive/{subdir}"
 fi
 
 if [[ -z ${WEB_PLOTTING_COPY_COMMAND} ]]; then
-	if [[ $HARRY_USERPC == *"rwth"* ]]; then
 		export WEB_PLOTTING_COPY_COMMAND="xrdcp -s -f {source} root://eosuser.cern.ch//eos/user/${HARRY_REMOTE_USER:0:1}/${HARRY_REMOTE_USER}/www/plots_archive/{subdir}"
-	else
-		export WEB_PLOTTING_COPY_COMMAND="rsync -u {source} ${HARRY_REMOTE_USER}@${HARRY_SSHPC}:/ekpwww/web/${HARRY_REMOTE_USER}/public_html/plots_archive/{subdir}"
-	fi
 fi
 
 start_ssh_agent() {
@@ -54,19 +34,6 @@ start_ssh_agent() {
 }
 
 web_plotting_no_passwd() {
-	if [[ $HARRY_USERPC == *"rwth"* ]]; then
-		kinit ${HARRY_REMOTE_USER}@CERN.CH
-		aklog -c cern.ch #For web-plotting on EOS, this line isn't needed
-	else
-		# startup ssh-agent
-		unset SSH_ASKPASS
-		SSH_ENV="$HOME/.ssh/environment"
-		if [ -f "${SSH_ENV}" ]; then
-			. "${SSH_ENV}" > /dev/null
-			ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || start_agent
-		else
-			start_ssh_agent;
-		fi
-	fi
+	kinit ${HARRY_REMOTE_USER}@CERN.CH
 }
 
