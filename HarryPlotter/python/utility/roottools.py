f87f6e1a215bdbb9ef24cd5f09a3a34e7ba814e1
dominikwolfschläger <dominik.wolfschlaeger@googlemail.com>
2017-12-30 00:30:36 +0100

    Use GetDirectory here to make it work.

--- HarryPlotter/python/utility/roottools.py
+++ HarryPlotter/python/utility/roottools.py
@@ -763,9 +763,9 @@ class RootTools(object):
 		root_file.cd()
 		root_directory = root_file
 		for directory in path.split("/")[:-1]:
-			if root_directory.Get(directory) == None:
+			if root_directory.GetDirectory(directory) == None:
 				root_directory.mkdir(directory)
-			root_directory = root_directory.Get(directory)
+			root_directory = root_directory.GetDirectory(directory)
 			root_directory.cd()
 		root_object.Write(path.split("/")[-1], ROOT.TObject.kWriteDelete)
 		root_file.cd()
