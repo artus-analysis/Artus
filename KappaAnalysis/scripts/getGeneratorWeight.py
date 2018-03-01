825f52439ad429dc5eec07f735e2bb633d106238
hale sert <sert@physik.rwth-aachen.de>
2018-03-01 14:06:39 +0100

    getGeneratorWeight.py: inputs are changed from root files to the collection filelists

--- KappaAnalysis/scripts/getGeneratorWeight.py
+++ KappaAnalysis/scripts/getGeneratorWeight.py
@@ -22,58 +22,67 @@ def main():
 	parser = argparse.ArgumentParser(description="Print out the generator weight.",
 	                                 parents=[logger.loggingParser])
 
-	parser.add_argument("files", nargs="+", help="Input files.")
+	parser.add_argument("files", nargs="+", help="Input filelists of the collection")
 	parser.add_argument("-n", "--nick", default="kappa_(?P<nick>.*)_\d+.root",
 	                    help="Regular expression to extract nickname from file names. Use 'NONE' to disable nick name matching. [Default: %(default)s]")
 
 	args = parser.parse_args()
 	logger.initLogger(args)
 	
-	sumweight_per_nick = {}
-	n_entries_per_nick = {}
-	for (fileindex, file_name) in enumerate(args.files):
-		
-		# retrieve weights and total number of events
-		root_file = ROOT.TFile.Open(file_name, "READ")
-		lumiTree = root_file.Get("Lumis")
-		n_entries = lumiTree.GetEntries()
-		
-		# nickname matching and sum of weights
-		no_regex_match = True if args.nick=='NONE' else False
-		nick = ( re.match(args.nick, os.path.basename(file_name)).groupdict().values()[0] if not no_regex_match else '*')
-		
-		for entry in xrange(n_entries):
-			lumiTree.GetEntry(entry)
-			n_entries_per_nick[nick] = n_entries_per_nick.get(nick, 0.0) + lumiTree.filterMetadata.nEventsTotal
-			sumweight_per_nick[nick] = sumweight_per_nick.get(nick, 0.0) + (lumiTree.filterMetadata.nEventsTotal - 2 * lumiTree.filterMetadata.nNegEventsTotal)
+	for samplelist in args.files:
+		with open(samplelist) as samplelist:
+			samplefiles = samplelist.readlines()
 
-		#counter
-		if (fileindex % 100 == 0):
-			print "...processing file %d (nickname: %s)" % (fileindex, nick)
-			print "\tweighted entries = ", sumweight_per_nick[nick]
-			print "\tentries = ", n_entries_per_nick[nick]
+	for filelist in samplefiles:
+		filelist=filelist.strip()
+		filelist =filelist.split("/",2)[-1]
+		with open(filelist) as lists:
+			rootfiles = lists.readlines()
 
-		#close rootfile
-		root_file.Close()
+		sumweight_per_nick = {}
+		n_entries_per_nick = {}
+		for (fileindex, file_name) in enumerate(rootfiles):
+			file_name = file_name.strip()
+			#print file_name
+			# retrieve weights and total number of events
+			root_file = ROOT.TFile.Open(file_name, "READ")
+			lumiTree = root_file.Get("Lumis")
+			n_entries = lumiTree.GetEntries()
+		
+			# nickname matching and sum of weights
+			no_regex_match = True if args.nick=='NONE' else False
+			nick = ( re.match(args.nick, os.path.basename(file_name)).groupdict().values()[0] if not no_regex_match else '*')
+			for entry in xrange(n_entries):
+				lumiTree.GetEntry(entry)
+				n_entries_per_nick[nick] = n_entries_per_nick.get(nick, 0.0) + lumiTree.filterMetadata.nEventsTotal
+				sumweight_per_nick[nick] = sumweight_per_nick.get(nick, 0.0) + (lumiTree.filterMetadata.nEventsTotal - 2 * lumiTree.filterMetadata.nNegEventsTotal)
+			#counter
+			if (fileindex % 100 == 0):
+				print "...processing file %d (nickname: %s)" % (fileindex, nick)
+				print "\tweighted entries = ", sumweight_per_nick[nick]
+				print "\tentries = ", n_entries_per_nick[nick]
 
-	# print results and save to dataset
-	if not no_regex_match: 
-		dataset = os.path.expandvars("$ARTUSPATH/../Kappa/Skimming/data/datasets.json")
-		dictionary = load_database(dataset)
-	for index, (nick, sumweight) in enumerate(sumweight_per_nick.items()):
+			#close rootfile
+			root_file.Close()
+			
+		# print results and save to dataset
 		if not no_regex_match: 
-			sample_name = get_sample_by_nick(nick)
-			dictionary[nick]["n_events_generated"] = str(int(n_entries_per_nick[nick]))
-			dictionary[nick]["generatorWeight"] = sumweight/n_entries_per_nick[nick]
+			dataset = os.path.expandvars("$ARTUSPATH/../Kappa/Skimming/data/datasets.json")
+			dictionary = load_database(dataset)
+		for index, (nick, sumweight) in enumerate(sumweight_per_nick.items()):
+			if not no_regex_match: 
+				sample_name = get_sample_by_nick(nick)
+				dictionary[nick]["n_events_generated"] = str(int(n_entries_per_nick[nick]))
+				dictionary[nick]["generatorWeight"] = sumweight/n_entries_per_nick[nick]
 
-			log.info("\n\n\"" + sample_name + "\"" + ": {")
-		log.info("\tn_events_generated: " + str(int(n_entries_per_nick[nick])))
-		log.info("\tgeneratorWeight: " + str(sumweight/n_entries_per_nick[nick]))
+				log.info("\n\n\"" + sample_name + "\"" + ": {")
+			log.info("\tn_events_generated: " + str(int(n_entries_per_nick[nick])))
+			log.info("\tgeneratorWeight: " + str(sumweight/n_entries_per_nick[nick]))
 	
-	log.info("}")
-	if not no_regex_match: save_database(dictionary, dataset)
+		log.info("}")
+		if not no_regex_match: save_database(dictionary, dataset)
 
-
+		
 
 if __name__ == "__main__":
 	main()
