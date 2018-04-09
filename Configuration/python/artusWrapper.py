3927368dfde6884bf31c22451aa8e8b5205bacf0
jordy <jordy.degens@rwth-aachen.de>
2018-04-09 13:41:35 +0200

    back to the old with json, python config now in kithiggs/higgstautauanalysiswrapper.py

--- Configuration/python/artusWrapper.py
+++ Configuration/python/artusWrapper.py
@@ -41,8 +41,8 @@ class ArtusWrapper(object):
 
 	def __init__(self, executable=None, userArgParsers=None):
 
-		#self._config = jsonTools.JsonDict()
-		self._config = {}
+		self._config = jsonTools.JsonDict()
+		
 		self._executable = executable
 
 		self._parser = None
@@ -301,14 +301,10 @@ class ArtusWrapper(object):
 	def readInExternals(self):
 		# to be overwritten by users
 		pass
-
 	def expandConfig(self):
 
 		# merge all base configs into the main config
-		if self._args.base_configs!=None:
-			self._config = jsonTools.JsonDict(self._config)
-			self._config += jsonTools.JsonDict.mergeAll(self._args.base_configs)
-
+		self._config += jsonTools.JsonDict.mergeAll(self._args.base_configs)
 		self._gridControlInputFiles = {}
 
 		#Set Input Filenames
@@ -333,104 +329,26 @@ class ArtusWrapper(object):
 			self.setOutputFilename(self._args.output_file)
 
 		# treat pipeline configs
-		"""		
 		pipelineJsonDict = {}
-		
 		if self._args.pipeline_configs and len(self._args.pipeline_configs) > 0:
 			pipelineJsonDict = []
-			print self._args.pipeline_configs
 			for pipelineConfigs in self._args.pipeline_configs:
 				pipelineJsonDict.append(jsonTools.JsonDict.expandAll(*map(lambda pipelineConfig: jsonTools.JsonDict.mergeAll(*pipelineConfig.split()), pipelineConfigs)))
 			pipelineJsonDict = jsonTools.JsonDict.mergeAll(*pipelineJsonDict)
 			pipelineJsonDict = jsonTools.JsonDict({"Pipelines": pipelineJsonDict})
 		pipelineJsonDict = jsonTools.JsonDict(pipelineJsonDict)
-		"""
 
-		nickname = self.determineNickname(self._args.nick)
-		print nickname
-		print self._args.channels
-		if nickname != "auto":
-			if self._args.channels and len(self._args.channels) > 0:
-				pipeline_config = {} #TODO add systematic option, extra loop or in pipelines?
-				syst_python_config = systematics.Systematics_Config()
+		# treat pipeline base configs
+		pipelineBaseJsonDict = jsonTools.JsonDict()
+		if self._args.pipeline_base_configs and len(self._args.pipeline_base_configs) > 0:
+			pipelineBaseJsonDict = jsonTools.JsonDict({
+				"Pipelines" : {
+					pipeline : jsonTools.JsonDict(*self._args.pipeline_base_configs) for pipeline in pipelineJsonDict["Pipelines"].keys()
+				}
+			})
 
-				for selected_channel in self._args.channels:
-					if selected_channel == "mt":
-						channel_python_config = mt.mt_ArtusConfig() 
-						channel_python_config.build_config(nickname)
-					
-					elif selected_channel == "et":
-						channel_python_config = et.et_ArtusConfig() 
-						channel_python_config.build_config(nickname)
-					
-					elif selected_channel == "em":
-						channel_python_config = em.em_ArtusConfig() #TODO change to the em config but for now let it be
-						channel_python_config.build_config(nickname)
-
-					elif selected_channel == "tt":
-						channel_python_config = tt.tt_ArtusConfig() 
-						channel_python_config.build_config(nickname)
-
-					elif selected_channel == "mm":
-						channel_python_config = mm.mm_ArtusConfig() #TODO change to the mm config but for now let it be
-						channel_python_config.build_config(nickname)
-
-					elif selected_channel == "gen":	
-						channel_python_config = gen.gen_ArtusConfig()
-						channel_python_config.build_config(nickname)
-					else:	
-						log.error("COULD NOT FIND CHANNEL")
-					#Ideas:
-					#TODO add function that adds list of quantities to the pipeline_python_config
-					"""
-					if len(args.add_quantities)>0:
-						pipeline_python_config["Quantities"]+= args.add_quantities
-					if len(args.add_processors)>0:
-						pipeline_python_config["Processors"]+= args.add_processors
-					#TODO: add function that lets you run artus for variables which are not in allready processed output files such that you can merge them later into the old outputs
-					if len(args.new_quantities)>0:
-						pipeline_python_config["Quantities"] = args.new_quantities
-					if len(args.new_processors)>0:
-						pipeline_python_config["Processors"] = args.add_processors
-					
-					"""
-					if selected_channel != "gen":	
-						for systematic_shift in self._args.systematics:
-
-							if systematic_shift != "nominal":
-								for shiftdirection in ["Up", "Down"]:
-									systematic_name = systematic_shift+shiftdirection
-									syst_python_config.clear_config()
-									syst_python_config.build_systematic_config(nickname, systematic_name)
-									pipeline_config[selected_channel+"_"+systematic_name] = copy.deepcopy(syst_python_config)
-									pipeline_config[selected_channel+"_"+systematic_name].update(copy.deepcopy(channel_python_config))
-
-
-							elif systematic_shift == "nominal":
-								syst_python_config.clear_config()
-								pprint(syst_python_config)
-								pipeline_config[selected_channel+"_"+systematic_shift] = copy.deepcopy(syst_python_config)
-								pipeline_config[selected_channel+"_"+systematic_shift].update(channel_python_config)
-
-	
-					elif selected_channel == "gen":
-						pipeline_config["gen"] = channel_python_config
-				
-
-			self._config["Pipelines"] = pipeline_config	
-			
-
-			# treat pipeline base configs
-
-			pipelineBaseDict = baseconfigcp.Baseconfig_cp(nickname)
-			globalProcessorsDict = globalprocessors.globalProccesors(nickname)
-
-			# merge pipeline config and baseline config
-			self._config.update(pipelineBaseDict)
-			self._config.update(globalProcessorsDict)
-			
-	
-		self._config = jsonTools.JsonDict(self._config)
+		# merge resulting pipeline config into the main config
+		self._config += (pipelineBaseJsonDict + pipelineJsonDict)
 
 		# treat includes, nicks and comments
 		self.tmp_directory_remote_files = None
@@ -438,6 +356,7 @@ class ArtusWrapper(object):
 			self._config = self._config.doIncludes().doComments()
 			self._config["BatchMode"] = True
 		else:
+			nickname = self.determineNickname(self._args.nick)
 			log.debug("Prepare config for \""+nickname+"\" sample...")
 			self._config = self._config.doIncludes().doNicks(nickname).doComments()
 			self._config["Nickname"] = nickname
@@ -458,6 +377,10 @@ class ArtusWrapper(object):
 
 		# set log level
 		self._config["LogLevel"] = self._args.log_level
+		
+		self._config["Pipelines"]["et_nominal"]["Quantities"] = sorted(self._config["Pipelines"]["et_nominal"]["Quantities"], key=lambda s: s.lower())
+
+	
 
 	def determineNickname(self, nickname):
 		if nickname.find("auto") != -1: # automatic determination of nicknames
