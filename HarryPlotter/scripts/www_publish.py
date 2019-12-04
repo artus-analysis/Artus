a7f8d6c805c0d60d5c41024c8c37782477866c61
Olena Hlushchenko <greyxray@gmail.com>
2019-12-04 16:55:00 +0100

    hp: www force exporting json and no-date format
    (cherry picked from commit 829f175684022bba3f9281e045922361d063be7e)
    Signed-off-by: Olena Hlushchenko <greyxray@gmail.com>

--- HarryPlotter/scripts/www_publish.py
+++ HarryPlotter/scripts/www_publish.py
@@ -19,21 +19,25 @@ parser.add_argument("-i", "--input-dirs", required=True, nargs="+",
 parser.add_argument("-o", "--output-subfolders", default=[""], nargs="+",
                     help="Output Subfolders")
 parser.add_argument("--no-publish", default=False, action="store_true", help="only produce overview.html but do not attempt to publish plots")
+parser.add_argument("--export-json", default=False, action="store_true", help="export-json")
+parser.add_argument("--www-nodate", default=False, action="store_true", help="www_nodate")
 
 args = parser.parse_args()
 logger.initLogger(args)
 
 if ((len(args.input_dirs) != len(args.output_subfolders))) and not args.no_publish:
-	log.fatal("you tried to use " + str(len(args.input_dirs)) + " input directories but " + str(len(args.output_subfolders)) + " output directories. This might cause overwriting of files. Quit!")
-	sys.exit(1)
+    log.fatal("you tried to use " + str(len(args.input_dirs)) + " input directories but " + str(len(args.output_subfolders)) + " output directories. This might cause overwriting of files. Quit!")
+    sys.exit(1)
 
 if args.no_publish:
-	args.output_subfolders = args.input_dirs
+    args.output_subfolders = args.input_dirs
 
 for input_dir, subfolder in zip(args.input_dirs, args.output_subfolders):
-	PlotData.webplotting(
-			www=subfolder,
-			output_dir=input_dir,
-			export_json=False,
-			no_publish=args.no_publish
-	)
+    print subfolder
+    PlotData.webplotting(
+            www=subfolder,
+            output_dir=input_dir,
+            export_json=args.export_json,
+            www_nodate=args.www_nodate,
+            no_publish=args.no_publish
+    )
