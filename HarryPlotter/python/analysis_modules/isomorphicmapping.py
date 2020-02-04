f920cde4247f4867dade69795270b1a2ef628a72
Lucas Wiens <lucas.wiens@rwth-aachen.de>
2020-02-04 13:13:53 +0100

    Use standard ROOT functions to get the quantile-quantile plot

--- HarryPlotter/python/analysis_modules/isomorphicmapping.py
+++ HarryPlotter/python/analysis_modules/isomorphicmapping.py
@@ -27,12 +27,9 @@ class IsomorphicMapping(analysisbase.AnalysisBase):
 				help="Nick names for the data sample. Multiple nicks (whitespace separated) will be summed up before calculating the isomorphic mapping. This will be used to calibrate the MC.")
 		self.isomap_options.add_argument("--isomap-result-nicks", nargs="+",
 				help="Nick names for the resulting graphs.")
-		self.isomap_options.add_argument("--axis-ordering", default="zyx",
-				choices=["xyz", "xzy", "yxz", "zxy", "yzx", "zyx"],
-				help="Axis ordering for multi-dimensional input histograms used for the cumulative distributions. [Default: %(default)s]")
-		self.isomap_options.add_argument("--axis-sense", default=1, type=int,
-				choices=[1, -1],
-				help="Sense for walking through bins when calculating the cumulative distributions. 1: ascending, -1: descending. [Default: %(default)s]")
+		self.isomap_options.add_argument("--n-quantile-positions", default=10000, type=int,
+				help="Choose how many quantile position will be probed for. [Default: $(default)s]")
+
 
 	def prepare_args(self, parser, plotData):
 		super(IsomorphicMapping, self).prepare_args(parser, plotData)
@@ -65,7 +62,6 @@ class IsomorphicMapping(analysisbase.AnalysisBase):
 		normalise_mc = 0.
 		normalise_data = 0.
 
-		isomap_tgraph = ROOT.TGraph()
 		isomap_histogram = {}
 		cumulative_histogram = {}
 		norm_factor = {}
@@ -73,80 +69,26 @@ class IsomorphicMapping(analysisbase.AnalysisBase):
 		nick_collection_mc = [plotData.plotdict["isomap_mc_nicks"]][0][0]
 		nick_collection_data = [plotData.plotdict["isomap_data_nicks"]][0][0]
 		nick_collection = nick_collection_mc + nick_collection_data
+		isomap_result_nick = plotData.plotdict["isomap_result_nicks"][-1]
+		histogram_mc = roottools.RootTools.add_root_histograms(*[plotData.plotdict["root_objects"][nick] for nick in nick_collection_mc])
+		histogram_data = roottools.RootTools.add_root_histograms(*[plotData.plotdict["root_objects"][nick] for nick in nick_collection_data])
 
-		for cumulative_input_nick in nick_collection:
-			isomap_histogram[cumulative_input_nick] = plotData.plotdict["root_objects"][cumulative_input_nick]
-			cumulative_histogram[cumulative_input_nick] = isomap_histogram[cumulative_input_nick].Clone("cumulative_distribution_" + hashlib.md5("_".join([isomap_histogram[cumulative_input_nick].GetName(), cumulative_input_nick, "cumulative_output_nick"])).hexdigest())
-			cumulative_histogram[cumulative_input_nick].Reset()
+		n_bins = plotData.plotdict["n_quantile_positions"]
 
-			n_bins = {
-				"x" : isomap_histogram[cumulative_input_nick].GetNbinsX(),
-				"y" : isomap_histogram[cumulative_input_nick].GetNbinsY(),
-				"z" : isomap_histogram[cumulative_input_nick].GetNbinsZ(),
-			}
-			bin_ranges = {}
-			for axis, bins in n_bins.iteritems():
-				bin_ranges[axis] = (1, 2) if bins == 1 else (0, bins+2)
+		quantile_positions = np.linspace(0, 1, n_bins)
 
-			current_bin = {
-				"x" : 0,
-				"y" : 0,
-				"z" : 0,
-			}
-			current_sum = 0.0
-			current_sum_error = 0.0
+		x_value_mc = np.array([ROOT.Double(0)] * n_bins)
+		histogram_mc.GetQuantiles(n_bins, x_value_mc, quantile_positions)
 
-			for bin_axis_1 in range(*bin_ranges[plotData.plotdict["axis_ordering"][0]])[::plotData.plotdict["axis_sense"]]:
-				current_bin[plotData.plotdict["axis_ordering"][0]] = bin_axis_1
+		x_value_data = np.array([ROOT.Double(0)] * n_bins)
+		histogram_data.GetQuantiles(n_bins, x_value_data, quantile_positions)
 
-				for bin_axis_2 in range(*bin_ranges[plotData.plotdict["axis_ordering"][1]])[::plotData.plotdict["axis_sense"]]:
-					current_bin[plotData.plotdict["axis_ordering"][1]] = bin_axis_2
-
-					for bin_axis_3 in range(*bin_ranges[plotData.plotdict["axis_ordering"][2]])[::plotData.plotdict["axis_sense"]]:
-						current_bin[plotData.plotdict["axis_ordering"][2]] = bin_axis_3
-
-						x_bin = current_bin["x"]
-						y_bin = current_bin["y"]
-						z_bin = current_bin["z"]
-						global_bin = isomap_histogram[cumulative_input_nick].GetBin(x_bin, y_bin, z_bin)
-
-						current_sum += isomap_histogram[cumulative_input_nick].GetBinContent(global_bin)
-						current_sum_error = math.sqrt(pow(current_sum_error, 2) + pow(isomap_histogram[cumulative_input_nick].GetBinError(global_bin), 2))
-
-						cumulative_histogram[cumulative_input_nick].SetBinContent(global_bin, current_sum)
-						cumulative_histogram[cumulative_input_nick].SetBinError(global_bin, current_sum_error)
-			norm_factor[cumulative_input_nick] = current_sum
-
-		norm_mc = sum([norm_factor[key] for key in nick_collection_mc])
-		cumulative_histogram_mc = roottools.RootTools.add_root_histograms(*[cumulative_histogram[nick] for nick in nick_collection_mc], scale_factors= [1./norm_mc]*len(nick_collection_mc))
-
-		norm_data = sum([norm_factor[key] for key in nick_collection_data])
-		cumulative_histogram_data = roottools.RootTools.add_root_histograms(*[cumulative_histogram[nick] for nick in nick_collection_data], scale_factors= [1./norm_data]*len(nick_collection_data))
-
-		# Calculate isomap
-		isomap_result_nick = plotData.plotdict["isomap_result_nicks"][0]
-
-		for bin_axis_1 in range(*bin_ranges[plotData.plotdict["axis_ordering"][0]])[::plotData.plotdict["axis_sense"]]:
-			current_bin[plotData.plotdict["axis_ordering"][0]] = bin_axis_1
-
-			for bin_axis_2 in range(*bin_ranges[plotData.plotdict["axis_ordering"][1]])[::plotData.plotdict["axis_sense"]]:
-				current_bin[plotData.plotdict["axis_ordering"][1]] = bin_axis_2
-
-				for bin_axis_3 in range(*bin_ranges[plotData.plotdict["axis_ordering"][2]])[::plotData.plotdict["axis_sense"]]:
-					current_bin[plotData.plotdict["axis_ordering"][2]] = bin_axis_3
-
-					x_bin = current_bin["x"]
-					y_bin = current_bin["y"]
-					z_bin = current_bin["z"]
-					global_bin = cumulative_histogram_mc.GetBin(x_bin, y_bin, z_bin)
-
-					x_value_mc = cumulative_histogram_mc.GetBinCenter(global_bin)
-					y_value_mc = cumulative_histogram_mc.GetBinContent(global_bin)
-
-					x_value_data = np.array([ROOT.Double(0)])
-					cumulative_histogram_data.GetQuantiles(1, x_value_data, np.array([y_value_mc]))
-					isomap_tgraph.SetPoint(global_bin, x_value_mc, x_value_data[0])
+		isomap_tgraph = ROOT.TGraph(n_bins, x_value_mc, x_value_data)
+		isomap_mc_tgraph = ROOT.TGraph(n_bins , x_value_mc, x_value_mc)
+		isomap_diff_tgraph = ROOT.TGraph(n_bins, x_value_mc, x_value_data - x_value_mc)
 
 		if hasattr(isomap_histogram, "SetDirectory"): isomap_histogram.SetDirectory(0)
 		isomap_tgraph.SetTitle("")
 		plotData.plotdict["root_objects"][isomap_result_nick] = isomap_tgraph
+		plotData.plotdict["root_objects"][isomap_result_nick + "_mc"] = isomap_mc_tgraph
+		plotData.plotdict["root_objects"]["difference"] = isomap_diff_tgraph
